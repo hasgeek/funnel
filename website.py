4119f61e54118878d4bd990e28ae9b510e2fad70
Samyak Bhuta <samyak.bhuta@gmail.com>
2011-11-03 08:33:34 +0000

    Added jquery.tablesorter.js plugin to make all the table columns on the site sortable.

--- website.py
+++ website.py
@@ -20,6 +20,7 @@ assets = Environment(app)
 js = Bundle('js/libs/jquery-1.5.1.min.js',
             'js/libs/jquery.form.js',
             'js/libs/jquery.oembed.js',
+            'js/libs/jquery.tablesorter.min.js',
             'js/scripts.js',
             filters='jsmin', output='js/packed.js')
 
