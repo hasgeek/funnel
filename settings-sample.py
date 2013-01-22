b17d026c0895f35d80abd73a41d8cf48d6a39f6d
kracekumar <me@kracekumar.com>
2013-01-22 14:14:25 +0530

    Added traling slash in settings-sample.py

--- settings-sample.py
+++ settings-sample.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'make this something random'
 #: Timezone
 TIMEZONE = 'Asia/Calcutta'
 #: LastUser server
-LASTUSER_SERVER = 'https://auth.hasgeek.com'
+LASTUSER_SERVER = 'https://auth.hasgeek.com/'
 #: LastUser client id
 LASTUSER_CLIENT_ID = ''
 #: LastUser client secret
