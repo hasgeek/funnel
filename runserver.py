d33cd5a6d488cd42a3fe300e958cb510880de098
Kiran Jonnalagadda <kiran@hasgeek.com>
2013-05-21 15:53:09 +0530

    Optional port number in runserver (via @anandology).

--- runserver.py
+++ runserver.py
@@ -1,4 +1,10 @@
+import sys
 from website import app, models, init_for
 init_for('dev')
 models.db.create_all()
-app.run('0.0.0.0', port=3000, debug=True)
+
+try:
+    port = int(sys.argv[1])
+except (IndexError, ValueError):
+    port = 3000
+app.run('0.0.0.0', port=port, debug=True)
