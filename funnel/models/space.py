d32d8462d11ce31fd54dc86ba7b59e0d528dad82
kracekumar <me@kracekumar.com>
2013-11-05 15:42:27 +0530

    Removed extra space

--- funnel/models/space.py
+++ funnel/models/space.py
@@ -83,4 +83,3 @@ class ProposalSpace(BaseNameMixin, db.Model):
             return url_for('usergroup_new', space=self.name, _external=_external)
         elif action == 'new-proposal':
             return url_for('proposal_new', space=self.name, _external=_external)
-        
