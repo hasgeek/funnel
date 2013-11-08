b680dabf86ad11b1d589b75062dca552752d5858
kracekumar <me@kracekumar.com>
2013-11-08 18:21:04 +0530

    rename

--- funnel/models/session.py
+++ funnel/models/session.py
@@ -1,21 +1,22 @@
 # -*- coding: utf-8 -*-
 
 from datetime import datetime
-from . import db, BaseScopedNameMixin, MarkdownColumn
+from . import db, BaseScopedIdMixin, MarkdownColumn
 
 
-__all__ = ['Schedule']
+__all__ = ['Session']
 
 
-class Schedule(BaseScopedNameMixin, db.Model):
-    __tablename__ = 'schedule'
+class Session(BaseScopedIdMixin, db.Model):
+    __tablename__ = 'session'
 
     description = MarkdownColumn('description', default=u'', nullable=False)
     proposal_id = db.Column(db.Integer, nullable=True)
     start_datetime = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
     end_datetime = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
-    time_zone = db.Column(db.Unicode(30), default=u'UTC', nullable=False)
-    proposal_space_id = db.Column(db.Integer, db.ForeignKey('proposal_space.id'), nullable=False)
+    proposal_space_id = db.Column(db.Integer, db.ForeignKey('proposal_space.id'),
+    	backref=db.backref('sessions', cascade='all, delete-orphan', order_by='Session.start_datetime'),
+    	nullable=False)
     venue_room_id = db.Column(db.Integer, db.ForeignKey('venue_room.id'), nullable=False)
     is_break = db.Column(db.Boolean, default=False, nullable=False)
 
