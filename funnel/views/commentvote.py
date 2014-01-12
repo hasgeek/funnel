f90c2b771910d53a1c1a1d41fc8e1be54da95e07
Mitesh Ashar <email@miteshashar.com>
2014-01-13 00:45:24 +0530

    Using AJAX for Proposal votes.

--- funnel/views/commentvote.py
+++ funnel/views/commentvote.py
@@ -3,60 +3,58 @@
 from flask import redirect, g, flash, request, render_template, get_template_attribute
 from coaster.views import jsonp, load_models
 from baseframe import _
+from baseframe.forms import Form
 
 from .. import app, lastuser
 from ..models import *
 
 
 # FIXME: This voting method uses GET but makes db changes. Not correct. Should be POST
-@app.route('/<space>/<proposal>/voteup')
+@app.route('/<space>/<proposal>/voteup', methods=['POST'])
 @lastuser.requires_login
 @load_models(
     (ProposalSpace, {'name': 'space'}, 'space'),
     (Proposal, {'url_name': 'proposal', 'proposal_space': 'space'}, 'proposal'),
     permission='vote-proposal', addlperms=lastuser.permissions)
 def proposal_voteup(space, proposal):
-    proposal.votes.vote(g.user, votedown=False)
-    db.session.commit()
-    if request.is_xhr:
+    if request.is_xhr and Form().validate_on_submit():
+        proposal.votes.vote(g.user, votedown=False)
+        db.session.commit()
         return render_template('proposal_votes.html', proposal=proposal)
     else:
-        flash(_("Your vote has been recorded"), 'info')
-        return redirect(proposal.url_for())
+        return (_("Voting link timed out, please refresh the page."), 401)
 
 
 # FIXME: This voting method uses GET but makes db changes. Not correct. Should be POST
-@app.route('/<space>/<proposal>/votedown')
+@app.route('/<space>/<proposal>/votedown', methods=['POST'])
 @lastuser.requires_login
 @load_models(
     (ProposalSpace, {'name': 'space'}, 'space'),
     (Proposal, {'url_name': 'proposal', 'proposal_space': 'space'}, 'proposal'),
     permission='vote-proposal', addlperms=lastuser.permissions)
 def proposal_votedown(space, proposal):
-    proposal.votes.vote(g.user, votedown=True)
-    db.session.commit()
-    if request.is_xhr:
+    if request.is_xhr and Form().validate_on_submit():
+        proposal.votes.vote(g.user, votedown=True)
+        db.session.commit()
         return render_template('proposal_votes.html', proposal=proposal)
     else:
-        flash(_("Your vote has been recorded"), 'info')
-        return redirect(proposal.url_for())
+        return (_("Voting link timed out, please refresh the page."), 401)
 
 
 # FIXME: This voting method uses GET but makes db changes. Not correct. Should be POST
-@app.route('/<space>/<proposal>/cancelvote')
+@app.route('/<space>/<proposal>/cancelvote', methods=['POST'])
 @lastuser.requires_login
 @load_models(
     (ProposalSpace, {'name': 'space'}, 'space'),
     (Proposal, {'url_name': 'proposal', 'proposal_space': 'space'}, 'proposal'),
     permission='vote-proposal', addlperms=lastuser.permissions)
 def proposal_cancelvote(space, proposal):
-    proposal.votes.cancelvote(g.user)
-    db.session.commit()
-    if request.is_xhr:
+    if request.is_xhr and Form().validate_on_submit():
+        proposal.votes.cancelvote(g.user)
+        db.session.commit()
         return render_template('proposal_votes.html', proposal=proposal)
     else:
-        flash(_("Your vote has been withdrawn"), 'info')
-        return redirect(proposal.url_for())
+        return (_("Voting link timed out, please refresh the page."), 401)
 
 
 @app.route('/<space>/<proposal>/comments/<int:comment>/json')
