b927c178ccfb9216eb07f142b70a57b7486255b9
Kiran Jonnalagadda <jace@pobox.com>
2014-05-15 05:48:21 +0530

    No more Markdown preview with Codemirror in use.

--- funnel/static/js/scripts.js
+++ funnel/static/js/scripts.js
@@ -9,51 +9,6 @@ function radioHighlight(radioName, highlightClass) {
     $(selector).click(handler);
 }
 
-function markdownPreview() {
-    converter = new Showdown.converter();
-    $(".markdown-field .button").click(function(e) {
-        var field = $(this).closest('.markdown-field').parent().find('textarea');
-        var action = $(this).attr('id').split('-')[1];
-        var field_id = $(field).attr('id');
-        var preview_id = field_id + "-preview-block";
-        var control = $(field).parent();
-        var write = $("#"+field_id+"-write");
-        var preview = $("#"+field_id+"-preview");
-        if (action=="write") {
-            if ($('#'+preview_id).length > 0) {
-                $('#'+preview_id).hide();
-                $('#'+preview_id).children().remove();
-            }
-            $(control).show();
-            $(write).attr('class', 'button-selected');
-            $(preview).attr('class', 'button');
-        } else if (action=="preview") {
-            var html_text;
-            if ($(field).val() === "") {
-                html_text = $('<p>').html("Nothing to preview");
-            } else {
-                html_text = $(converter.makeHtml($(field).val()));
-            }
-            $(preview).attr('class', 'button-selected');
-            $(write).attr('class', 'button');
-            if ($('#'+preview_id).length === 0) {
-                var preview_control = $('<div>').attr('class', 'preview-control');
-                preview = $('<div>').attr('id', preview_id).attr('class', 'preview-block');
-                $(preview).append(html_text);
-                $(preview_control).append(preview);
-                $(control).parent().append(preview_control);
-                $(control).hide();
-            } else {
-                $(control).hide();
-                $('#'+preview_id).children().remove();
-                $(html_text).appendTo($('#'+preview_id));
-                $('#'+preview_id).show();
-            }
-        }
-        e.preventDefault();
-    });
-}
-
 function commentsInit(pageURL) {
     $(".comments .collapse").click(function() {
         $(this).addClass('hidden');
@@ -134,6 +89,3 @@ $(function() {
     }
   });
 });
-
-// Enable markdown previews
-markdownPreview();
