9bf628aade6f42bd6dabda14b8a841969d714f3b
Shreyas Satish <shreyas.satish@gmail.com>
2015-04-08 13:15:36 +0530

    html badges

--- funnel/models/event.py
+++ funnel/models/event.py
@@ -3,14 +3,11 @@ from . import db, BaseMixin
 from .space import ProposalSpace
 from .user import User
 import uuid
-import svgutils.transform as sg
-import sys
+from .. import app
 import qrcode
 import qrcode.image.svg
-import subprocess
-import os
-import time
-from .. import app
+from flask import url_for, render_template
+
 
 __all__ = ['Event', 'TicketType', 'EventTicketType', 'Participant', 'Attendee', 'SyncTicket']
 
@@ -18,6 +15,24 @@ __all__ = ['Event', 'TicketType', 'EventTicketType', 'Participant', 'Attendee', 
 def make_key():
     # 8-character string sliced from uuid
     return str(uuid.uuid4())[0:8]
+
+
+def split_name(fullname):
+    name_splits = fullname.split()
+    first_name = name_splits[0]
+    last_name = " ".join([s for s in name_splits[1:]])
+    return first_name, last_name
+
+
+def format_twitter(twitter):
+    return "@{0}".format(twitter)
+
+
+def make_qrcode(data, path):
+    factory = qrcode.image.svg.SvgPathImage
+    img = qrcode.make(data, image_factory=factory)
+    img.save(path)
+    return path
 
 
 class EventTicketType(BaseMixin, db.Model):
@@ -75,54 +90,30 @@ class Participant(BaseMixin, db.Model):
     proposal_space = db.relationship(ProposalSpace,
         backref=db.backref('participants', cascade='all, delete-orphan', lazy='dynamic'))
 
-    def make_badge(self, space):
-        participant = self
+    def make_file_path(self, space):
+        return "{0}_{1}_{2}".format(space.profile.name, space.name, str(self.id))
+
+    def make_badge(self, space, qrcode_full_path=True):
+        first_name, last_name = split_name(self.fullname)
+        qrcode_filename = '{0}.{1}'.format(self.make_file_path(space), 'svg')
+        qrcode_data = "{0}:{1}".format(str(self.id), self.key)
+        qrcode_path = "{0}/{1}".format(app.config.get('BADGES_PATH'), qrcode_filename)
+        make_qrcode(qrcode_data, qrcode_path)
+        if qrcode_full_path:
+            qrcode_path = url_for('static', filename="{0}/{1}".format('badges', qrcode_filename))
+        else:
+            qrcode_path = qrcode_filename
+        badge_html = render_template('badge.html', first_name=first_name, last_name=last_name, twitter=format_twitter(self.twitter), qrcode_path=qrcode_path, participant=self)
+        return badge_html
+
+    def make_badge_file(self, space):
         basepath = app.config['BADGES_PATH']
-        filename = '{0}_{1}_{2}'.format(space.profile.name, space.name, str(self.id))
-        factory = qrcode.image.svg.SvgPathImage
-        img = qrcode.make("{0}:{1}".format(str(participant.id), participant.key), image_factory=factory)
-        img_path = "{0}/{1}.svg".format(basepath, filename)
-        img.save(img_path)
-
-        fields = []
-        name_splits = participant.fullname.split()
-        first_name = name_splits[0]
-        last_name = "".join([s for s in name_splits[1:]])
-        x = 400
-        y = 300
-        fields.append(sg.TextElement(x, y, first_name, size=140, weight="bold"))
-        if last_name:
-            y += 100
-            fields.append(sg.TextElement(x, y, last_name, size=80))
-        if participant.company:
-            y += 100
-            fields.append(sg.TextElement(x, y, participant.company, size=60))
-        if participant.twitter:
-            y += 100
-            fields.append(sg.TextElement(x, y, "@{0}".format(participant.twitter), size=60))
-        y += 50
-        qr_sg = sg.fromfile(img_path).getroot()
-        qr_sg.moveto(370, 650, scale=15)
-        fields.append(qr_sg)
-
-        # badge_sg = sg.fromfile(badge_template)
-        badge_sg = sg.SVGFigure("50cm", "30cm")
-        badge_sg.append(fields)
-        # badge_path = 'badge_svgs/{0}_{1}.svg'.format(filename, 'badge')
-        badge_svg_path = "{0}/{1}_badge.svg".format(basepath, filename)
-        badge_sg.save(badge_svg_path)
-        badge_path = '{0}/{1}.pdf'.format(basepath, filename)
-        # inkscape --export-pdf=metarefresh_2015_1.svg_badge.pdf metarefresh_2015_1.svg_badge.svg
-        subprocess.call(['inkscape', '--export-pdf={0}'.format(badge_path), badge_svg_path])
-        return badge_path
-
-    def print_badge(self, space):
-        path = app.config['BADGES_PATH'] + '/{0}'.format(self.badge_url(space))
-        os.system("lpr -o page-ranges=1 -P %s %s" % (app.config['PRINTER_NAME'], path))
-
-    def badge_url(self, space):
-        path = '{0}_{1}_{2}.pdf'.format(space.profile.name, space.name, str(self.id))
-        return path
+        filename = '{0}.{1}'.format(self.make_file_path(space), 'html')
+        path = "{0}/{1}".format(basepath, filename)
+        html = self.make_badge(space, qrcode_full_path=False)
+        f = open(path, "wb")
+        f.write(html)
+        f.close()
 
 
 class Attendee(BaseMixin, db.Model):
