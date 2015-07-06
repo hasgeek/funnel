f8a23e0431481da39f6067f35a949ac3dda45a9a
Shreyas Satish <shreyas.satish@gmail.com>
2015-07-06 17:57:07 +0530

    removed id column. using composite primary key

--- funnel/models/contact_exchange.py
+++ funnel/models/contact_exchange.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
 
-from . import db, BaseMixin
+from . import db, TimestampMixin
 
 __all__ = ['ContactExchange']
 
 
-class ContactExchange(BaseMixin, db.Model):
+class ContactExchange(TimestampMixin, db.Model):
     """
     Model to track who scanned whose badge, at which event.
     """
     __tablename__ = 'contact_exchange'
-    user_id = db.Column(None, db.ForeignKey('user.id'))
-    proposal_space_id = db.Column(None, db.ForeignKey('proposal_space.id'))
-    participant_id = db.Column(None, db.ForeignKey('participant.id'))
-    __table_args__ = (db.UniqueConstraint('user_id', 'proposal_space_id', 'participant_id'),)
+    user_id = db.Column(None, db.ForeignKey('user.id'), primary_key=True)
+    proposal_space_id = db.Column(None, db.ForeignKey('proposal_space.id'), primary_key=True)
+    participant_id = db.Column(None, db.ForeignKey('participant.id'), primary_key=True)
