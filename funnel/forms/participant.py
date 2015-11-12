14ffe846d1450026f8accce6175845c454784a50
Shreyas Satish <shreyas.satish@gmail.com>
2015-11-12 16:28:13 +0530

    rm file validator. later release

--- funnel/forms/participant.py
+++ funnel/forms/participant.py
@@ -23,20 +23,6 @@ class ParticipantForm(forms.Form):
         validators=[forms.validators.DataRequired(u"Select at least one event")])
 
 
-class ValidFile(object):
-    # TODO: Move to Baseframe?
-    def __init__(self, allowed_exts=[], message=None):
-        if not message:
-            message = __(u"Please upload a valid file.")
-        self.message = message
-        self.allowed_exts = allowed_exts
-
-    def __call__(self, form, field):
-        ext = os.path.splitext(field.data.filename)[1].strip(".")
-        if ext and ext.lower() not in self.allowed_exts:
-            raise wtforms.validators.StopValidation(self.message)
-
-
 class ParticipantBadgeForm(forms.Form):
     choices = [('', "Badge printing status"), ('t', "Printed"), ('f', "Not printed")]
     badge_printed = forms.SelectField("", choices=[(val_title[0], val_title[1]) for val_title in choices])
