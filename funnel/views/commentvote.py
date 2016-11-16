476a67869d9c7a797b4b6ec1979f6638ab6aa149
Shreyas Satish <shreyas.satish@gmail.com>
2016-11-17 01:45:34 +0530

    added csrf protection

--- funnel/views/commentvote.py
+++ funnel/views/commentvote.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from flask import redirect, g, flash
+from flask import redirect, g, flash, abort
 from coaster.views import jsonp, load_models
-from baseframe import _
+from baseframe import _, forms
 
 from .. import app, lastuser
 from ..models import db, Profile, ProposalSpace, ProposalSpaceRedirect, Proposal, ProposalRedirect, Comment
@@ -16,6 +16,9 @@ from ..models import db, Profile, ProposalSpace, ProposalSpaceRedirect, Proposal
     ((Proposal, ProposalRedirect), {'url_name': 'proposal', 'proposal_space': 'space'}, 'proposal'),
     permission='vote-proposal', addlperms=lastuser.permissions)
 def proposal_voteup(profile, space, proposal):
+    csrf_form = forms.Form()
+    if not csrf_form.validate_on_submit():
+        abort(401)
     proposal.votes.vote(g.user, votedown=False)
     db.session.commit()
     flash(_("Your vote has been recorded"), 'info')
@@ -30,6 +33,9 @@ def proposal_voteup(profile, space, proposal):
     ((Proposal, ProposalRedirect), {'url_name': 'proposal', 'proposal_space': 'space'}, 'proposal'),
     permission='vote-proposal', addlperms=lastuser.permissions)
 def proposal_votedown(profile, space, proposal):
+    csrf_form = forms.Form()
+    if not csrf_form.validate_on_submit():
+        abort(401)
     proposal.votes.vote(g.user, votedown=True)
     db.session.commit()
     flash(_("Your vote has been recorded"), 'info')
@@ -44,6 +50,9 @@ def proposal_votedown(profile, space, proposal):
     ((Proposal, ProposalRedirect), {'url_name': 'proposal', 'proposal_space': 'space'}, 'proposal'),
     permission='vote-proposal', addlperms=lastuser.permissions)
 def proposal_cancelvote(profile, space, proposal):
+    csrf_form = forms.Form()
+    if not csrf_form.validate_on_submit():
+        abort(401)
     proposal.votes.cancelvote(g.user)
     db.session.commit()
     flash(_("Your vote has been withdrawn"), 'info')
@@ -73,6 +82,9 @@ def comment_json(profile, space, proposal, comment):
     (Comment, {'id': 'comment'}, 'comment'),
     permission='vote-comment', addlperms=lastuser.permissions)
 def comment_voteup(profile, space, proposal, comment):
+    csrf_form = forms.Form()
+    if not csrf_form.validate_on_submit():
+        abort(401)
     comment.votes.vote(g.user, votedown=False)
     db.session.commit()
     flash(_("Your vote has been recorded"), 'info')
@@ -88,6 +100,9 @@ def comment_voteup(profile, space, proposal, comment):
     (Comment, {'id': 'comment'}, 'comment'),
     permission='vote-comment', addlperms=lastuser.permissions)
 def comment_votedown(profile, space, proposal, comment):
+    csrf_form = forms.Form()
+    if not csrf_form.validate_on_submit():
+        abort(401)
     comment.votes.vote(g.user, votedown=True)
     db.session.commit()
     flash(_("Your vote has been recorded"), 'info')
@@ -103,6 +118,9 @@ def comment_votedown(profile, space, proposal, comment):
     (Comment, {'id': 'comment'}, 'comment'),
     permission='vote-comment', addlperms=lastuser.permissions)
 def comment_cancelvote(profile, space, proposal, comment):
+    csrf_form = forms.Form()
+    if not csrf_form.validate_on_submit():
+        abort(401)
     comment.votes.cancelvote(g.user)
     db.session.commit()
     flash(_("Your vote has been withdrawn"), 'info')
