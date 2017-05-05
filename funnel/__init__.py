4eafe8bb7a8191f6415fa826e5efdf63c4a7859d
Vidya Ramakrishnan <vidya@hasgeek.in>
2017-05-05 19:04:27 +0530

    Fix merge conflict.

--- funnel/__init__.py
+++ funnel/__init__.py
@@ -45,7 +45,7 @@ lastuser.init_app(app)
 lastuser.init_usermanager(UserManager(db, models.User, models.Team))
 baseframe.init_app(app, requires=['funnel'], ext_requires=[
     ('codemirror-markdown', 'pygments'), 'toastr', 'baseframe-bs3', 'fontawesome>=4.0.0',
-    'baseframe-footable'])
+    'baseframe-footable', 'ractive'])
 app.assets.register('js_fullcalendar',
     Bundle(assets.require('!jquery.js', 'jquery.fullcalendar.js', 'spectrum.js'),
         output='js/fullcalendar.packed.js', filters='uglipyjs'))
