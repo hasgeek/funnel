34e114ba4cec40f3c117704f4472a6eac03e32fb
Vidya Ramakrishnan <vidya@hasgeek.in>
2018-01-16 16:20:20 +0530

    Add ReactFoo pune badge template.

--- funnel/views/participant.py
+++ funnel/views/participant.py
@@ -150,7 +150,7 @@ def participant(profile, space):
     permission='view-participant')
 def participant_badge(profile, space, participant):
     # badge_type = request.args.getlist('type')
-    badge_template = 'https://images.hasgeek.com/embed/file/8c17b42474bf4c2cbf6a0452b70537ca'
+    badge_template = 'https://images.hasgeek.com/embed/file/79304b4afb1f49c2869d91c7d661de2b'
     return render_template('badge.html.jinja2', badge_template=badge_template,
         badges=participant_badge_data([participant], space))
 
@@ -231,6 +231,6 @@ def event_participants_json(profile, space, event):
 def event_badges(profile, space, event):
     badge_printed = True if request.args.get('badge_printed') == 't' else False
     participants = Participant.query.join(Attendee).filter(Attendee.event_id == event.id).filter(Participant.badge_printed == badge_printed).all()
-    badge_template = 'https://images.hasgeek.com/embed/file/8c17b42474bf4c2cbf6a0452b70537ca'
+    badge_template = 'https://images.hasgeek.com/embed/file/79304b4afb1f49c2869d91c7d661de2b'
     return render_template('badge.html.jinja2', badge_template=badge_template,
         badges=participant_badge_data(participants, space))
