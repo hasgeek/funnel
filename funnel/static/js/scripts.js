f3c798af310ddc2441951625ab0743f1519008c7
Vidya Ramakrishnan <vidya@hasgeek.in>
2018-11-19 12:02:03 +0530

    Add boxoffice and hasjob embed.

--- funnel/static/js/scripts.js
+++ funnel/static/js/scripts.js
@@ -328,6 +328,59 @@ window.Talkfunnel.ParticipantTable = {
   }
 };
 
+window.Talkfunnel.TicketWidget = {
+  init: function(config) {
+    console.log("config", config.boxofficeUrl);
+    var url = config.boxofficeUrl + "/api/1/boxoffice.js";
+    console.log("url", url);
+    $.get({
+      url: url,
+      crossDomain: true,
+      timeout: 8000,
+      retries: 5,
+      retryInterval: 8000,
+      success: function(data) {
+        var boxofficeScript = document.createElement('script');
+        boxofficeScript.innerHTML = data.script;
+        document.getElementsByTagName('body')[0].appendChild(boxofficeScript);
+      },
+      error: function(response) {
+        var ajaxLoad = this;
+        ajaxLoad.retries -= 1;
+        var errorMsg;
+        if (response.readyState === 4) {
+          errorMsg = "Server error, please try again later.";
+          $(config.widgetElem).html(errorMsg);
+        }
+        else if (response.readyState === 0) {
+          if (ajaxLoad.retries < 0) {
+            if(!navigator.onLine) {
+              errorMsg = "Unable to connect. There is no network!";
+            }
+            else {
+              errorMsg = "<p>Unable to connect. If you are behind a firewall or using any script blocking extension (like Privacy Badger), please ensure your browser can load boxoffice.hasgeek.com, api.razorpay.com and checkout.razorpay.com .</p>";
+            }
+            $(config.widgetElem).html(errorMsg);
+          } else {
+            setTimeout(function() {
+              $.get(ajaxLoad);
+            }, ajaxLoad.retryInterval);
+          }
+        }
+      }
+    });
+
+    window.addEventListener('onBoxofficeInit', function (e) {
+      window.Boxoffice.init({
+        org: config.org,
+        itemCollection: config.itemCollectionId,
+        paymentDesc: config.itemCollectionTitle,
+
+      });
+    }, false);
+  }
+};
+
 window.TableSearch = function(tableId){
   // a little library that takes a table id
   // and provides a method to search the table's rows for a given query.
