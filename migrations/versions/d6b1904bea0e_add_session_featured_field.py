e7f8a5ff9331438dbe678364b47a454f8af0c126
Bibhas <me@bibhas.in>
2018-11-20 17:55:46 +0530

    Added featured field to Session  (#296)
    * added featured field to session
    
    * added is_featured to session form
    
    * removed a word
    
    * renamed featured field of session
    
    * typo in session form

---
+++ migrations/versions/d6b1904bea0e_add_session_featured_field.py
@@ -0,0 +1,22 @@
+"""add session featured field
+
+Revision ID: d6b1904bea0e
+Revises: 70ffbc1bcf88
+Create Date: 2018-11-20 17:22:11.582473
+
+"""
+
+revision = 'd6b1904bea0e'
+down_revision = '70ffbc1bcf88'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.add_column('session', sa.Column('featured', sa.Boolean(), nullable=False, server_default=sa.sql.expression.false()))
+    op.alter_column('session', 'featured', server_default=None)
+
+
+def downgrade():
+    op.drop_column('session', 'featured')
