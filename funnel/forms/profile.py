6bcced09eb7636ca848e4129fdad6fd0cce35397
Vidya Ramakrishnan <vidya@hasgeek.in>
2018-11-22 15:40:11 +0530

    Add logo_url field to form.

--- funnel/forms/profile.py
+++ funnel/forms/profile.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from flask import g
-from baseframe import _
+from baseframe import _, __
 import baseframe.forms as forms
 from baseframe.forms.sqlalchemy import QuerySelectField
 from ..models import Team
@@ -11,8 +11,8 @@ class NewProfileForm(forms.Form):
     """
     Create a new profile.
     """
-    profile = forms.RadioField(u"Organization", validators=[forms.validators.DataRequired("Select an organization")],
-        description=u"Select the organization you’d like to create a Talkfunnel for")
+    profile = forms.RadioField(__("Organization"), validators=[forms.validators.DataRequired("Select an organization")],
+        description=__("Select the organization you’d like to create a Talkfunnel for"))
 
 
 def profile_teams():
@@ -23,10 +23,12 @@ class EditProfileForm(forms.Form):
     """
     Edit a profile.
     """
-    description = forms.MarkdownField(u"Welcome message",
+    description = forms.MarkdownField(__("Welcome message"),
         validators=[forms.validators.DataRequired(_(u"Please write a message for the landing page"))],
-        description=_(u"This welcome message will be shown on the landing page."))
+        description=__("This welcome message will be shown on the landing page."))
+    logo_url = forms.URLField(__("Logo URL"), description=__("Profile logo"),
+        validators=[forms.validators.Optional(), forms.validators.Length(max=2000)])
     admin_team = QuerySelectField(u"Admin Team",
         validators=[forms.validators.DataRequired(_(u"Please select a team"))],
         query_factory=profile_teams, get_label='title', allow_blank=False,
-        description=_(u"The team of users with administrative rights to this Talkfunnel (owners always have admin access)"))
+        description=__("The team of users with administrative rights to this Talkfunnel (owners always have admin access)"))
