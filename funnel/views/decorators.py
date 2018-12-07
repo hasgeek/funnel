1b3e414db4026a1b1ab64e7b3de2b453221c4800
Bibhas <me@bibhas.in>
2018-12-07 23:56:39 +0530

    updated decorator

--- funnel/views/decorators.py
+++ funnel/views/decorators.py
@@ -1,17 +1,17 @@
 from functools import wraps
-from flask import g, request, redirect, current_app
+from flask import g, redirect, current_app, request
 from funnel import app, funnelapp
 
 
 def legacy_redirect(f):
     @wraps(f)
-    def decorated_function(clview, **kwargs):
-        if g.profile:
-            if g.profile.legacy and current_app == app:
+    def decorated_function(classview, **kwargs):
+        if g.profile and request.method == 'GET':
+            if g.profile.legacy and current_app._get_current_object() is app:
                 with funnelapp.app_context(), funnelapp.test_request_context():
-                    return redirect(clview.obj.url_for(_external=True), code=303)
-            elif not g.profile.legacy and current_app == funnelapp:
+                    return redirect(classview.obj.url_for(classview.current_handler.name, _external=True), code=303)
+            elif not g.profile.legacy and current_app._get_current_object() is funnelapp:
                 with app.app_context(), app.test_request_context():
-                    return redirect(clview.obj.url_for(_external=True), code=303)
-        return f(clview, **kwargs)
+                    return redirect(classview.obj.url_for(classview.current_handler.name, _external=True), code=303)
+        return f(classview, **kwargs)
     return decorated_function
