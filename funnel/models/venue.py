6748eb99a5f5eac3774e718646e1e16ef5d230bc
Bibhas <me@bibhas.in>
2019-02-07 10:56:46 +0530

    added sequence to venue and venue room

--- funnel/models/venue.py
+++ funnel/models/venue.py
@@ -14,7 +14,7 @@ class Venue(UuidMixin, BaseScopedNameMixin, CoordinatesMixin, db.Model):
 
     project_id = db.Column(None, db.ForeignKey('project.id'), nullable=False)
     project = db.relationship(Project,
-        backref=db.backref('venues', lazy='dynamic', cascade='all, delete-orphan', order_by='Venue.name'))
+        backref=db.backref('venues', lazy='dynamic', cascade='all, delete-orphan', order_by='Venue.seq'))
     parent = db.synonym('project')
     description = MarkdownColumn('description', default=u'', nullable=False)
     address1 = db.Column(db.Unicode(160), default=u'', nullable=False)
@@ -24,6 +24,8 @@ class Venue(UuidMixin, BaseScopedNameMixin, CoordinatesMixin, db.Model):
     postcode = db.Column(db.Unicode(20), default=u'', nullable=False)
     country = db.Column(db.Unicode(2), default=u'', nullable=False)
 
+    seq = db.Column(db.Integer, nullable=False, default=0)
+
     __table_args__ = (db.UniqueConstraint('project_id', 'name'),)
 
 
@@ -32,10 +34,12 @@ class VenueRoom(BaseScopedNameMixin, db.Model):
 
     venue_id = db.Column(None, db.ForeignKey('venue.id'), nullable=False)
     venue = db.relationship(Venue,
-        backref=db.backref('rooms', cascade='all, delete-orphan', order_by='VenueRoom.name'))
+        backref=db.backref('rooms', cascade='all, delete-orphan', order_by='VenueRoom.seq'))
     parent = db.synonym('venue')
     description = MarkdownColumn('description', default=u'', nullable=False)
     bgcolor = db.Column(db.Unicode(6), nullable=False, default=u'229922')
+
+    seq = db.Column(db.Integer, nullable=False, default=0)
 
     scheduled_sessions = db.relationship("Session",
         primaryjoin='and_(Session.venue_room_id == VenueRoom.id, Session.scheduled)')
