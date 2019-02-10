49e91eb1d1d9c403978462af64f106bdf067b763
Bibhas <me@bibhas.in>
2019-02-11 00:24:26 +0530

    fixed draft model

--- funnel/models/draft.py
+++ funnel/models/draft.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from sqlalchemy_utils import UUIDType
-from . import db, JsonDict, NoIdMixin, TimestampMixin
+from . import db, JsonDict, NoIdMixin
 
 __all__ = ['Draft']
 
 
-class Draft(NoIdMixin, TimestampMixin, db.Model):
+class Draft(NoIdMixin, db.Model):
     """Store for autosaved, unvalidated drafts on behalf of other models"""
     __tablename__ = 'draft'
-    __uuid_primary_key__ = True
 
     table = db.Column(db.UnicodeText, primary_key=True)
     table_row_id = db.Column(UUIDType(binary=False), primary_key=True)
