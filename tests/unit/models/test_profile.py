e28be735dda8e4c71fd07bb98159203aa176f84f
Bibhas <me@bibhas.in>
2019-03-26 16:12:58 +0530

    removed hack from reserved list, added simple test

--- tests/unit/models/test_profile.py
+++ tests/unit/models/test_profile.py
@@ -17,3 +17,11 @@ def test_profile_urltype_invalid(test_client, test_db, new_profile):
     with pytest.raises(StatementError):
         test_db.session.commit()
     test_db.session.rollback()
+
+
+def test_reserved_name(test_client, test_db, new_profile):
+    new_profile.title = "Proposals"
+    new_profile.make_name()
+    # because `proposals` is in reserved name list, `make_name` will
+    # try to generate a name for it starting with suffixing it with 2.
+    assert new_profile.name == "proposals2"
