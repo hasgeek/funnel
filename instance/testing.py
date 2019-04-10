4f1bda4d1e32cca3984511f5adbac934465ceefe
Bibhas <me@bibhas.in>
2019-04-10 16:12:14 +0530

    logging in without dummy usermanager

--- instance/testing.py
+++ instance/testing.py
@@ -5,7 +5,7 @@ STATIC_SUBDOMAIN = 'static'
 LASTUSER_SERVER = 'https://auth.hasgeek.com'
 LASTUSER_CLIENT_ID = ''
 LASTUSER_CLIENT_SECRET = ''
-
+LASTUSER_USE_SESSIONS = False
 UPLOAD_FOLDER = '/tmp'
 TIMEZONE = 'Asia/Kolkata'
 RQ_LOW_URL = 'redis://localhost:6379/0'
--- tests/unit/models/test_user.py
+++ tests/unit/models/test_user.py
@@ -2,8 +2,10 @@
 
 
 class TestUser(object):
-    def test_admin_role(self, test_client, test_db, new_profile):
+    def test_admin_role(self, test_client, test_db, new_user, new_profile):
+        with test_client.session_transaction() as session:
+            session['lastuser_userid'] = new_user.userid
         with test_client as c:
             rv = c.get('/usertest')
-            assert rv.data == u"testuser"
+            assert rv.data == new_user.username
             assert new_profile.current_roles.admin
