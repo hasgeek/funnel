d403b22a840ac202d690e1a90621f00f462b2de8
Bibhas <me@bibhas.in>
2019-04-16 17:24:49 +0530

    replaced labelset views and form

--- funnel/forms/proposal.py
+++ funnel/forms/proposal.py
@@ -3,62 +3,25 @@
 from baseframe import __
 import baseframe.forms as forms
 from flask import g
-from baseframe.forms.sqlalchemy import QuerySelectField
-from ..models import Project, Profile, Label, Labelset
+from baseframe.forms.sqlalchemy import QuerySelectField, QuerySelectMultipleField
+from ..models import Project, Profile, Label
 
 __all__ = ['TransferProposal', 'ProposalForm', 'ProposalTransitionForm',
-    'ProposalMoveForm', 'get_proposal_form', 'ProposalLabelsetBaseForm']
+    'ProposalMoveForm', 'ProposalLabelsForm']
 
 
 class TransferProposal(forms.Form):
     userid = forms.UserSelectField(__("Transfer to"), validators=[forms.validators.DataRequired()])
 
 
-class ProposalLabelsetBaseForm(forms.Form):
-    """
-    This base form provides the `set_queries()` and `populate_obj_labels` methods
-    that lets you fill a proposal form with the labels and then save them.
+class ProposalLabelsForm(forms.Form):
+    labels = QuerySelectMultipleField(__("Labels"), get_label='title', get_pk=lambda l: l.name)
 
-    Any Proposal form that needs to show labels, need to inherit this class.
-    """
     def set_queries(self):
-        if self.edit_parent is not None:
-            # Fill up the choices for the labelsets
-            for labelset in self.edit_parent.labelsets:
-                labelset_field = getattr(self, labelset.form_name)
-                labelset_field.choices = [(l.name, l.title) for l in labelset.labels]
-                if self.edit_obj is not None:
-                    # If it's an edit form, select the proper label for each labelset
-                    labels_data = set(self.edit_obj.labels).intersection(set(labelset.labels))
-                    data = labels_data.pop().name if len(labels_data) == 1 else [l.name for l in labels_data]
-                    if labelset_field.data == 'None' and data:
-                        labelset_field.data = data
-
-    def populate_obj_labels(self, proposal):
-        """
-        Assign the appropriate labels to the proposal
-        """
-        labelset_keys = [key for key in self.data.keys() if key.startswith('labelset_')]
-        for key in labelset_keys:
-            labelset = Labelset.query.filter_by(form_name=key, project=proposal.project).first()
-            if labelset.radio_mode:
-                # in case of RadioField, self.data.get(key) should be a single value
-                new_label = Label.query.filter_by(labelset=labelset, name=self.data.get(key)).first()
-                proposal.assign_label(new_label)
-            else:
-                # FIXME: Move this part inside model?
-                existing_labels = set(labelset.labels).intersection(set(proposal.labels))
-                # in case of MultiSelectField, self.data.get(key) should be a list
-                label_names = self.data.get(key)
-                new_labels = Label.query.filter(Label.labelset == labelset, Label.name.in_(label_names)).all()
-                removed_labels = existing_labels.difference(set(new_labels))
-                for rlabel in removed_labels:
-                    proposal.labels.remove(rlabel)
-                for nlabel in new_labels:
-                    proposal.assign_label(nlabel)
+        self.labels.query = self.edit_parent.labels
 
 
-class ProposalForm(ProposalLabelsetBaseForm):
+class ProposalForm(forms.Form):
     speaking = forms.RadioField(__("Are you speaking?"), coerce=int,
         choices=[(1, __(u"I will be speaking")),
                  (0, __(u"I’m proposing a topic for someone to speak on"))])
@@ -93,6 +56,8 @@ class ProposalForm(ProposalLabelsetBaseForm):
     location = forms.StringField(__("Your location"), validators=[forms.validators.DataRequired(), forms.validators.Length(max=80)],
         description=__("Your location, to help plan for your travel if required"))
 
+    labels = QuerySelectMultipleField(__("Labels"), get_label='title', get_pk=lambda l: l.name)
+
     def __init__(self, *args, **kwargs):
         super(ProposalForm, self).__init__(*args, **kwargs)
         project = kwargs.get('parent')
@@ -105,28 +70,8 @@ class ProposalForm(ProposalLabelsetBaseForm):
         if project.proposal_part_b.get('hint'):
             self.description.description = project.proposal_part_b.get('hint')
 
-
-def get_proposal_form(base_form_class, *args, **kwargs):
-    """
-    Takes a proposal form class as base form and adds the labelset fields to it.
-    Dynamic fields can only be added to the form class and not instance. Hence this.
-    Any form that has `obj=<proposal_object>` or `parent=<proposal_parent_obj>`
-    passed to it, can be used with this function.
-    `parent` kwarg must be provided, otherwise the labelsets wont be added to the form.
-    """
-    if 'parent' in kwargs:
-        # we need parent project to be able to handle labelsets
-        project = kwargs.get('parent')
-        for labelset in project.labelsets:
-            ls_name = labelset.form_name
-            if not hasattr(base_form_class, ls_name):
-                if labelset.restricted and not set(project.current_roles).intersection({'admin', 'reviewer'}):
-                    continue
-                FieldType = forms.RadioField if labelset.radio_mode else forms.SelectMultipleField
-                validators = [forms.validators.DataRequired()] if labelset.required else []
-                setattr(base_form_class, ls_name, FieldType(labelset.title, validators=validators,
-                    choices=[], description=labelset.description))
-    return base_form_class(*args, **kwargs)
+    def set_queries(self):
+        self.labels.query = self.edit_parent.labels
 
 
 class ProposalTransitionForm(forms.Form):
