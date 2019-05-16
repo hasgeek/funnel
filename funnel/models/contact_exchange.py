500cd39595b1bdf9eb2d756a7a08859332909190
Kiran Jonnalagadda <kiran@hasgeek.com>
2019-05-16 17:00:14 +0530

    Add cascades for contact_exchange foreign keys

--- funnel/models/contact_exchange.py
+++ funnel/models/contact_exchange.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from . import db, TimestampMixin
+from .user import User
+from .project import Project
+from .event import Participant
 
 __all__ = ['ContactExchange']
 
@@ -10,6 +13,9 @@ class ContactExchange(TimestampMixin, db.Model):
     Model to track who scanned whose badge, at which event.
     """
     __tablename__ = 'contact_exchange'
-    user_id = db.Column(None, db.ForeignKey('user.id'), primary_key=True)
-    project_id = db.Column(None, db.ForeignKey('project.id'), primary_key=True)
-    participant_id = db.Column(None, db.ForeignKey('participant.id'), primary_key=True)
+    user_id = db.Column(None, db.ForeignKey('user.id', ondelete='CASCADE'), primary_key=True)
+    user = db.relationship(User, backref=db.backref('scanned_contacts', lazy='dynamic', passive_deletes=True))
+    project_id = db.Column(None, db.ForeignKey('project.id', ondelete='CASCADE'), primary_key=True)
+    project = db.relationship(Project)
+    participant_id = db.Column(None, db.ForeignKey('participant.id', ondelete='CASCADE'), primary_key=True)
+    participant = db.relationship(Participant, backref=db.backref('scanned_contacts', passive_deletes=True))
