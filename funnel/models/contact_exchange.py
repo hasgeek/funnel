75d29f58ddb90f9afb11a03c705d27e710d18adb
Vidya Ramakrishnan <vidya@hasgeek.in>
2019-06-03 23:49:53 +0530

    Add support to scan attendee badge for contact exchange (#424)

--- funnel/models/contact_exchange.py
+++ funnel/models/contact_exchange.py
@@ -2,7 +2,6 @@
 
 from . import db, TimestampMixin
 from .user import User
-from .project import Project
 from .event import Participant
 
 __all__ = ['ContactExchange']
@@ -13,9 +12,11 @@ class ContactExchange(TimestampMixin, db.Model):
     Model to track who scanned whose badge, at which event.
     """
     __tablename__ = 'contact_exchange'
-    user_id = db.Column(None, db.ForeignKey('user.id', ondelete='CASCADE'), primary_key=True)
-    user = db.relationship(User, backref=db.backref('scanned_contacts', lazy='dynamic', passive_deletes=True))
-    project_id = db.Column(None, db.ForeignKey('project.id', ondelete='CASCADE'), primary_key=True)
-    project = db.relationship(Project)
-    participant_id = db.Column(None, db.ForeignKey('participant.id', ondelete='CASCADE'), primary_key=True)
-    participant = db.relationship(Participant, backref=db.backref('scanned_contacts', passive_deletes=True))
+    user_id = db.Column(None, db.ForeignKey('user.id', ondelete='CASCADE'),
+        primary_key=True)
+    user = db.relationship(User,
+        backref=db.backref('scanned_contacts', lazy='dynamic', passive_deletes=True))
+    participant_id = db.Column(None, db.ForeignKey('participant.id', ondelete='CASCADE'),
+        primary_key=True, index=True)
+    participant = db.relationship(Participant,
+        backref=db.backref('scanned_contacts', passive_deletes=True))
--- funnel/views/participant.py
+++ funnel/views/participant.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
-from flask import flash, redirect, render_template, request, g, url_for, jsonify, make_response, current_app
-from datetime import timedelta
+from flask import flash, redirect, render_template, request, url_for, jsonify, make_response
 from sqlalchemy.exc import IntegrityError
 from baseframe import _
 from baseframe import forms
 from baseframe.forms import render_form
-from coaster.views import load_models, requestargs, route, requires_permission, UrlForView, ModelView
-from coaster.utils import midnight_to_utc, getbool, utcnow
+from coaster.views import load_models, route, requires_permission, UrlForView, ModelView
+from coaster.utils import getbool
 from .. import app, funnelapp, lastuser
-from ..models import (db, Profile, Project, Attendee, ProjectRedirect, Participant, Event, ContactExchange, SyncTicket)
+from ..models import db, Profile, Project, Attendee, ProjectRedirect, Participant, Event, SyncTicket
 from ..forms import ParticipantForm
 from ..views.helpers import mask_email
 from funnel.util import split_name, format_twitter_handle, make_qrcode
@@ -125,39 +124,6 @@ def participant_edit(profile, project, participant):
         flash(_(u"Your changes have been saved"), 'info')
         return redirect(project.url_for('admin'), code=303)
     return render_form(form=form, title=_(u"Edit Participant"), submit=_(u"Save changes"))
-
-
-@app.route('/<profile>/<project>/participant', methods=['GET', 'POST'])
-@funnelapp.route('/<project>/participant', methods=['GET', 'POST'], subdomain='<profile>')
-@lastuser.requires_login
-@load_models(
-    (Profile, {'name': 'profile'}, 'g.profile'),
-    ((Project, ProjectRedirect), {'name': 'project', 'profile': 'profile'}, 'project'),
-    permission='view')
-@requestargs('puk', 'key')
-def participant(profile, project, puk, key):
-    """
-    Endpoint for contact exchange.
-
-    TODO: The GET method to this endpoint is deprecated and will be removed by 1st September, 2018
-    """
-    if project.date_upto:
-        if midnight_to_utc(project.date_upto + timedelta(days=1), project.timezone) < utcnow():
-            return jsonify(message=u"This event has concluded", code=401)
-    participant = Participant.query.filter_by(puk=puk, project=project).first()
-    if not participant:
-        return jsonify(message=u"Participant not found", code=404)
-    elif participant.key == key:
-        try:
-            contact_exchange = ContactExchange(user_id=g.user.id, participant_id=participant.id, project_id=project.id)
-            db.session.add(contact_exchange)
-            db.session.commit()
-        except IntegrityError:
-            current_app.logger.warning(u"Contact Exchange already present")
-            db.session.rollback()
-        return jsonify(participant=participant_data(participant, project.id, full=True))
-    else:
-        return jsonify(message=u"Unauthorized contact exchange", code=401)
 
 
 @app.route('/<profile>/<project>/participant/<participant_id>/badge')
