543cd296db860d9253c302a6964a3aee5dd5c8d8
Vidya Ramakrishnan <vidya@hasgeek.in>
2019-06-04 21:01:23 +0530

    Add support to download scanned contact as vcard

---
+++ funnel/assets/js/contact.js
@@ -0,0 +1,16 @@
+var vCardsJS = require('vcards-js');
+
+$(() => {
+  window.HasGeek.downloadVcard = function (element, fullname, email, phone, company) {
+    let vCard = vCardsJS();
+    let lastName;
+    [vCard.firstName, ...lastName] = fullname.split(' ');
+    vCard.lastName = lastName.join(' ');
+    vCard.email = email;
+    vCard.cellPhone = phone;    
+    vCard.organization = company;
+    element.setAttribute('href', 'data:text/x-vcard;charset=utf-8,' + encodeURIComponent(vCard.getFormattedString()));
+    element.setAttribute('download', `${vCard.firstName}.vcf`);
+  };
+});
+
--- funnel/assets/webpack.config.js
+++ funnel/assets/webpack.config.js
@@ -50,6 +50,7 @@ module.exports = {
     "event": path.resolve(__dirname, "js/event.js"),
     "scan_badge": path.resolve(__dirname, "js/scan_badge.js"),
     "scan_contact": path.resolve(__dirname, "js/scan_contact.js"),
+    "contact": path.resolve(__dirname, "js/contact.js"),
   },
   output: {
     path: path.resolve(__dirname,  "../static/build"),
