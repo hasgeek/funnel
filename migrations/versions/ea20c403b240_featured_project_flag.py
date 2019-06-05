0348b007058d25a60b4a2ac91c52936f87cec903
Kiran Jonnalagadda <kiran@hasgeek.com>
2019-06-05 12:44:29 +0530

    Featured project flag, for #433

---
+++ migrations/versions/ea20c403b240_featured_project_flag.py
@@ -0,0 +1,24 @@
+"""Featured project flag
+
+Revision ID: ea20c403b240
+Revises: c38fa391613f
+Create Date: 2019-06-05 12:38:15.928874
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'ea20c403b240'
+down_revision = 'c38fa391613f'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.add_column('project', sa.Column('featured', sa.Boolean(),
+        nullable=False, server_default=sa.sql.expression.false()))
+    op.alter_column('project', 'featured', server_default=None)
+
+
+def downgrade():
+    op.drop_column('project', 'featured')
