463f28d019f0edb7b73c5d229264219475087c39
Bibhas <me@bibhas.in>
2019-10-30 10:55:16 +0530

    added migration for admin team and checkin team

--- migrations/versions/8297ea2c9645_data_migrate_teams_to_membership.py
+++ migrations/versions/8297ea2c9645_data_migrate_teams_to_membership.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""data migrate teams to membership
+"""data migrate checkin teams to membership
 
 Revision ID: 8297ea2c9645
 Revises: 8cb98498a659
@@ -21,7 +21,10 @@ import sqlalchemy as sa  # NOQA
 from funnel.models.membership import MEMBERSHIP_RECORD_TYPE
 
 project = table(
-    'project', column('id', sa.Integer()), column('checkin_team_id', sa.Integer())
+    'project',
+    column('id', sa.Integer()),
+    column('checkin_team_id', sa.Integer()),
+    column('admin_team_id', sa.Integer()),
 )
 
 users_teams = table(
@@ -40,72 +43,79 @@ project_crew_membership = table(
     column('is_concierge', sa.Boolean()),
     column('is_usher', sa.Boolean()),
     column('granted_by_id', sa.Integer()),
+    column('revoked_by_id', sa.Integer()),
     column('granted_at', sa.TIMESTAMP(timezone=True)),
     column('record_type', sa.Integer()),
     column('created_at', sa.TIMESTAMP(timezone=True)),
     column('updated_at', sa.TIMESTAMP(timezone=True)),
+    column('revoked_at', sa.TIMESTAMP(timezone=True)),
 )
+
+
+def get_existing_membership(conn, user_id, project_id):
+    return conn.execute(
+        sa.select([project_crew_membership.c.id])
+        .where(
+            sa.and_(
+                project_crew_membership.c.project_id == project_id,
+                project_crew_membership.c.user_id == user_id,
+                project_crew_membership.c.revoked_at == None,  # NOQA
+            )
+        )
+        .returning(
+            project_crew_membership.c.id,
+            project_crew_membership.c.is_editor,
+            project_crew_membership.c.is_concierge,
+            project_crew_membership.c.is_usher,
+        )
+    ).first()
 
 
 def upgrade():
     conn = op.get_bind()
-    projects = conn.execute(sa.select([project.c.id, project.c.checkin_team_id]))
-    for project_id, checkin_team_id in projects:
+    projects = conn.execute(
+        sa.select([project.c.id, project.c.checkin_team_id, project.c.admin_team_id])
+    )
+    for project_id, checkin_team_id, admin_team_id in projects:
+        # migrate checkin team
         checkin_team_users = conn.execute(
             sa.select([users_teams.c.user_id, users_teams.c.created_at]).where(
                 users_teams.c.team_id == checkin_team_id
             )
         )
-        for user_id, created_at in checkin_team_users:
-            existing_membership = conn.execute(
-                sa.select([project_crew_membership.c.id]).where(
-                    sa.and_(
-                        project_crew_membership.c.project_id == project_id,
-                        project_crew_membership.c.user_id == user_id,
-                    )
+        checkin_dict = {
+            user_id: created_at for user_id, created_at in checkin_team_users
+        }
+        admin_team_users = conn.execute(
+            sa.select([users_teams.c.user_id, users_teams.c.created_at]).where(
+                users_teams.c.team_id == admin_team_id
+            )
+        )
+        admin_dict = {user_id: created_at for user_id, created_at in admin_team_users}
+        all_users = set(checkin_dict.keys() + admin_dict.keys())
+
+        for user_id in all_users:
+            conn.execute(
+                project_crew_membership.insert().values(
+                    {
+                        'id': uuid4(),
+                        'project_id': project_id,
+                        'user_id': user_id,
+                        'is_editor': user_id in admin_dict,
+                        'is_concierge': user_id in admin_dict,
+                        'is_usher': user_id in checkin_dict,
+                        'granted_by_id': None,
+                        'granted_at': checkin_dict[user_id]
+                        if user_id in checkin_dict
+                        else admin_dict[user_id],
+                        'record_type': MEMBERSHIP_RECORD_TYPE.DIRECT_ADD,
+                        'created_at': sa.func.now(),
+                        'updated_at': sa.func.now(),
+                    }
                 )
-            ).first()
-            if existing_membership is None:
-                conn.execute(
-                    project_crew_membership.insert()
-                    .values(
-                        {
-                            'id': uuid4(),
-                            'project_id': project_id,
-                            'user_id': user_id,
-                            'is_editor': False,
-                            'is_concierge': False,
-                            'is_usher': True,
-                            'granted_by_id': None,
-                            'granted_at': created_at,
-                            'record_type': MEMBERSHIP_RECORD_TYPE.DIRECT_ADD,
-                            'created_at': sa.func.now(),
-                            'updated_at': sa.func.now(),
-                        }
-                    )
-                    .returning(project_crew_membership.c.id)
-                )
+            )
 
 
 def downgrade():
     conn = op.get_bind()
-    projects = conn.execute(sa.select([project.c.id, project.c.checkin_team_id]))
-    for project_id, checkin_team_id in projects:
-        checkin_team_users = conn.execute(
-            sa.select([users_teams.c.user_id, users_teams.c.created_at]).where(
-                users_teams.c.team_id == checkin_team_id
-            )
-        )
-        for user_id, created_at in checkin_team_users:
-            conn.execute(
-                project_crew_membership.delete().where(
-                    sa.and_(
-                        project_crew_membership.c.project_id == project_id,
-                        project_crew_membership.c.user_id == user_id,
-                        project_crew_membership.c.is_editor == False,
-                        project_crew_membership.c.is_concierge == False,
-                        project_crew_membership.c.is_usher == True,
-                        project_crew_membership.c.granted_by_id == None,  # NOQA
-                    )
-                )
-            )
+    conn.execute(project_crew_membership.delete())
