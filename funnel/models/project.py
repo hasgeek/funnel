4faf40bb6dbef48b5344cbb513a0b3feff79f798
Vidya Ramakrishnan <vidya@hasgeek.in>
2019-12-20 17:02:11 +0530

    Show event start and end time on hover of dates on the calendar widget

--- funnel/models/project.py
+++ funnel/models/project.py
@@ -252,7 +252,7 @@ class Project(UuidMixin, BaseScopedNameMixin, db.Model):
                 'primary_venue',
                 'livestream_urls',
             },
-            'call': {'url_for', 'current_sessions'},
+            'call': {'url_for', 'current_sessions', 'is_saved_by'},
         }
     }
 
