709d9c7d737ab1e364879ca50504ede25134b541
Vidya Ramakrishnan <vidya@hasgeek.in>
2020-01-02 11:56:44 +0530

    Preserve login cookies

--- funnel/assets/cypress/integration/00_login.js
+++ funnel/assets/cypress/integration/00_login.js
@@ -1,6 +1,12 @@
 describe('Login', function() {
   const { admin } = require('../fixtures/user.js');
 
+  beforeEach(function() {
+    Cypress.Cookies.defaults({
+      whitelist: ['lastuser', 'root_session'],
+    });
+  });
+
   it('Create a new project', function() {
     cy.login('https://auth.hasgeek.com', admin.username, admin.password);
   });
