06b3d7a8bd82d283924a60961be1eb27ef07e874
Vidya Ramakrishnan <vidya@hasgeek.in>
2020-01-07 08:12:33 +0530

    Update init db

--- tests/e2e/frontend_tests_initdb.py
+++ tests/e2e/frontend_tests_initdb.py
@@ -1,20 +1,12 @@
 # -*- coding: utf-8 -*-
 from funnel import app
-from funnel.models import User, db
+from funnel.models import db
 
 
 def init_models():
     with app.app_context():
         db.drop_all()
         db.create_all()
-
-        u = User(username="admin-cypress", email="cypressuser1@gmail.com")
-        db.session.add(u)
-        db.session.commit()
-
-        u = User(username="member-user", email="cypressmemberuser@gmail.com")
-        db.session.add(u)
-        db.session.commit()
 
 
 if __name__ == "__main__":
