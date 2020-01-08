4b582e177572c68d5201f832e1d5d15edc66aee5
Vidya Ramakrishnan <vidya@hasgeek.in>
2020-01-08 17:10:05 +0530

    Remove about hasgeek section

--- funnel/assets/js/index.js
+++ funnel/assets/js/index.js
@@ -1,32 +1,7 @@
 import { SaveProject } from './util';
 
 $(() => {
-  window.HasGeek.HomeInit = function(config) {
-    function getRandomNumber() {
-      return Math.floor(
-        Math.random() * Math.floor(config.hgBannerImgList.length)
-      );
-    }
-    function getImagePath(num) {
-      return config.ImgFolderPath + config.hgBannerImgList[num];
-    }
-
-    // Random display of HasGeek banner
-    if (config.hgBannerImgList && config.hgBannerImgList.length) {
-      let randomNumList = [];
-      do {
-        let randomNum = getRandomNumber();
-        if (randomNum > 0 && randomNumList.indexOf(randomNum) === -1) {
-          randomNumList.push(randomNum);
-        }
-      } while (randomNumList.length < 4);
-      $('.js-hg-banner').each(function(index) {
-        $(this).attr('src', getImagePath(randomNumList[index]));
-      });
-    } else {
-      $('.js-hg-banner').attr('src', config.defaultBanner);
-    }
-
+  window.HasGeek.HomeInit = function() {
     // Expand CFP section
     $('.jquery-show-all').click(function showAll(event) {
       event.preventDefault();
