cb3ee628c884881a4574498210db492263e00ad7
Vidya Ramakrishnan <vidya@hasgeek.in>
2020-01-31 08:55:47 +0530

    Add tests to verify livestream and add confirmed proposal to schedule. Change wait timer of login to wait for login xhr response.

---
+++ funnel/assets/cypress/integration/16_verifyatteeList.js
@@ -0,0 +1,16 @@
+describe('Verify attendee list', function() {
+  const { admin, user } = require('../fixtures/user.js');
+  const project = require('../fixtures/project.json');
+
+  it('Verify list of attendees who have responded yes to attending a project', function() {
+    cy.relogin('/testcypressproject');
+    cy.get('[data-cy-project="' + project.title + '"]')
+      .first()
+      .click();
+    cy.location('pathname').should('contain', project.url);
+    cy.get('a[data-cy="see-responses"]').click();
+    cy.location('pathname').should('contain', 'rsvp_list');
+    cy.get('[data-cy-status="yes"]').click();
+    cy.get('[data-cy="user"]').should('contain', user.username);
+  });
+});
