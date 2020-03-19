ed8fa30270ab1fe355b4ef670912043878d85647
Bibhas <me@bibhas.in>
2020-03-19 14:07:46 +0530

    returning dict for proposal/session.video

--- funnel/models/mixins.py
+++ funnel/models/mixins.py
@@ -9,29 +9,22 @@ class VideoMixin:
     video_source = db.Column(db.UnicodeText, nullable=True)
 
     @property
-    def video_url(self):
-        if self.video_source == 'youtube':
-            return 'https://www.youtube.com/watch?v={video_id}'.format(
-                video_id=self.video_id
-            )
-        elif self.video_source == 'vimeo':
-            return 'https://vimeo.com/{video_id}'.format(video_id=self.video_id)
-        elif self.video_source == 'raw':
-            return self.video_id
-        else:
-            return None
-
-    @property
-    def embeddable_video_url(self):
-        if self.video_source == 'youtube':
-            return '//www.youtube.com/embed/{video_id}?wmode=transparent&showinfo=0&rel=0&autohide=0&autoplay=0&enablejsapi=1&version=3'.format(
-                video_id=self.video_id
-            )
-        elif self.video_source == 'vimeo':
-            return '//player.vimeo.com/video/{video_id}?api=1&player_id=vimeoplayer'.format(
-                video_id=self.video_id
-            )
-        elif self.video_source == 'raw':
-            return self.video_id
-        else:
-            return None
+    def video(self):
+        data = None
+        if self.video_source is not None:
+            data = {'source': self.video_source, 'id': self.video_id}
+            if data['source'] == 'youtube':
+                data[
+                    'url'
+                ] = '//www.youtube.com/embed/{video_id}?wmode=transparent&showinfo=0&rel=0&autohide=0&autoplay=0&enablejsapi=1&version=3'.format(
+                    video_id=self.video_id
+                )
+            elif data['source'] == 'vimeo':
+                data[
+                    'url'
+                ] = '//player.vimeo.com/video/{video_id}?api=1&player_id=vimeoplayer'.format(
+                    video_id=self.video_id
+                )
+            else:
+                data['url'] = self.video_id
+        return data
