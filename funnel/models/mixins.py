e11140e8cb89934d8b52eda90edcdf3b142d2783
Bibhas <me@bibhas.in>
2020-03-20 16:24:18 +0530

    returning thumbnail url created from ID

--- funnel/models/mixins.py
+++ funnel/models/mixins.py
@@ -11,20 +11,39 @@ class VideoMixin:
     @property
     def video(self):
         data = None
-        if self.video_source is not None:
-            data = {'source': self.video_source, 'id': self.video_id}
-            if data['source'] == 'youtube':
-                data[
-                    'url'
-                ] = '//www.youtube.com/embed/{video_id}?wmode=transparent&showinfo=0&rel=0&autohide=0&autoplay=0&enablejsapi=1&version=3'.format(
-                    video_id=self.video_id
-                )
-            elif data['source'] == 'vimeo':
-                data[
-                    'url'
-                ] = '//player.vimeo.com/video/{video_id}?api=1&player_id=vimeoplayer'.format(
-                    video_id=self.video_id
-                )
-            else:
-                data['url'] = self.video_id
+        if self.video_source:
+            data = {
+                'source': self.video_source,
+                'id': self.video_id,
+                'url': self.video_url,
+                'thumbnail': self.thumbnail_url,
+            }
         return data
+
+    @property
+    def video_url(self):
+        if self.video_source:
+            if self.video_source == 'youtube':
+                return '//www.youtube.com/embed/{video_id}?wmode=transparent&showinfo=0&rel=0&autohide=0&autoplay=0&enablejsapi=1&version=3'.format(
+                    video_id=self.video_id
+                )
+            elif self.video_source == 'vimeo':
+                return '//player.vimeo.com/video/{video_id}?api=1&player_id=vimeoplayer'.format(
+                    video_id=self.video_id
+                )
+            elif self.video_source == 'raw':
+                return self.video_id
+        return None
+
+    @property
+    def thumbnail_url(self):
+        if self.video_source:
+            if self.video_source == 'youtube':
+                return '//i.ytimg.com/vi/{video_id}/mqdefault.jpg'.format(
+                    video_id=self.video_id
+                )
+            elif self.video_source == 'vimeo':
+                return '//i.vimeocdn.com/video/{video_id}_200x150.jpg'.format(
+                    video_id=self.video_id
+                )
+        return None
