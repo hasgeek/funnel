6ac0fe2bc63c552248244abf2afed380621ca1af
Kiran Jonnalagadda <kiran@hasgeek.com>
2020-04-08 00:48:10 +0530

    Reformat files using pre-commit hooks

--- lastuser_oauth/static/js/app.js
+++ lastuser_oauth/static/js/app.js
@@ -1,6 +1,6 @@
 // Detect timezone
 $(function() {
   if ($.cookie('timezone') === null) {
-    $.cookie('timezone', jstz.determine().name(), {path: '/'});
+    $.cookie('timezone', jstz.determine().name(), { path: '/' });
   }
 });
