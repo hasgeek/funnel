53879cb56bc3727cc5ee6d065b1b8a098851bd95
Vidya Ramakrishnan <vidya@hasgeek.in>
2020-04-17 13:03:22 +0530

    Fix spacing in session popup. Fix the edit url on the proposal page video

--- runfrontendtests.sh
+++ runfrontendtests.sh
@@ -8,7 +8,7 @@ SERVER_PID=$!
 ./rq.sh &
 RQ_PID=$!
 cd funnel/assets
-npx cypress run --browser chrome  --record --key $RECORD_KEY
+npx cypress run --browser chrome
 kill $SERVER_PID
 kill $RQ_PID
 python -m tests.e2e.frontend_tests_dropdb
