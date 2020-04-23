44a830cbe83d2fc24daaf13123f8044b79d39c56
Bibhas <me@bibhas.in>
2020-04-23 16:34:27 +0530

    fix existing membership tests

--- tests/unit/models/test_profile.py
+++ tests/unit/models/test_profile.py
@@ -5,25 +5,26 @@ from sqlalchemy.exc import StatementError
 from furl import furl
 import pytest
 
+from funnel.models import Profile
+
 
 class TestProfile(object):
-    def test_profile_urltype_valid(self, test_client, test_db, new_profile):
-        new_profile.logo_url = "https://hasgeek.com"
-        test_db.session.add(new_profile)
+    def test_profile_urltype_valid(self, test_client, test_db, new_organization):
+        profile = Profile.query.filter_by(id=new_organization.profile.id).first()
+        assert profile.name == 'test-org'
+        profile.logo_url = "https://hasgeek.com"
+        test_db.session.add(profile)
         test_db.session.commit()
-        assert isinstance(new_profile.logo_url, furl)
-        assert new_profile.logo_url.url == "https://hasgeek.com"
+        assert isinstance(profile.logo_url, furl)
+        assert profile.logo_url.url == "https://hasgeek.com"
 
-    def test_profile_urltype_invalid(self, test_client, test_db, new_profile):
-        new_profile.logo_url = "noturl"
-        test_db.session.add(new_profile)
+    def test_profile_urltype_invalid(self, test_client, test_db, new_organization):
+        profile = Profile.query.filter_by(id=new_organization.profile.id).first()
+        profile.logo_url = "noturl"
+        test_db.session.add(profile)
         with pytest.raises(StatementError):
             test_db.session.commit()
         test_db.session.rollback()
 
-    def test_reserved_name(self, test_client, test_db, new_profile):
-        new_profile.title = "Proposals"
-        new_profile.make_name()
-        # because `proposals` is in reserved name list, `make_name` will
-        # try to generate a name for it starting with suffixing it with 2.
-        assert new_profile.name == "proposals2"
+    def test_validate_name(self, test_client, test_db, new_organization):
+        assert Profile.validate_name_candidate(new_organization.profile.name) == 'org'
