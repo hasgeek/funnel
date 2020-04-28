b50b2a21762f1d582063cc3344f655517b0a34aa
Vidya Ramakrishnan <vidya@hasgeek.in>
2020-04-28 18:15:11 +0530

    Hide ticketing widget once the project ends (#609)
    * Hide ticketing widget once the project ends
    
    * Fix e2e tests

--- funnel/assets/cypress/integration/15_verifyConciergeRoles.js
+++ funnel/assets/cypress/integration/15_verifyConciergeRoles.js
@@ -18,7 +18,7 @@ describe('Verify roles of concierge', function() {
     cy.get('a[data-cy="edit-schedule"]').should('not.exist');
     cy.get('a[data-cy="manage-labels"]').should('not.exist');
     cy.get('a[data-cy="setup-events"]').should('exist');
-    cy.get('a[data-cy="scan-checkin"]').should('not.exist');
+    cy.get('a[data-cy="scan-checkin"]').should('exist');
     cy.get('a[data-cy="download-csv"]').should('exist');
     cy.get('a[data-cy="download-json"]').should('exist');
     cy.get('a[data-cy="download-schedule-json"]').should('exist');
