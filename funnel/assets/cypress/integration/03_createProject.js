ab6ecb123a9e1bdc058e6e6b1c5324adbdfd495a
Vidya Ramakrishnan <vidya@hasgeek.in>
2020-04-30 16:03:11 +0530

    Add admin buttons in the pages

--- funnel/assets/cypress/integration/03_createProject.js
+++ funnel/assets/cypress/integration/03_createProject.js
@@ -1,15 +1,14 @@
-describe('Project', function () {
+describe('Project', function() {
   const admin = require('../fixtures/user.json').admin;
   const profile = require('../fixtures/profile.json');
   const project = require('../fixtures/project.json');
 
-  it('Create a new project', function () {
+  it('Create a new project', function() {
     cy.login('/' + profile.title, admin.username, admin.password);
 
     cy.get('a[data-cy="new-project"]').click();
     cy.location('pathname').should('contain', '/new');
 
-    cy.get('#name').type(project.url);
     cy.get('#title').type(project.title);
     cy.get('#location').type(project.location);
     cy.get('#tagline').type(project.tagline);
@@ -17,7 +16,9 @@ describe('Project', function () {
       .find('.CodeMirror textarea')
       .type(project.description, { force: true });
     cy.get('#bg_image').type(project.bg_image);
-    cy.get('button').contains('Create project').click();
+    cy.get('button')
+      .contains('Create project')
+      .click();
     cy.location('pathname').should('contain', project.url);
 
     cy.title().should('include', project.title);
