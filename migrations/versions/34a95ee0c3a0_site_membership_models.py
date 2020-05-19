62905594e993c642105c6acf72b936d866808e24
Bibhas <github@bibhasdn.com>
2020-05-19 23:47:55 +0530

    Site membership (#639)
    * added site membership model
    
    * added site membership migration
    
    * added comment moderation template and views
    
    * added select all in comment search page
    
    * renamed form
    
    * deleting comments is functional
    
    * moved moderator properties in site membership
    
    * removed footer
    
    * flash message on comment delete
    
    * no template required for delete view
    
    * comment moderation form not needed
    
    * some extra spacing
    
    * added user flags to column property
    
    * added User.active_site_memberships
    
    * made select all button a checkbox
    
    * added site editor role
    
    * site_admin role is given by default
    
    * prevent form submit in case of cancel of confirm

---
+++ migrations/versions/34a95ee0c3a0_site_membership_models.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""site membership models
+
+Revision ID: 34a95ee0c3a0
+Revises: 887db555cca9
+Create Date: 2020-05-14 16:32:52.553441
+
+"""
+
+from alembic import op
+from sqlalchemy.dialects import postgresql
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = '34a95ee0c3a0'
+down_revision = '887db555cca9'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        'site_membership',
+        sa.Column('id', postgresql.UUID(), nullable=False),
+        sa.Column('granted_at', sa.TIMESTAMP(timezone=True), nullable=False),
+        sa.Column('revoked_at', sa.TIMESTAMP(timezone=True), nullable=True),
+        sa.Column('record_type', sa.Integer(), nullable=False),
+        sa.Column('is_comment_moderator', sa.Boolean(), nullable=False),
+        sa.Column('is_user_moderator', sa.Boolean(), nullable=False),
+        sa.Column('is_site_editor', sa.Boolean(), nullable=False),
+        sa.Column('user_id', sa.Integer(), nullable=False),
+        sa.Column('revoked_by_id', sa.Integer(), nullable=True),
+        sa.Column('granted_by_id', sa.Integer(), nullable=True),
+        sa.Column('created_at', sa.TIMESTAMP(timezone=True), nullable=False),
+        sa.Column('updated_at', sa.TIMESTAMP(timezone=True), nullable=False),
+        sa.CheckConstraint(
+            'is_comment_moderator IS true OR is_user_moderator IS true OR is_site_editor IS true',
+            name='site_membership_has_role',
+        ),
+        sa.ForeignKeyConstraint(['granted_by_id'], ['user.id'], ondelete='SET NULL'),
+        sa.ForeignKeyConstraint(['revoked_by_id'], ['user.id'], ondelete='SET NULL'),
+        sa.ForeignKeyConstraint(['user_id'], ['user.id'], ondelete='CASCADE'),
+        sa.PrimaryKeyConstraint('id'),
+    )
+    op.create_index(
+        'ix_site_membership_active',
+        'site_membership',
+        ['user_id'],
+        unique=True,
+        postgresql_where=sa.text('revoked_at IS NULL'),
+    )
+    op.create_index(
+        op.f('ix_site_membership_user_id'), 'site_membership', ['user_id'], unique=False
+    )
+
+
+def downgrade():
+    op.drop_index(op.f('ix_site_membership_user_id'), table_name='site_membership')
+    op.drop_index('ix_site_membership_active', table_name='site_membership')
+    op.drop_table('site_membership')
