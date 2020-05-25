4e578bde2868ba5275818ad30fd738c8bc962c18
Vidya Ramakrishnan <vidya@hasgeek.in>
2020-05-26 00:03:46 +0530

    crew page style fixes

--- funnel/views/membership.py
+++ funnel/views/membership.py
@@ -187,6 +187,7 @@ class OrganizationMembershipView(UrlChangeCheck, UrlForView, ModelView):
                 db.session.commit()
                 return {
                     'status': 'ok',
+                    'message': _("The member's roles has been updated"),
                     'memberships': [
                         membership.current_access()
                         for membership in self.obj.organization.active_admin_memberships
@@ -245,6 +246,7 @@ class OrganizationMembershipView(UrlChangeCheck, UrlForView, ModelView):
                     )
                 return {
                     'status': 'ok',
+                    'message': _("The member has been removed"),
                     'memberships': [
                         membership.current_access()
                         for membership in self.obj.organization.active_admin_memberships
@@ -262,13 +264,13 @@ class OrganizationMembershipView(UrlChangeCheck, UrlForView, ModelView):
 
         form_html = render_form(
             form=form,
-            title=_("Delete member"),
+            title=_("Remove member"),
             message=_(
                 "Are you sure you want to remove {member} from {profile} as an admin?"
             ).format(
                 member=self.obj.user.fullname, profile=self.obj.organization.title
             ),
-            submit=_("Delete"),
+            submit=_("Remove"),
             ajax=False,
             with_chrome=False,
         )
@@ -481,6 +483,7 @@ class ProjectCrewMembershipView(
                 db.session.commit()
                 return {
                     'status': 'ok',
+                    'message': _("The member's roles has been updated"),
                     'memberships': [
                         membership.current_access()
                         for membership in self.obj.project.active_crew_memberships
@@ -533,6 +536,7 @@ class ProjectCrewMembershipView(
                     )
                 return {
                     'status': 'ok',
+                    'message': _("The member has been removed"),
                     'memberships': [
                         membership.current_access()
                         for membership in self.obj.project.active_crew_memberships
@@ -543,11 +547,11 @@ class ProjectCrewMembershipView(
 
         form_html = render_form(
             form=form,
-            title=_("Delete member"),
+            title=_("Remove member"),
             message=_(
                 "Are you sure you want to remove {member} from the project?"
             ).format(member=self.obj.user.fullname),
-            submit=_("Delete"),
+            submit=_("Remove"),
             ajax=False,
             with_chrome=False,
         )
