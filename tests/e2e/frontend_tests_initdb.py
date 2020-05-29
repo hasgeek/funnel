d70d4d4ae54d59f37ed837680998582676fddb3a
Bibhas <github@bibhasdn.com>
2020-05-29 17:41:36 +0530

    added useremail for all test users

--- tests/e2e/frontend_tests_initdb.py
+++ tests/e2e/frontend_tests_initdb.py
@@ -6,30 +6,45 @@ def init_models():
     with app.test_request_context():
         db.drop_all()
         db.create_all()
+
         user_admin = User(username='admin-user', fullname='admin-user')
         user_admin._set_password('cypress129_ftz')
-        useradmin_email = UserEmail(email='adminuser@example.com', user=user_admin)
+        user_admin_email = UserEmail(email='adminuser@example.com', user=user_admin)
+
         user = User(username='member-user', fullname='member-user')
         user._set_password('cypress341_wer')
         user_email = UserEmail(email='memberuser@example.com', user=user)
+
         profile_owner = User(username='profile-cypress', fullname='profile-cypress')
         profile_owner._set_password('cypress123_st')
+        profile_owner_email = UserEmail(email='profileowner@example.com', user=user)
+
         concierge = User(username='concierge-user', fullname='concierge-user')
         concierge._set_password('cypress341_qwe')
+        concierge_email = UserEmail(email='concierge@example.com', user=user)
+
         usher = User(username='usher-cypress', fullname='usher-cypress')
         usher._set_password('cypress566_yut')
+        usher_email = UserEmail(email='usher@example.com', user=user)
+
         editor = User(username='editor-cypress', fullname='editor-cypress')
         editor._set_password('cypress900_ghy')
+        editor_email = UserEmail(email='editor@example.com', user=user)
+
         db.session.add_all(
             [
                 user_admin,
-                useradmin_email,
+                user_admin_email,
                 user,
                 user_email,
                 profile_owner,
+                profile_owner_email,
                 concierge,
+                concierge_email,
                 usher,
+                usher_email,
                 editor,
+                editor_email,
             ]
         )
         db.session.commit()
