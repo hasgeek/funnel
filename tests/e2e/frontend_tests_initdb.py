28ace23965c7e2542120057d04b6961f64034300
Bibhas <github@bibhasdn.com>
2020-05-29 17:19:26 +0530

    adding useremail for tests users

--- tests/e2e/frontend_tests_initdb.py
+++ tests/e2e/frontend_tests_initdb.py
@@ -1,5 +1,5 @@
 from funnel import app
-from funnel.models import User, db
+from funnel.models import User, UserEmail, db
 
 
 def init_models():
@@ -8,8 +8,10 @@ def init_models():
         db.create_all()
         user_admin = User(username='admin-user', fullname='admin-user')
         user_admin._set_password('cypress129_ftz')
+        useradmin_email = UserEmail(email='adminuser@example.com', user=user_admin)
         user = User(username='member-user', fullname='member-user')
         user._set_password('cypress341_wer')
+        user_email = UserEmail(email='memberuser@example.com', user=user)
         profile_owner = User(username='profile-cypress', fullname='profile-cypress')
         profile_owner._set_password('cypress123_st')
         concierge = User(username='concierge-user', fullname='concierge-user')
@@ -18,7 +20,18 @@ def init_models():
         usher._set_password('cypress566_yut')
         editor = User(username='editor-cypress', fullname='editor-cypress')
         editor._set_password('cypress900_ghy')
-        db.session.add_all([user_admin, user, profile_owner, concierge, usher, editor])
+        db.session.add_all(
+            [
+                user_admin,
+                useradmin_email,
+                user,
+                user_email,
+                profile_owner,
+                concierge,
+                usher,
+                editor,
+            ]
+        )
         db.session.commit()
 
 
