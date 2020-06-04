8d540410431ab11258a2d02f92085641d8646ea4
Bibhas <github@bibhasdn.com>
2020-06-04 13:53:52 +0530

    Users need to have at least one verified contact to become members or more than one to remove contacts (#689)
    Co-authored-by: Vidya Ramakrishnan <vidya@hasgeek.in>

--- funnel/assets/cypress/integration/00_addProfile.js
+++ funnel/assets/cypress/integration/00_addProfile.js
@@ -1,6 +1,7 @@
 describe('Profile', function() {
   const owner = require('../fixtures/user.json').owner;
   const profile = require('../fixtures/profile.json');
+  const hguser = require('../fixtures/user.json').hguser;
 
   it('Create a new profile', function() {
     cy.login('/', owner.username, owner.password);
@@ -17,5 +18,13 @@ describe('Profile', function() {
     cy.get('button')
       .contains('Next')
       .click();
+    cy.logout();
+
+    cy.login('/', hguser.username, hguser.password);
+    cy.visit('/organizations');
+    cy.get('a')
+      .contains('new organization')
+      .click();
+    cy.get('.alert--error').should('exist');
   });
 });
