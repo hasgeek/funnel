6b1a06050240f48dc7fe249734f2cf3f235aa891
Kiran Jonnalagadda <kiran@hasgeek.com>
2020-06-06 02:44:17 +0530

    Introduce blake2b hashes to replace SHA256 and MD5 (with matching digest sizes) (#704)

--- tests/unit/lastuser_core/test_model_client_ClientCredential.py
+++ tests/unit/lastuser_core/test_model_client_ClientCredential.py
@@ -19,7 +19,7 @@ class TestClientCredential(TestDatabaseFixture):
         assert cred.auth_client == auth_client
         assert isinstance(cred, models.AuthClientCredential)
         client_secret = cred.secret_hash
-        assert client_secret.startswith('sha256$')
+        assert client_secret.startswith('blake2b$32$')
 
     def test_clientcredential_get(self):
         """
@@ -40,3 +40,26 @@ class TestClientCredential(TestDatabaseFixture):
         cred, secret = models.AuthClientCredential.new(auth_client)
         assert cred.secret_is(secret)
         assert len(secret) in (43, 44)
+
+    def test_clientcredential_upgrade_hash(self):
+        """
+        Test for transparent upgrade of client credential hash type
+        """
+        secret = 'D2axSjtbbWDkRFmSDXGpNSB9ypfqE1ekYD3YP37J85yJ'
+        secret_sha256 = (
+            'sha256$45c879362ed45b3f92a7ea3c1e53ecab0dd79c61cb357e6eb0de6d64408ea25c'
+        )
+        secret_blake2b = 'blake2b$32$e7b49edf2b7c3945631d229dae3db30517f75047ce547a97ae27e9b46f69723a'
+        cred = models.AuthClientCredential(
+            auth_client=self.fixtures.auth_client, secret_hash=secret_sha256
+        )
+        assert cred.secret_hash == secret_sha256
+        assert cred.secret_is('incorrect') is False
+        assert cred.secret_hash == secret_sha256
+        assert cred.secret_is('incorrect', upgrade_hash=True) is False
+        assert cred.secret_hash == secret_sha256
+        assert cred.secret_is(secret) is True
+        assert cred.secret_hash == secret_sha256
+        assert cred.secret_is(secret, upgrade_hash=True) is True
+        assert cred.secret_hash != secret_sha256
+        assert cred.secret_hash == secret_blake2b
