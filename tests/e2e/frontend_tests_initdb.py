635c8d615834e800962daf66f141f8a64f4be27f
Vidya Ramakrishnan <vidya@hasgeek.in>
2020-06-30 23:47:57 +0530

    Add stronger password for e2e test fixtures

--- tests/e2e/frontend_tests_initdb.py
+++ tests/e2e/frontend_tests_initdb.py
@@ -8,33 +8,33 @@ def init_models():
         db.create_all()
 
         user_admin = User(username='admin-user', fullname='admin-user')
-        user_admin._set_password('cypress129_ftz')
+        user_admin._set_password('admin-user129_Ftz')
         user_admin_email = UserEmail(email='adminuser@example.com', user=user_admin)
 
         user = User(username='member-user', fullname='member-user')
-        user._set_password('cypress341_wer')
+        user._set_password('member-user341_Wer')
         user_email = UserEmail(email='memberuser@example.com', user=user)
 
         profile_owner = User(username='profile-cypress', fullname='profile-cypress')
-        profile_owner._set_password('cypress123_st')
+        profile_owner._set_password('profile-cypress123_St')
         profile_owner_email = UserEmail(
             email='profileowner@example.com', user=profile_owner
         )
 
         concierge = User(username='concierge-user', fullname='concierge-user')
-        concierge._set_password('cypress341_qwe')
+        concierge._set_password('concierge-user341_qQE')
         concierge_email = UserEmail(email='concierge@example.com', user=concierge)
 
         usher = User(username='usher-cypress', fullname='usher-cypress')
-        usher._set_password('cypress566_yut')
+        usher._set_password('usher-cypress566_YUt')
         usher_email = UserEmail(email='usher@example.com', user=usher)
 
         editor = User(username='editor-cypress', fullname='editor-cypress')
-        editor._set_password('cypress900_ghy')
+        editor._set_password('editor-cypress900_GHy')
         editor_email = UserEmail(email='editor@example.com', user=editor)
 
         user2 = User(username='hg-user', fullname='hg-user')
-        user2._set_password('cypress5678y9')
+        user2._set_password('hg-user567_HE')
 
         db.session.add_all(
             [
