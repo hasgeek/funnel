d851630259b4676ee02ca7fb04b6486bdbfddc1f
Vidya Ramakrishnan <vidya@hasgeek.in>
2020-07-24 13:04:50 +0530

    Add frontend for updates

---
+++ funnel/assets/js/post.js
@@ -0,0 +1,183 @@
+import Vue from 'vue/dist/vue.min';
+import VS2 from 'vue-script2';
+import { Utils } from './util';
+
+const Posts = {
+  init({
+    newPostUrl,
+    posts = '',
+    divElem,
+    postTemplate,
+    isuserloggedin,
+    isEditor,
+    loginUrl,
+    headerHeight,
+  }) {
+    Vue.config.devtools = true;
+    Vue.use(VS2);
+
+    const postUI = Vue.component('post', {
+      template: postTemplate,
+      props: ['post'],
+      data() {
+        return {
+          postForm: '',
+          errorMsg: '',
+          svgIconUrl: window.HasGeek.config.svgIconUrl,
+          hide: false,
+        };
+      },
+      methods: {
+        getInitials: window.Baseframe.Utils.getInitials,
+        collapse(action) {
+          this.hide = action;
+        },
+        fetchForm(event, url, post) {
+          if (post) {
+            this.$parent.fetchForm(event, url, post);
+          } else {
+            this.$parent.fetchForm(event, url, this);
+          }
+        },
+        updatepostsList(postsList) {
+          this.$parent.updatepostsList(postsList);
+        },
+        activateForm(post) {
+          this.$parent.activateForm(post);
+        },
+        closeForm(event) {
+          event.preventDefault();
+          this.postForm = '';
+          this.errorMsg = '';
+        },
+      },
+      computed: {
+        Form() {
+          const template = this.postForm ? this.postForm : '<div></div>';
+          const isFormTemplate = this.postForm ? true : '';
+          return {
+            template,
+            mounted() {
+              if (isFormTemplate) {
+                this.$parent.activateForm(this.$parent);
+              }
+            },
+          };
+        },
+      },
+    });
+
+    const app = new Vue({
+      el: divElem,
+      components: {
+        postUI,
+      },
+      data() {
+        return {
+          newPostUrl,
+          posts: posts.length > 0 ? posts : '',
+          isuserloggedin,
+          isEditor,
+          postForm: '',
+          errorMsg: '',
+          loginUrl,
+          headerHeight,
+          svgIconUrl: window.HasGeek.config.svgIconUrl,
+        };
+      },
+      methods: {
+        fetchForm(event, url, post = '') {
+          event.preventDefault();
+          if (this.isEditor) {
+            $.ajax({
+              type: 'GET',
+              url,
+              timeout: window.HasGeek.config.ajaxTimeout,
+              dataType: 'json',
+              success(data) {
+                console.log('data', data);
+                const vueFormHtml = data.form;
+                if (post) {
+                  post.postForm = vueFormHtml.replace(/\bscript\b/g, 'script2');
+                } else {
+                  app.postForm = vueFormHtml.replace(/\bscript\b/g, 'script2');
+                }
+              },
+            });
+          }
+        },
+        activateForm(parentApp) {
+          const formId = Utils.getElementId(parentApp.postForm);
+          const url = Utils.getActionUrl(formId);
+          const onSuccess = (responseData) => {
+            parentApp.postForm = '';
+            parentApp.errorMsg = '';
+            if (responseData.posts) {
+              parentApp.updatepostsList(responseData.posts);
+              window.toastr.success(responseData.message);
+            }
+            app.refreshpostsTimer();
+          };
+          const onError = (response) => {
+            parentApp.errorMsg = Utils.formErrorHandler(formId, response);
+          };
+          window.Baseframe.Forms.handleFormSubmit(
+            formId,
+            url,
+            onSuccess,
+            onError,
+            {}
+          );
+        },
+        updatepostsList(postsList) {
+          this.posts = postsList.length > 0 ? postsList : '';
+        },
+        fetchpostsList() {
+          $.ajax({
+            type: 'GET',
+            timeout: window.HasGeek.config.ajaxTimeout,
+            dataType: 'json',
+            success(data) {
+              app.updatepostsList(data.posts);
+            },
+          });
+        },
+        closeForm(event) {
+          event.preventDefault();
+          this.postForm = '';
+          this.errorMsg = 'Posts';
+        },
+      },
+      computed: {
+        Form() {
+          const template = this.postForm ? this.postForm : '<div></div>';
+          const isFormTemplate = this.postForm ? true : '';
+          return {
+            template,
+            mounted() {
+              if (isFormTemplate) {
+                this.$parent.activateForm(this.$parent);
+              }
+            },
+          };
+        },
+      },
+      mounted() {
+        this.fetchpostsList();
+        if (window.location.hash) {
+          Utils.animateScrollTo(
+            document
+              .getElementById(window.location.hash)
+              .getBoundingClientRect().top - this.headerHeight
+          );
+        }
+      },
+    });
+  },
+};
+
+$(() => {
+  window.HasGeek.PostsInit = function (config) {
+    Posts.init(config);
+  };
+});
--- funnel/views/post.py
+++ funnel/views/post.py
@@ -1,12 +1,12 @@
-from flask import g, redirect
+from flask import g
 
-from baseframe import _, forms
+from baseframe import _, forms, request_is_xhr
 from coaster.auth import current_auth
 from coaster.utils import make_name
 from coaster.views import ModelView, UrlForView, render_with, requires_roles, route
 
 from .. import app
-from ..forms import PostForm
+from ..forms import PostForm, SavedProjectForm
 from ..models import Post, Profile, Project, db
 from .decorators import legacy_redirect
 from .helpers import requires_login
@@ -23,25 +23,21 @@ class ProjectPostView(ProjectViewMixin, UrlForView, ModelView):
     @requires_login
     @requires_roles({'reader'})
     def posts(self):
-        post_form = PostForm()
+        project_save_form = SavedProjectForm()
 
-        post_form_html = forms.render_form(
-            form=post_form,
-            action=self.obj.url_for('new_post'),
-            formid='post_form',
-            title=_("Post update"),
-            submit=_("Post"),
-            ajax=True,
-            with_chrome=False,
-        )
-
+        if request_is_xhr():
+            return {
+                'posts': self.obj.views.json_posts(),
+            }
+        project_save_form = SavedProjectForm()
         return {
             'project': self.obj.current_access(),
-            'post_form': post_form_html,
-            'posts': self.obj.views.json_posts(),
+            'new_post': self.obj.url_for('new_post'),
+            'project_save_form': project_save_form,
+            'csrf_form': forms.Form(),
         }
 
-    @route('new', methods=['POST'])
+    @route('new', methods=['GET', 'POST'])
     @render_with(json=True)
     @requires_login
     @requires_roles({'editor'})
@@ -52,12 +48,16 @@ class ProjectPostView(ProjectViewMixin, UrlForView, ModelView):
             post = Post(user=current_auth.user, project=self.obj)
             post_form.populate_obj(post)
             post.name = make_name(post.title)
-            post.publish(actor=current_auth.user)
+            # post.publish(actor=current_auth.user)
             if post_form.restricted.data:
                 post.make_restricted()
             db.session.add(post)
             db.session.commit()
-            return redirect(self.obj.url_for('posts'))
+            return {
+                'status': 'ok',
+                'message': _("Your update has been posted"),
+                'posts': self.obj.views.json_posts(),
+            }
 
         post_form_html = forms.render_form(
             form=post_form,
@@ -68,8 +68,7 @@ class ProjectPostView(ProjectViewMixin, UrlForView, ModelView):
             with_chrome=False,
         )
         return {
-            'status': 'error',
-            'post_form': post_form_html,
+            'form': post_form_html,
         }
 
 
