92b90ce36c3bcd0b87ff452de87b60f1e655577b
Karan Saini <37518297+qurbat@users.noreply.github.com>
2020-07-30 16:24:14 +0530

    Update email.py

--- funnel/views/email.py
+++ funnel/views/email.py
@@ -159,9 +159,17 @@ def send_email_for_project_registration(project, user):
 
 @signals.user_cancelled_project_registration.connect
 def send_email_for_project_deregistration(project, user):
-    background_project_deregister_email.queue(
-        project_id=project.id, user_id=user.id, locale=get_locale()
-    )
+    locale = get_locale()
+    with force_locale(locale):
+        send_email(
+            subject=_("Registration cancelled for {project}").format(
+                project=project.title
+            ),
+            to=[user],
+            content=render_template(
+                'email_project_deregister.html.jinja2', user=user, project=project,
+            ),
+        )
 
 
 @rq.job('funnel')
@@ -183,22 +191,6 @@ def background_project_registration_email(project_id, user_id, locale):
             ],
             content=render_template(
                 'email_project_registration.html.jinja2', user=user, project=project,
-            ),
-        )
-
-
-@rq.job('funnel')
-def background_project_deregister_email(project_id, user_id, locale):
-    with app.app_context(), force_locale(locale):
-        project = Project.query.get(project_id)
-        user = User.query.get(user_id)
-        send_email(
-            subject=_("Registration cancelled for {project}").format(
-                project=project.title
-            ),
-            to=[user],
-            content=render_template(
-                'email_project_deregister.html.jinja2', user=user, project=project,
             ),
         )
 
