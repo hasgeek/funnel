ecf3775b4618b5ba7044f8606977962deb7b2dc0
Vidya Ramakrishnan <vidya@hasgeek.in>
2020-08-05 21:12:49 +0530

    User profile change it  to floating button

--- funnel/models/profile.py
+++ funnel/models/profile.py
@@ -136,6 +136,7 @@ class Profile(UuidMixin, BaseMixin, db.Model):
                 'organization',
                 'banner_image_url',
                 'is_organization_profile',
+                'is_user_profile',
             },
             'call': {'url_for', 'features', 'forms'},
         }
