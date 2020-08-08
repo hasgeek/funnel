91c38c6db3db27b15c100182c796ab42b62c7682
Kiran Jonnalagadda <kiran@hasgeek.com>
2020-08-08 08:48:09 +0530

    Finish migration and drop text search weights

--- funnel/models/update.py
+++ funnel/models/update.py
@@ -121,7 +121,7 @@ class Update(UuidMixin, BaseScopedIdNameMixin, TimestampMixin, db.Model):
                 'name',
                 'title',
                 'body_text',
-                weights={'name': 'A', 'title': 'A', 'body_text': 'B'},
+                weights={'name': 'C', 'title': 'C', 'body_text': 'D'},
                 regconfig='english',
                 hltext=lambda: db.func.concat_ws(
                     visual_field_delimiter, Update.title, Update.body_html
---
+++ migrations/versions/ea1ea3b0ff95_rename_post_constraints_to_update_.py
@@ -0,0 +1,97 @@
+"""Rename Post constraints to Update constraints
+
+Revision ID: ea1ea3b0ff95
+Revises: 851473d61beb
+Create Date: 2020-08-08 08:40:19.751509
+
+"""
+from textwrap import dedent
+
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = 'ea1ea3b0ff95'
+down_revision = '851473d61beb'
+branch_labels = None
+depends_on = None
+
+# (old, new)
+renamed_constraints = [
+    ("post_commentset_id_fkey", "update_commentset_id_fkey"),
+    ("post_deleted_by_id_fkey", "update_deleted_by_id_fkey"),
+    ("post_project_id_fkey", "update_project_id_fkey"),
+    ("post_published_by_id_fkey", "update_published_by_id_fkey"),
+    ("post_user_id_fkey", "update_user_id_fkey"),
+    ("post_voteset_id_fkey", "update_voteset_id_fkey"),
+]
+
+
+def upgrade():
+    op.alter_column('update', 'project_id', existing_type=sa.INTEGER(), nullable=False)
+    for old, new in renamed_constraints:
+        op.execute(sa.DDL(f'ALTER TABLE update RENAME CONSTRAINT "{old}" TO "{new}"'))
+
+    op.execute(
+        sa.DDL(
+            dedent(
+                '''
+        DROP TRIGGER post_search_vector_trigger ON update;
+        DROP FUNCTION post_search_vector_update();
+        '''
+            )
+        )
+    )
+
+    op.execute(
+        sa.DDL(
+            dedent(
+                '''
+        CREATE FUNCTION update_search_vector_update() RETURNS trigger AS $$
+        BEGIN
+            NEW.search_vector := setweight(to_tsvector('english', COALESCE(NEW.name, '')), 'C') || setweight(to_tsvector('english', COALESCE(NEW.title, '')), 'C') || setweight(to_tsvector('english', COALESCE(NEW.body_text, '')), 'D');
+            RETURN NEW;
+        END
+        $$ LANGUAGE plpgsql;
+
+        CREATE TRIGGER update_search_vector_trigger BEFORE INSERT OR UPDATE ON update
+        FOR EACH ROW EXECUTE PROCEDURE update_search_vector_update();
+                '''
+            )
+        )
+    )
+
+
+def downgrade():
+    op.execute(
+        sa.DDL(
+            dedent(
+                '''
+        DROP TRIGGER update_search_vector_trigger ON update;
+        DROP FUNCTION update_search_vector_update();
+        '''
+            )
+        )
+    )
+
+    op.execute(
+        sa.DDL(
+            dedent(
+                '''
+        CREATE FUNCTION post_search_vector_update() RETURNS trigger AS $$
+        BEGIN
+            NEW.search_vector := setweight(to_tsvector('english', COALESCE(NEW.name, '')), 'A') || setweight(to_tsvector('english', COALESCE(NEW.title, '')), 'A') || setweight(to_tsvector('english', COALESCE(NEW.body_text, '')), 'B');
+            RETURN NEW;
+        END
+        $$ LANGUAGE plpgsql;
+
+        CREATE TRIGGER post_search_vector_trigger BEFORE INSERT OR UPDATE ON post
+        FOR EACH ROW EXECUTE PROCEDURE post_search_vector_update();
+                '''
+            )
+        )
+    )
+
+    for old, new in renamed_constraints:
+        op.execute(sa.DDL(f'ALTER TABLE update RENAME CONSTRAINT "{new}" TO "{old}"'))
+    op.alter_column('update', 'project_id', existing_type=sa.INTEGER(), nullable=True)
