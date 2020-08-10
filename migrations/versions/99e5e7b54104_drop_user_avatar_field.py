d0cab82c68588acca5572f607cc438d4f23a66a6
Vidya Ramakrishnan <vidya@hasgeek.in>
2020-08-10 12:36:24 +0530

    User avatar UI (#815)
    * User avatar UI
    
    * dropping avatar field of user model
    
    * Add gravatar to account
    
    * Fix the macro import
    
    * Check for user.profile
    
    * Add avatar property to participant model. Change the dropdown styling
    
    * Remove extra character
    
    * Update migrations
    
    * Fixed migration number in comment
    
    * Fix the test fixture
    
    * Fix the test fixture
    
    Co-authored-by: Bibhas <github@bibhasdn.com>

---
+++ migrations/versions/99e5e7b54104_drop_user_avatar_field.py
@@ -0,0 +1,26 @@
+"""drop user avatar field
+
+Revision ID: 99e5e7b54104
+Revises: ea1ea3b0ff95
+Create Date: 2020-08-07 12:10:20.004784
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = '99e5e7b54104'
+down_revision = 'ea1ea3b0ff95'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.drop_column('user', 'avatar')
+
+
+def downgrade():
+    op.add_column(
+        'user', sa.Column('avatar', sa.TEXT(), autoincrement=False, nullable=True)
+    )
