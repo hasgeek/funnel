9d96cd4157fc1c3f2af048d3fbd582214e42ada4
Kiran Jonnalagadda <kiran@hasgeek.com>
2020-08-17 09:58:04 +0530

    Fix pycountry translations in VenueForm

--- funnel/forms/venue.py
+++ funnel/forms/venue.py
@@ -65,7 +65,7 @@ class VenueForm(forms.Form):
 
     def set_queries(self):
         pycountry_locale = gettext.translation(
-            'iso3166-2', pycountry.LOCALES_DIR, languages=[get_locale()]
+            'iso3166-2', pycountry.LOCALES_DIR, languages=[str(get_locale()), 'en']
         )
         countries = [
             (pycountry_locale.gettext(country.name), country.alpha_2)
