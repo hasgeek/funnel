83ce1682ce2360cb59599f7520fc3c7c5b647d56
anandv <anand@hasgeek.com>
2020-09-01 17:41:56 +0530

    1. Added more checks for discarding SNS messages from unknown sources.
    2. Added stats counters for
       a. Event Delivery
       b. Event Rejection
       c. Mail  Delivery
       d. Mail  Complaint
       e. Mail  bounce
       f. Mail  Opened
       g. Mail  Link Clicks

--- funnel/views/email_events.py
+++ funnel/views/email_events.py
@@ -1,7 +1,10 @@
+from typing import List
+
 from flask import request
 
 import requests
 
+from baseframe import statsd
 from coaster.views import render_with
 
 from .. import app
@@ -18,26 +21,47 @@ from ..transports.email.aws_ses import (
 class SesProcessor(SesProcessorAbc):
     """SES message processor."""
 
-    # `EmailAddress.add` does an implicit `.get`, but we call `.get` first because
-    # `.add` will fail if the address is blocked, while `.get` won't. Why add if we've
-    # never seen this email address before? Because it may have originated in Hasjob
-    # or elsewhere in shared infrastructure.
+    @staticmethod
+    def _email_address(address: str) -> EmailAddress:
+        """
+        `EmailAddress.add` does an implicit `.get`, but we call `.get` first because
+        `.add` will fail if the address is blocked, while `.get` won't. Why add if we've
+        never seen this email address before? Because it may have originated in Hasjob
+        or elsewhere in shared infrastructure.
+        :param address: Email Address
+        :returns Email Address object
+        """
+        email_address = EmailAddress.get(address)
+        if not email_address:
+            email_address = EmailAddress.add(address)
+        return email_address
 
     def bounce(self, ses_event: SesEvent) -> None:
+
+        # Statistics for bounced recipients.
+        statsd.incr(
+            "email_address.ses_email.bounced",
+            count=len(ses_event.bounce.bounced_recipients),
+        )
+
+        # Process bounces
         for bounced in ses_event.bounce.bounced_recipients:
-            email_address = EmailAddress.get(bounced.email)
-            if not email_address:
-                email_address = EmailAddress.add(bounced.email)
+            email_address = self._email_address(bounced.email)
             if ses_event.bounce.is_hard_bounce:
                 email_address.mark_hard_fail()
             else:
                 email_address.mark_soft_fail()
 
     def delayed(self, ses_event: SesEvent) -> None:
+
+        # Statistics for delayed recipients.
+        statsd.incr(
+            "email_address.ses_email.delayed",
+            count=len(ses_event.delivery_delay.delayed_recipients),
+        )
+
         for failed in ses_event.delivery_delay.delayed_recipients:
-            email_address = EmailAddress.get(failed.email)
-            if not email_address:
-                email_address = EmailAddress.add(failed.email)
+            email_address = self._email_address(failed.email)
             email_address.mark_soft_fail()
 
     def complaint(self, ses_event: SesEvent) -> None:
@@ -50,11 +74,11 @@ class SesProcessor(SesProcessorAbc):
         if len(ses_event.complaint.complained_recipients) == 1:
             for complained in ses_event.complaint.complained_recipients:
                 if ses_event.complaint.complaint_feedback_type == 'not-spam':
-                    email_address = EmailAddress.get(complained.email)
-                    if not email_address:
-                        email_address = EmailAddress.add(complained.email)
+                    email_address = self._email_address(complained.email)
                     email_address.mark_active()
+                    statsd.incr("email_address.ses_email.not_spam")
                 elif ses_event.complaint.complaint_feedback_type == 'abuse':
+                    statsd.incr("email_address.ses_email.abuse")
                     EmailAddress.mark_blocked(complained.email)
                 else:
                     # TODO: Process 'auth-failure', 'fraud', 'other', 'virus'
@@ -64,30 +88,27 @@ class SesProcessor(SesProcessorAbc):
         # Recipients here are strings and not structures. Unusual, but reflected in
         # the documentation.
         # https://docs.aws.amazon.com/ses/latest/DeveloperGuide/event-publishing-retrieving-sns-examples.html#event-publishing-retrieving-sns-send
+        statsd.incr(
+            "email_address.ses_email.delivered", count=ses_event.delivery.recipients
+        )
         for sent in ses_event.delivery.recipients:
-            email_address = EmailAddress.get(sent)
-            if not email_address:
-                email_address = EmailAddress.add(sent)
+            email_address = self._email_address(sent)
             email_address.mark_sent()
 
     def opened(self, ses_event: SesEvent) -> None:
         # SES doesn't track the recipient that triggered this action, so process this
         # only if the original email had a single recipient
         if len(ses_event.mail.destination) == 1:
-            email = ses_event.mail.destination[0]
-            email_address = EmailAddress.get(email)
-            if not email_address:
-                email_address = EmailAddress.add(email)
+            statsd.incr("email_address.ses_email.opened")
+            email_address = self._email_address(ses_event.mail.destination[0])
             email_address.mark_active()
 
     def click(self, ses_event: SesEvent) -> None:
         # SES doesn't track the recipient that triggered this action, so process this
         # only if the original email had a single recipient
         if len(ses_event.mail.destination) == 1:
-            email = ses_event.mail.destination[0]
-            email_address = EmailAddress.get(email)
-            if not email_address:
-                email_address = EmailAddress.add(email)
+            statsd.incr("email_address.ses_email.clicked")
+            email_address = self._email_address(ses_event.mail.destination[0])
             email_address.mark_active()
 
 
@@ -98,6 +119,13 @@ validator: SnsValidator = SnsValidator()
 # SES Message Processor
 processor: SesProcessor = SesProcessor()
 
+# SNS Headers that should be present in all messages
+sns_headers: List[str] = [
+    'x-amz-sns-message-type',
+    'x-amz-sns-message-id',
+    'x-amz-sns-topic-arn',
+]
+
 
 @app.route('/api/1/email/ses_event', methods=['POST'])
 @render_with(json=True)
@@ -107,9 +135,21 @@ def process_ses_event():
 
     The events are sent based on the configuration set of the outgoing email.
     """
+
+    # Register the fact that we got an SES event. If there are too many rejections, then it is a hack
+    # attempt.
+    statsd.incr('email_address.ses_event.received')
+
+    # Check for standard SNS headers and filter out, if they are not found.
+    for header in sns_headers:
+        if not request.headers.get(header):
+            statsd.incr('email_address.ses_event.rejected')
+            return {'status': 'error', 'error': 'not_json'}, 400
+
     # Get the JSON message
     message = request.get_json(force=True, silent=True)
     if not message:
+        statsd.incr('email_address.ses_event.rejected')
         return {'status': 'error', 'error': 'not_json'}, 400
 
     # Validate the message
@@ -117,10 +157,9 @@ def process_ses_event():
         validator.topics = app.config['SES_NOTIFICATION_TOPICS']
         validator.check(message)
     except SnsValidatorException as exc:
+        app.logger.info("SNS/SES event: %r", message)
+        statsd.incr('email_address.ses_event.rejected')
         return {'status': 'error', 'error': 'invalid_topic', 'message': exc.args}, 400
-
-    # Log the message for later analysis
-    app.logger.info("SNS/SES event: %r", message)
 
     # Message Type
     m_type = message.get('Type')
@@ -129,6 +168,7 @@ def process_ses_event():
     if m_type == SnsNotificationType.SubscriptionConfirmation.value:
         resp = requests.get(message.get('SubscribeURL'))
         if resp.status_code != 200:
+            statsd.incr('email_address.ses_event.rejected')
             return {'status': 'error', 'error': 'subscription_failed'}, 400
         return {'status': 'ok', 'message': 'subscription_success'}
 
@@ -136,6 +176,7 @@ def process_ses_event():
     if m_type == SnsNotificationType.UnsubscribeConfirmation.value:
         resp = requests.get(message.get('UnsubscribeURL'))
         if resp.status_code != 200:
+            statsd.incr('email_address.ses_event.rejected')
             return {'status': 'error', 'error': 'unsubscribe_failed'}, 400
         return {'status': 'ok', 'message': 'unsubscribe_success'}
 
@@ -147,4 +188,5 @@ def process_ses_event():
         return {'status': 'ok', 'message': 'notification_processed'}
 
     # We'll only get here if there's a misconfiguration
+    statsd.incr('email_address.ses_event.rejected')
     return {'status': 'error', 'error': 'unknown_message_type'}, 400
