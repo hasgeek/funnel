eed85a7f4a7d7302f205dd19c76789f2c1eeb32c
anandv <anand@hasgeek.com>
2020-09-21 09:40:56 +0530

    Moved Twilio Message checker from using Singleton attributes to Function Attributes

--- funnel/views/sms_events.py
+++ funnel/views/sms_events.py
@@ -13,31 +13,28 @@ from ..models import SMS_STATUS, SMSMessage, db
 from ..transports.sms import TwilioSmsResponse
 
 
-class TwilioMessageValidator:
-    """ Validator for Twilio SMS Messages """
-
-    def __init__(self):
-        """ Just setup the Object variables, but it will be used later. """
-        self.url = None
-        self.validator = None
-
-    def check(self, signature: str, message: Dict) -> bool:
-        """
-        Check if the incoming message can be validated as coming from Twilio
-        :param signature Message Signature
-        :param message   Incoming Message Payload
-        """
-        if not self.url:
-            self.url = url_for('process_twilio_event', _external=True)
-            self.validator = RequestValidator(app.config['SMS_TWILIO_TOKEN'])
+def _twilio_message_check(signature: str, message: Dict) -> bool:
+    """
+    Check if the incoming message can be validated as coming from Twilio
+    :param signature Message Signature
+    :param message   Incoming Message Payload
+    """
+    if not _twilio_message_check.url:
+        _twilio_message_check.url = url_for('process_twilio_event', _external=True)
+        _twilio_message_check.validator = RequestValidator(
+            app.config['SMS_TWILIO_TOKEN']
+        )
 
         # Let us now validate and reject if signatures mismatch. See this:
         # https://www.twilio.com/docs/usage/security#validating-requests
-        return self.validator.validate(self.url, message, signature)
+    return _twilio_message_check.validator.validate(
+        _twilio_message_check.url, message, signature
+    )
 
 
-# Unexposed global validator
-twilio_validator = TwilioMessageValidator()
+# Function Attributes
+_twilio_message_check.url = None
+_twilio_message_check.validator = None
 
 
 @app.route('/api/1/sms/twilio_event', methods=['POST'])
@@ -64,7 +61,7 @@ def process_twilio_event():
     # Needs conversion to Dict
     payload = json.loads(message)
     sms_response: TwilioSmsResponse = TwilioSmsResponse.from_dict(payload)
-    if not twilio_validator.check(signature, payload):
+    if not _twilio_message_check(signature, payload):
         app.logger.info("Twilio event: %r", message)
         statsd.incr('sms_address.twilio_event.rejected')
         return {'status': 'error', 'error': 'invalid_signature'}, 400
