3769f7d8c630d0bab327d2afa0ff95954fcf32c7
Vidya Ramakrishnan <vidya@hasgeek.in>
2020-10-05 23:08:43 +0530

    Fix error page titles. Order the error functions

--- funnel/views/errors.py
+++ funnel/views/errors.py
@@ -1,34 +1,19 @@
 from __future__ import unicode_literals
 
 from flask import current_app, redirect, request
-from werkzeug import exceptions
 from werkzeug.routing import MethodNotAllowed, NotFound, RequestRedirect
+import werkzeug.exceptions as exceptions
 
 from coaster.views import render_with
 
 from .. import app
-from . import baseframe_translations
+from ..models import db
 
 
-@app.errorhandler(exceptions.TooManyRequests)
-@render_with('429.html.jinja2', json=True)
-def error429(e):
-    baseframe_translations.as_default()
-    return {'error': "429 Too Many Requests"}, 429
-
-
-@app.errorhandler(exceptions.Gone)
-@render_with('410.html.jinja2', json=True)
-def error410(e):
-    baseframe_translations.as_default()
-    return {'error': "410 Page now gone"}, 410
-
-
-@app.errorhandler(exceptions.MethodNotAllowed)
-@render_with('405.html.jinja2', json=True)
-def error405(e):
-    baseframe_translations.as_default()
-    return {'error': "405 Method Not Allowed"}, 405
+@app.errorhandler(exceptions.Forbidden)
+@render_with('403.html.jinja2', json=True)
+def error403(e):
+    return {'error': "403 Forbidden"}, 403
 
 
 @app.errorhandler(exceptions.NotFound)
@@ -49,29 +34,35 @@ def error404(e):
                 return redirect(redirect_url)
         except (NotFound, RequestRedirect, MethodNotAllowed):
             pass
-    baseframe_translations.as_default()
     return {'error': "404 Not Found"}, 404
 
 
-@app.errorhandler(exceptions.Forbidden)
-@render_with('403.html.jinja2', json=True)
-def error403(e):
-    baseframe_translations.as_default()
-    return {'error': "403 Forbidden"}, 403
+@app.errorhandler(exceptions.MethodNotAllowed)
+@render_with('405.html.jinja2', json=True)
+def error405(e):
+    return {'error': "405 Method Not Allowed"}, 405
+
+
+@app.errorhandler(exceptions.Gone)
+@render_with('410.html.jinja2', json=True)
+def error410(e):
+    return {'error': "410 Gone"}, 410
+
+
+@app.errorhandler(exceptions.TooManyRequests)
+@render_with('429.html.jinja2', json=True)
+def error429(e):
+    return {'error': "429 Too Many Requests"}, 429
 
 
 @app.errorhandler(exceptions.InternalServerError)
 @render_with('500.html.jinja2', json=True)
 def error500(e):
-    if current_app.extensions and 'sqlalchemy' in current_app.extensions:
-        current_app.extensions['sqlalchemy'].db.session.rollback()
-
-    baseframe_translations.as_default()
+    db.session.rollback()
     return {'error': "500 Internal Server Error"}, 500
 
 
 @app.errorhandler(exceptions.ServiceUnavailable)
 @render_with('503.html.jinja2', json=True)
 def error503(e):
-    baseframe_translations.as_default()
     return {'error': "503 Service Unavailable"}, 503
