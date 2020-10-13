918b25a3bebd5637a1be0744fbf4e2be318c8754
Kiran Jonnalagadda <kiran@hasgeek.com>
2020-10-13 15:18:08 +0530

    Tag-based statsd events for email and SMS

--- funnel/views/sms_events.py
+++ funnel/views/sms_events.py
@@ -11,18 +11,25 @@ from ..transports.sms import validate_exotel_token
 
 
 @app.route('/api/1/sms/twilio_event', methods=['POST'])
-@render_with(template=None, json=True)
+@render_with(json=True)
 def process_twilio_event():
     """Process SMS callback event from Twilio."""
 
     # Register the fact that we got a Twilio SMS event.
     # If there are too many rejects, then most likely a hack attempt.
-    statsd.incr('phone_number.sms.twilio_event.received')
+    statsd.incr('phone_number.event', tags={'engine': 'twilio', 'stage': 'received'})
 
     # Check if we find twilio headers and if not reject it
     signature = request.headers.get('X-Twilio-Signature')
     if not signature:
-        statsd.incr('phone_number.sms.twilio_event.rejected')
+        statsd.incr(
+            'phone_number.event',
+            tags={
+                'engine': 'twilio',
+                'stage': 'rejected',
+                'error': 'missing_signature',
+            },
+        )
         return {'status': 'error', 'error': 'missing_signature'}, 422
 
     # Create Request Validator
@@ -30,7 +37,14 @@ def process_twilio_event():
     if not validator.validate(
         request.url, request.form, request.headers.get('X-Twilio-Signature', '')
     ):
-        statsd.incr('phone_number.sms.twilio_event.rejected')
+        statsd.incr(
+            'phone_number.event',
+            tags={
+                'engine': 'twilio',
+                'stage': 'rejected',
+                'error': 'invalid_signature',
+            },
+        )
         return {'status': 'error', 'error': 'invalid_signature'}, 422
 
     # This code segment needs to change and re-written once Phone Number model is
@@ -65,26 +79,41 @@ def process_twilio_event():
         request.form['To'],
         request.form['MessageStatus'],
     )
+    statsd.incr(
+        'phone_number.event',
+        tags={
+            'engine': 'twilio',
+            'stage': 'processed',
+            'event': request.form['MessageStatus'],
+        },
+    )
     return {'status': 'ok', 'message': 'sms_notification_processed'}
 
 
 @app.route('/api/1/sms/exotel_event/<secret_token>', methods=['POST'])
-@render_with(template=None, json=True)
+@render_with(json=True)
 def process_exotel_event(secret_token: str):
     """Process SMS callback event from Exotel."""
 
     # Register the fact that we got a Exotel SMS event.
     # If there are too many rejects, then most likely a hack attempt.
-    statsd.incr('phone_number.sms.exotel_event.received')
+    statsd.incr('phone_number.event', tags={'engine': 'exotel', 'stage': 'received'})
 
-    # We just need to verify the token first.
+    # We need to verify the token first.
     if not validate_exotel_token(secret_token, request.form.get('To')):
-        statsd.incr('phone_number.sms.exotel_event.rejected')
+        statsd.incr(
+            'phone_number.event',
+            tags={
+                'engine': 'exotel',
+                'stage': 'rejected',
+                'error': 'invalid_signature',
+            },
+        )
         return {'status': 'error', 'error': 'invalid_signature'}, 422
 
-    # This code segment needs to change and re-written once Phone Number model is
+    # This code segment needs to be re-written once Phone Number model is
     # in place. The Message parameter has to be '' because exotel does not send
-    # back the message as per the API model but the DB model expects it
+    # back the message as per the API model, but the DB model expects it
     # and we get a exception, otherwise.
 
     # There are only 3 parameters in the callback as per the documentation
@@ -121,5 +150,13 @@ def process_exotel_event(secret_token: str):
         "Exotel event for phone: %s %s",
         request.form['To'],
         request.form['Status'],
+    )
+    statsd.incr(
+        'phone_number.event',
+        tags={
+            'engine': 'twilio',
+            'stage': 'processed',
+            'event': request.form['Status'],
+        },
     )
     return {'status': 'ok', 'message': 'sms_notification_processed'}
