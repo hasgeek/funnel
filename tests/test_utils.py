1b769347f09d6d5fe5c5178c55122ab1c14bae50
Kiran Jonnalagadda <kiran@hasgeek.com>
2020-11-02 12:53:45 +0530

    Rename legacy test files

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -12,38 +12,42 @@ from funnel.utils import (
 )
 
 
-class TestUtils(object):
-    def test_extract_twitter_handle(self):
-        expected = 'shreyas_satish'
-        assert extract_twitter_handle('https://twitter.com/shreyas_satish') == expected
-        assert (
-            extract_twitter_handle('https://twitter.com/shreyas_satish/favorites')
-            == expected
-        )
-        assert extract_twitter_handle('@shreyas_satish') == expected
-        assert extract_twitter_handle('shreyas_satish') == expected
-        assert extract_twitter_handle('seriouslylongstring') is None
-        assert extract_twitter_handle('https://facebook.com/shreyas') is None
-        assert extract_twitter_handle('') is None
+def test_extract_twitter_handle():
+    expected = 'shreyas_satish'
+    assert extract_twitter_handle('https://twitter.com/shreyas_satish') == expected
+    assert (
+        extract_twitter_handle('https://twitter.com/shreyas_satish/favorites')
+        == expected
+    )
+    assert extract_twitter_handle('@shreyas_satish') == expected
+    assert extract_twitter_handle('shreyas_satish') == expected
+    assert extract_twitter_handle('seriouslylongstring') is None
+    assert extract_twitter_handle('https://facebook.com/shreyas') is None
+    assert extract_twitter_handle('') is None
 
-    def test_geonameid_from_location(self, test_client):
-        with current_app.test_request_context('/'):
-            location = "Bangalore"
-            expected_geonameid = 1277333
-            assert expected_geonameid in geonameid_from_location(location)
 
-    def test_split_name(self):
-        assert split_name("ABC DEF EFG") == ["ABC", "DEF EFG"]
+def test_geonameid_from_location(test_client):
+    with current_app.test_request_context('/'):
+        location = "Bangalore"
+        expected_geonameid = 1277333
+        assert expected_geonameid in geonameid_from_location(location)
 
-    def test_format_twitter_handle(self):
-        assert format_twitter_handle("testusername") == "@testusername"
 
-    def test_null_abort_tainted(self, test_client):
-        with current_app.test_request_context('/'):
-            with pytest.raises(expected_exception=BadRequest):
-                abort_null('\x00')
+def test_split_name():
+    assert split_name("ABC DEF EFG") == ["ABC", "DEF EFG"]
 
-    def test_null_abort_clean(self, test_client):
-        with current_app.test_request_context('/'):
-            expected = abort_null('Sample string')
-            assert expected == 'Sample string'
+
+def test_format_twitter_handle():
+    assert format_twitter_handle("testusername") == "@testusername"
+
+
+def test_null_abort_tainted(test_client):
+    with current_app.test_request_context('/'):
+        with pytest.raises(expected_exception=BadRequest):
+            abort_null('\x00')
+
+
+def test_null_abort_clean(test_client):
+    with current_app.test_request_context('/'):
+        expected = abort_null('Sample string')
+        assert expected == 'Sample string'
