61b2ecd224e2c727874d1d293beb5318930530eb
Karan Saini <37518297+qurbat@users.noreply.github.com>
2020-12-14 23:03:00 +0530

    test fix

--- funnel/assets/cypress/integration/29_postComment.js
+++ funnel/assets/cypress/integration/29_postComment.js
@@ -1,94 +1,62 @@
-describe('Test comments feature', function () {
+describe('Add a new proposal', function () {
   const user = require('../fixtures/user.json').user;
-  const hguser = require('../fixtures/user.json').hguser;
   const editor = require('../fixtures/user.json').editor;
+  const profile = require('../fixtures/profile.json');
+  const proposal = require('../fixtures/proposal.json');
   const project = require('../fixtures/project.json');
+  const labels = require('../fixtures/labels.json');
 
-  it('Post comment on project page', function () {
+  it('Add proposal', function () {
     cy.server();
-    cy.route('GET', '**/new').as('get-form');
+    cy.route('GET', '**/admin').as('fetch-admin-panel');
     cy.route('GET', '**/updates/*').as('fetch-updates');
     cy.route('POST', '**/new').as('post-comment');
-    cy.route('POST', '**/edit').as('edit-comment');
-    cy.route('POST', '**/reply').as('reply-comment');
-    cy.route('POST', '**/delete').as('delete-comment');
-    cy.route('**/json').as('edit-comment');
 
-    cy.visit('/');
-    cy.get('.upcoming')
-      .find('.card--upcoming')
-      .contains(project.title)
-      .click({ force: true });
+    cy.login('/' + profile.title, user.username, user.password);
+
+    cy.get('a[data-cy-title="' + project.title + '"]').click();
     cy.location('pathname').should('contain', project.url);
-    cy.get('a[data-cy-navbar="comments"]').click();
-    cy.get('a[data-cy="login-btn"]').click();
-    cy.fill_login_details(user.username, user.password);
+    cy.get('a[data-cy-navbar="submissions"]').click();
+    cy.location('pathname').should('contain', 'proposals');
+    cy.get('a[data-cy="propose-a-session"]').click();
+    cy.location('pathname').should('contain', 'new');
+    cy.get('#title').type(proposal.title);
+    cy.get('#field-body')
+      .find('.CodeMirror textarea')
+      .type(proposal.content, { force: true });
+    cy.get('#field-video_url').type(proposal.preview_video);
+    cy.get('fieldset').find('.listwidget').eq(0).find('input').eq(0).click();
+    cy.get('fieldset').find('.listwidget').eq(1).find('input').eq(0).click();
+    cy.get('button[data-cy="form-submit-btn"]').click();
+    cy.location('pathname').should('contain', 'proposals');
 
+    cy.get('[data-cy="proposal-title"]')
+      .should('exist')
+      .contains(proposal.title);
+    cy.get('[data-cy="proposal-video"]').find('iframe').should('be.visible');
+    cy.get('.proposal__section').find('a[data-cy="admin-panel"]').click();
+    cy.wait('@fetch-admin-panel');
+    cy.get('[data-cy-admin="edit"]').should('exist');
+    cy.get('[data-cy-admin="delete"]').should('exist');
+    cy.get('[data-cy="edit-proposal-video"]').should('exist');
+
+    cy.get('a[data-cy="close-admin-panel"]').click();
     cy.get('[data-cy="post-comment"]').click();
     cy.get('[data-cy="new-form"]')
       .find('.CodeMirror textarea')
-      .type(project.comment, { force: true });
+      .type(proposal.proposer_note, { force: true });
     cy.wait(1000);
     cy.get('[data-cy="new-form"]').find('[data-cy="submit-comment"]').click();
     cy.wait('@post-comment');
-    var cid = window.location.hash;
-    cy.get('.comment__body').contains(project.comment);
+    cy.get('.comment__body').contains(proposal.proposer_note);
     cy.get('.comment__header').contains(user.username);
-
-    cy.wait(2000);
-    cy.get('a[data-cy="comment-menu"]:visible').click();
-    cy.wait(1000);
-    cy.get('a[data-cy="edit"]').click();
-    cy.get('[data-cy="edit-form"]')
-      .find('.CodeMirror textarea')
-      .type(project.edit_comment, { force: true });
-    cy.wait(1000);
-    cy.get('[data-cy="edit-form"]').find('[data-cy="edit-comment"]').click();
-    cy.wait('@edit-comment');
-    cy.get(`${cid} .comment__body`).contains(project.edit_comment);
-
-    cy.get('a[data-cy="reply"]').click();
-    cy.get('[data-cy="reply-form"]')
-      .find('.CodeMirror textarea')
-      .type(project.reply_comment, { force: true });
-    cy.wait(1000);
-    cy.get('[data-cy="reply-form"]').find('[data-cy="reply-comment"]').click();
-    cy.wait('@reply-comment');
-    cid = window.location.hash;
-    cy.get(`${cid} .comment__body`).contains(project.reply_comment);
-    cy.wait(1000);
-
-    /*
-    The test for deleting comments has been disabled as it
-    interferes with the comment notification test. To be added back later.
-    cy.get('a[data-cy="comment-menu"]:visible').eq(1).click();
-    cy.wait(1000);
-    cy.get('a[data-cy="delete"]:visible').click();
-    cy.wait(1000);
-    cy.get('button[data-cy="confirm-delete-btn"]').click();
-    cy.wait('@delete-comment');
-    cy.get('.comment__body')
-      .contains(project.reply_comment)
-      .should('not.exist');
-    cy.wait(5000);
-     */
 
     cy.visit('/');
     cy.logout();
-
-    cy.login('/', hguser.username, hguser.password);
-    cy.get('.upcoming')
-      .find('.card--upcoming')
-      .contains(project.title)
-      .click({ force: true });
-    cy.location('pathname').should('contain', project.url);
-    cy.get('a[data-cy-navbar="comments"]').click();
-    cy.get('[data-cy="post-comment"]').should('exist');
-
-    cy.logout();
-    cy.login('/', editor.username, editor.password);
+    cy.wait(1000);
+    cy.login('/' + profile.title, editor.username, editor.password);
     cy.visit('/updates');
     cy.wait('@fetch-updates');
-    cy.get('[data-cy="notification-box"]').contains(project.title);
+    cy.get('[data-cy="notification-box"]').contains(proposal.title);
   });
 });
