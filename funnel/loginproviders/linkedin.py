75ca888e5e6c52bf8a018a14d468eadeb8c07d4e
Bibhas <me@bibhas.in>
2021-01-27 19:19:17 +0530

    Handler for invalid JSON response from Linkedin (#1011)

--- funnel/loginproviders/linkedin.py
+++ funnel/loginproviders/linkedin.py
@@ -1,8 +1,10 @@
 from urllib.parse import quote
 from uuid import uuid4
 
-from flask import redirect, request, session
+from flask import current_app, redirect, request, session
 
+from sentry_sdk import capture_exception
+from simplejson.errors import JSONDecodeError
 import requests
 
 from baseframe import _
@@ -14,7 +16,7 @@ __all__ = ['LinkedInProvider']
 
 class LinkedInProvider(LoginProvider):
     auth_url = 'https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id={client_id}&scope={scope}&redirect_uri={redirect_uri}&state={state}'
-    token_url = 'https://www.linkedin.com/uas/oauth2/accessToken'
+    token_url = 'https://www.linkedin.com/uas/oauth2/accessToken'  # nosec
     user_info = 'https://api.linkedin.com/v2/me?projection=(id,localizedFirstName,localizedLastName)'
     user_email = 'https://api.linkedin.com/v2/emailAddress?q=members&projection=(elements*(handle~))'
 
@@ -77,6 +79,12 @@ class LinkedInProvider(LoginProvider):
                 _(
                     "Unable to authenticate via LinkedIn. Internal details: {error}"
                 ).format(error=e)
+            )
+        except JSONDecodeError as e:
+            current_app.logger.error(e.msg)
+            capture_exception(e)
+            raise LoginCallbackError(
+                _("LinkedIn is having intermittent issues. Try again?")
             )
         if 'error' in response:
             raise LoginCallbackError(response['error'])
