edd3a21c729272ce4950363d1348da7867863fc1
Vidya Ramakrishnan <vidya@hasgeek.in>
2021-03-15 18:11:04 +0530

    Move js function to assets

--- funnel/assets/js/proposals.js
+++ funnel/assets/js/proposals.js
@@ -1,7 +1,7 @@
 import { TableSearch } from './util';
 
 $(() => {
-  window.Hasgeek.ProposalsInit = function ({ search = '' }) {
+  window.Hasgeek.ProposalsInit = function ({ search = '', sort = '' }) {
     if (search) {
       const tableSearch = new TableSearch(search.tableId);
       const inputId = `#${search.inputId}`;
@@ -14,6 +14,28 @@ $(() => {
         $(tableRow).addClass('mui--hide');
         const hits = tableSearch.searchRows($(this).val());
         $(hits.join(',')).removeClass('mui--hide');
+      });
+    }
+    if (sort.permission) {
+      $('.proposal-list-table tbody').sortable({
+        placeholder: $(this).data('drag-placeholder'),
+        cursor: 'move',
+        update: function (event, ui) {
+          var sortedIDs = $(this).sortable('toArray');
+          var currentID = $(ui.item).attr('id');
+          var currentIndex = sortedIDs.indexOf(currentID);
+          $.ajax({
+            url: sort.url,
+            type: 'POST',
+            data: {
+              csrf_token: $('meta[name="csrf-token"]').attr('content'),
+              currentItemId: currentID,
+              previousItemId:
+                currentID === 0 ? null : sortedIDs[currentIndex - 1],
+            },
+            dataType: 'json',
+          });
+        },
       });
     }
   };
