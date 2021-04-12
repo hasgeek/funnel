7b437cabf0e22947d7b85bf99417f00d5b32057d
Bibhas <me@bibhas.in>
2021-04-12 18:11:19 +0530

    Misc pre-commit related fixes (#1084)
    * Misc pre-commit related fixes
    
    * Uppercase NOQA

--- runlastuser.py
+++ runlastuser.py
@@ -8,4 +8,4 @@ try:
     port = int(sys.argv[1])
 except (IndexError, ValueError):
     port = 7000
-lastuserapp.run('0.0.0.0', port=port, debug=True)
+lastuserapp.run('0.0.0.0', port=port, debug=True)  # nosec
