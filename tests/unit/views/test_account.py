26a4c33b9a941f8f8d243dedab3866e1bce46c34
Kiran Jonnalagadda <kiran@hasgeek.com>
2021-05-13 03:46:17 +0530

    Enable CSRF in tests

--- tests/unit/views/test_account.py
+++ tests/unit/views/test_account.py
@@ -5,22 +5,31 @@ def test_username_available(client, new_user):
     """Test the username availability endpoint."""
     endpoint = '/api/1/account/username_available'
 
+    # Get a CSRF token
+    csrf_token = client.get('/api/baseframe/1/csrf/refresh').get_data(as_text=True)
+
     # Does not support GET requests
     rv = client.get(endpoint)
     assert rv.status_code == 405
 
     # Requires a username to process
-    rv = client.post(endpoint)
+    rv = client.post(endpoint, data={'csrf_token': csrf_token})
     assert rv.status_code == 422  # Incomplete forms are 422 Unprocessable Entity
     assert rv.get_json() == {'status': 'error', 'error': 'username_required'}
 
     # Valid usernames will return an ok response
-    rv = client.post(endpoint, data={'username': 'should-be-available'})
+    rv = client.post(
+        endpoint,
+        data={'username': 'should-be-available', 'csrf_token': csrf_token},
+    )
     assert rv.status_code == 200
     assert rv.get_json() == {'status': 'ok'}
 
     # Taken usernames won't be available
-    rv = client.post(endpoint, data={'username': new_user.username})
+    rv = client.post(
+        endpoint,
+        data={'username': new_user.username, 'csrf_token': csrf_token},
+    )
     assert rv.status_code == 200  # Validation failures are not 400/422
     assert rv.get_json() == {
         'status': 'error',
@@ -29,7 +38,10 @@ def test_username_available(client, new_user):
     }
 
     # Misformatted usernames will render an explanatory error
-    rv = client.post(endpoint, data={'username': 'this is invalid'})
+    rv = client.post(
+        endpoint,
+        data={'username': 'this is invalid', 'csrf_token': csrf_token},
+    )
     assert rv.status_code == 200  # Validation failures are not 400/422
     assert rv.get_json() == {
         'status': 'error',
