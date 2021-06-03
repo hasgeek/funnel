23ec126c3ece5fb9c757b68dc40aa4656b170d5f
Kiran Jonnalagadda <kiran@hasgeek.com>
2021-06-04 00:36:16 +0530

    Add CHECK constraints for project and session dates (#1175)
    Co-authored-by: Vidya Ramakrishnan <vidya@hasgeek.in>

---
+++ migrations/versions/60ee1687f59c_project_cfp_end_at_after_cfp_start_at.py
@@ -0,0 +1,42 @@
+"""Project cfp_end_at after cfp_start_at.
+
+Revision ID: 60ee1687f59c
+Revises: 5f465411775c
+Create Date: 2021-06-03 15:34:31.913604
+
+"""
+
+from alembic import op
+from sqlalchemy.sql import column
+import sqlalchemy as sa
+
+cfp_start_at: column = column('start_at')
+cfp_end_at: column = column('end_at')
+
+# revision identifiers, used by Alembic.
+revision = '60ee1687f59c'
+down_revision = '5f465411775c'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_check_constraint(
+        'project_cfp_start_at_cfp_end_at_check',
+        'project',
+        sa.or_(
+            sa.and_(cfp_start_at.is_(None), cfp_end_at.is_(None)),
+            sa.and_(cfp_start_at.isnot(None), cfp_end_at.is_(None)),
+            sa.and_(
+                cfp_start_at.isnot(None),
+                cfp_end_at.isnot(None),
+                cfp_end_at > cfp_start_at,
+            ),
+        ),
+    )
+
+
+def downgrade():
+    op.drop_constraint(
+        'project_cfp_start_at_cfp_end_at_check', 'project', type_='check'
+    )
