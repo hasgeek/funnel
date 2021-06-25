hasgeek/funnel

commit 6e329de6925fc94d0d88e3db58839cdc12785621

Flask 2.0 session uses tz-aware datetimes (#1207)

funnel/views/notification_preferences.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import timedelta
 from typing import Optional
 
 from flask import abort, flash, redirect, request, session, url_for
@@ -7,7 +7,7 @@ import itsdangerous.exc
 from baseframe import _, __
 from baseframe.forms import render_form, render_message
 from coaster.auth import current_auth
-from coaster.utils import getbool
+from coaster.utils import getbool, utcnow
 from coaster.views import ClassView, render_with, requestargs, route
 
 from .. import app
@@ -246,8 +246,7 @@ class AccountNotificationView(ClassView):
         if token and not cookietest:
             session['temp_token'] = token
             session['temp_token_type'] = token_type
-            # Use naive datetime as the session can't handle tz-aware datetimes
-            session['temp_token_at'] = datetime.utcnow()
+            session['temp_token_at'] = utcnow()
             # These values are removed from the session in 10 minutes by
             # :func:`funnel.views.login_session.clear_expired_temp_token` if the user
             # abandons this page.
@@ -267,7 +266,7 @@ class AccountNotificationView(ClassView):
         if token and 'temp_token' not in session:
             session['temp_token'] = token
             session['temp_token_type'] = token_type
-            session['temp_token_at'] = datetime.utcnow()
+            session['temp_token_at'] = utcnow()
             return metarefresh_redirect(
                 url_for('notification_unsubscribe_do')
                 + ('?' + request.query_string.decode())
@@ -335,9 +334,7 @@ class AccountNotificationView(ClassView):
                 discard_temp_token()
                 flash(unsubscribe_link_invalid, 'error')
                 return redirect(url_for('notification_preferences'), code=303)
-            if payload['timestamp'].replace(
-                tzinfo=None
-            ) < datetime.utcnow() - timedelta(days=7):
+            if payload['timestamp'] < utcnow() - timedelta(days=7):
                 # Link older than a week. Expire it
                 discard_temp_token()
                 flash(unsubscribe_link_expired, 'error')

funnel/views/account_reset.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 from flask import (
     Markup,
     current_app,
@@ -17,7 +15,7 @@ from pytz import utc
 
 from baseframe import _
 from baseframe.forms import render_form, render_message
-from coaster.utils import getbool
+from coaster.utils import getbool, utcnow
 from coaster.views import requestargs
 
 from .. import app
@@ -132,8 +130,7 @@ def reset_email(token: str, cookietest=False) -> ReturnView:
     """Move token into session cookie and redirect to a token-free URL."""
     if not cookietest:
         session['temp_token'] = token
-        # Use naive datetime as the session can't handle tz-aware datetimes
-        session['temp_token_at'] = datetime.utcnow()
+        session['temp_token_at'] = utcnow()
         # Reconstruct current URL with ?cookietest=1 or &cookietest=1 appended
         # and reload the page
         if request.query_string:
@@ -143,7 +140,7 @@ def reset_email(token: str, cookietest=False) -> ReturnView:
         # Browser is refusing to set cookies on 302 redirects. Set it again and use
         # the less secure meta-refresh redirect (browser extensions can read the URL)
         session['temp_token'] = token
-        session['temp_token_at'] = datetime.utcnow()
+        session['temp_token_at'] = utcnow()
         return metarefresh_redirect(url_for('reset_email_do'))
     # implicit: cookietest is True and 'temp_token' in session
     return redirect(url_for('reset_email_do'))

funnel/views/login_session.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import timedelta
 from functools import wraps
 from typing import Optional, Type
 
@@ -254,13 +254,9 @@ def clear_expired_temp_token():
     :meth:`funnel.views.notification.AccountNotificationView.unsubscribe`.
     """
     if 'temp_token_at' in session:
-        # Use naive datetime as Flask 1.0 sessions can't handle tz-aware datetimes,
-        # while Flask 2.0 sessions convert naive datetimes into UTC.
         # Give the user 10 minutes to complete the action. Remove the token if it's
         # been longer than 10 minutes.
-        if session['temp_token_at'].replace(
-            tzinfo=None
-        ) < datetime.utcnow() - timedelta(minutes=10):
+        if session['temp_token_at'] < utcnow() - timedelta(minutes=10):
             discard_temp_token()
             current_app.logger.info("Cleared expired temp_token from session cookie")
     elif 'temp_token' in session:

commit 5a91f7584849dca2fd45597dfcfa3049acfbe638

Fix and test SMS unsubscribe view with new form submit wrapper (#1216)
The SMS unsubscribe view was broken by #1207 due to a mistaken assumption about datetimes. While the Flask 2.0 `session` object now stores tz-aware datetimes, Flask-Caching does not do this. Datetimes stored in Redis continue to be naive.

This PR fixes the bug and also introduces tests for the view, to confirm the SMS unsubscribe form is accessible and can be submitted successfully.

A new `ResponseWithForms` test helper class is introduced for the test client fixture, making it possible to test forms using the actually rendered HTML.

.pre-commit-config.yaml
@@ -51,21 +51,22 @@ repos:
     hooks:
       - id: mypy
         additional_dependencies:
-          - sqlalchemy-stubs
-          - types-certifi
-          - types-cryptography
-          - types-Flask
-          - types-geoip2
-          - types-itsdangerous
-          - types-maxminddb
-          - types-python-dateutil
-          - types-pytz
-          - types-requests
-          - types-setuptools
-          - types-simplejson
-          - types-six
-          - types-toml
-          - types-Werkzeug
+          - lxml-stubs==0.2.0
+          - sqlalchemy-stubs==0.4
+          - types-certifi==0.1.4
+          - types-cryptography==3.3.3
+          - types-Flask==1.1.1
+          - types-geoip2==0.1.2
+          - types-itsdangerous==1.1.2
+          - types-maxminddb==0.1.2
+          - types-python-dateutil==0.1.4
+          - types-pytz==2021.1.0
+          - types-requests==2.25.0
+          - types-setuptools==57.0.0
+          - types-simplejson==0.1.4
+          - types-six==0.1.7
+          - types-toml==0.1.3
+          - types-Werkzeug==1.0.2
   - repo: https://gitlab.com/pycqa/flake8
     rev: 3.9.2
     hooks:

funnel/views/notification.py
@@ -178,7 +178,7 @@ class RenderNotification:
         if unsubscribe_domain:
             # For this to work, a web server must listen on the unsubscribe domain and
             # redirect all paths to url_for('notification_unsubscribe_short') + path
-            return 'https://' + unsubscribe_domain + '/' + token
+            return f'https://{unsubscribe_domain}/{token}'
         return url_for('notification_unsubscribe_short', token=token, _external=True)
 
     @cached_property

funnel/views/notification_preferences.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from datetime import timedelta
+from datetime import datetime, timedelta
 from typing import Optional
 
 from flask import abort, flash, redirect, request, session, url_for
@@ -336,7 +336,13 @@ class AccountNotificationView(ClassView):
                 discard_temp_token()
                 flash(unsubscribe_link_invalid, 'error')
                 return redirect(url_for('notification_preferences'), code=303)
-            if payload['timestamp'] < utcnow() - timedelta(days=7):
+
+            # Flask-Caching still returns naive datetimes (unlike Flask 2.0 session),
+            # so we must compare with a naive datetime. We do a `.replace(tzinfo=None)`
+            # anyway as a safety catch in case this changes without notice later.
+            if payload['timestamp'].replace(
+                tzinfo=None
+            ) < datetime.utcnow() - timedelta(days=7):
                 # Link older than a week. Expire it
                 discard_temp_token()
                 flash(unsubscribe_link_expired, 'error')

instance/testing.py
@@ -1,7 +1,7 @@
 from os import environ
 
 TESTING = True
-CACHE_TYPE = 'null'
+CACHE_TYPE = 'SimpleCache'
 SECRET_KEYS = ['testkey']  # nosec
 LASTUSER_SECRET_KEYS = ['testkey']  # nosec
 SITE_TITLE = 'Hasgeek'
@@ -16,12 +16,12 @@ STATIC_SUBDOMAIN = 'static'
 LASTUSER_COOKIE_DOMAIN = '.funnel.travis.local:3002'
 UPLOAD_FOLDER = '/tmp'  # nosec  # noqa: S108
 TIMEZONE = 'Asia/Kolkata'
-ASSET_BASE_PATH = "build"
 GOOGLE_MAPS_API_KEY = environ.get('GOOGLE_MAPS_API_KEY')
 BOXOFFICE_SERVER = 'https://boxoffice.hasgeek.com/api/1/'
 
-ASSET_MANIFEST_PATH = "static/build/manifest.json"
-ASSET_BASE_PATH = "build"
+ASSET_MANIFEST_PATH = 'static/build/manifest.json'
+ASSET_BASE_PATH = 'build'
+UNSUBSCRIBE_DOMAIN = 'bye.test'
 #: Recaptcha for the registration form
 RECAPTCHA_USE_SSL = True
 RECAPTCHA_PUBLIC_KEY = environ.get('RECAPTCHA_PUBLIC_KEY')

tests/conftest.py
@@ -1,8 +1,13 @@
 from datetime import datetime
-from types import SimpleNamespace
+from types import MethodType, SimpleNamespace
+from typing import List, Optional
 
 from sqlalchemy import event
 
+from flask.testing import FlaskClient
+from flask.wrappers import Response
+
+from lxml.html import FormElement, HtmlElement, fromstring  # noqa: S410
 from pytz import utc
 import pytest
 
@@ -19,6 +24,95 @@ from funnel.models import (
     User,
     db,
 )
+
+# --- ResponseWithForms, to make form submission in the test client testing easier
+# --- Adapted from the abandoned Flask-Fillin package
+
+
+class ResponseWithForms(Response):
+    """
+    Wrapper for the test client response that makes form submission easier.
+
+    Usage::
+
+        def test_mytest(client):
+            response = client.get('/page_with_forms')
+            form = response.form('login')
+            form.fields['username'] = 'my username'
+            form.fields['password'] = 'secret'
+            form.fields['remember'] = True
+            next_response = form.submit(client)
+    """
+
+    _parsed_html = None
+
+    @property
+    def html(self) -> HtmlElement:
+        """Return the parsed HTML tree."""
+        if self._parsed_html is None:
+            self._parsed_html = fromstring(self.data)
+
+            # add click method to all links
+            def _click(self, client, **kwargs):
+                # `self` is the `a` element here
+                path = self.attrib['href']
+                return client.get(path, **kwargs)
+
+            for link in self._parsed_html.iter('a'):
+                setattr(link, 'click', MethodType(_click, link))
+
+            # add submit method to all forms
+            def _submit(self, client, path=None, **kwargs):
+                # `self` is the `form` element here
+                data = dict(self.form_values())
+                if 'data' in kwargs:
+                    data.update(kwargs['data'])
+                    del kwargs['data']
+                if path is None:
+                    path = self.action
+                if 'method' not in kwargs:
+                    kwargs['method'] = self.method
+                return client.open(path, data=data, **kwargs)
+
+            for form in self._parsed_html.forms:
+                setattr(form, 'submit', MethodType(_submit, form))
+        return self._parsed_html
+
+    @property
+    def forms(self) -> List[FormElement]:
+        """
+        Return list of all forms in the document.
+
+        Contains the LXML form type as documented at http://lxml.de/lxmlhtml.html#forms
+        with an additional `.submit(client)` method to submit the form.
+        """
+        return self.html.forms
+
+    def form(
+        self, id_: Optional[str] = None, name: Optional[str] = None
+    ) -> Optional[FormElement]:
+        """Return the first form matching given id or name in the document."""
+        if id_:
+            forms = self.html.cssselect(f'form#{id_}')
+        elif name:
+            forms = self.html.cssselect(f'form[name={name}]')
+        else:
+            forms = self.forms
+        if forms:
+            return forms[0]
+        return None
+
+    def links(self, selector: str = 'a') -> List[HtmlElement]:
+        """Get all the links matching the given CSS selector."""
+        return self.html.cssselect(selector)
+
+    def link(self, selector: str = 'a') -> Optional[HtmlElement]:
+        """Get first link matching the given CSS selector."""
+        links = self.links(selector)
+        if links:
+            return links[0]
+        return None
+
 
 # --- New fixtures, to replace the legacy tests below as tests are updated
 
@@ -76,7 +170,7 @@ def db_session(database, db_connection):
 def client(request, db_session):
     """Provide a test client."""
     with app.app_context():  # Not required for test_client, but required for autouse
-        with app.test_client() as test_client:
+        with FlaskClient(app, ResponseWithForms, use_cookies=True) as test_client:
             yield test_client
 
 

tests/unit/views/test_notification.py
@@ -1,0 +1,135 @@
+from urllib.parse import urlsplit
+
+from flask import url_for
+
+import pytest
+
+from funnel.models import (
+    NewUpdateNotification,
+    Notification,
+    NotificationPreferences,
+    Update,
+)
+
+
+@pytest.fixture
+def phone_vetinari(db_session, user_vetinari):
+    """Add a phone number to user_vetinari."""
+    userphone = user_vetinari.add_phone('+12345678900')
+    db_session.add(userphone)
+    db_session.commit()
+    return userphone
+
+
+@pytest.fixture
+def notification_prefs_vetinari(db_session, user_vetinari):
+    """Add main notification preferences for user_vetinari."""
+    prefs = NotificationPreferences(
+        user=user_vetinari,
+        notification_type='',
+        by_email=True,
+        by_sms=True,
+        by_webpush=True,
+        by_telegram=True,
+        by_whatsapp=True,
+    )
+    db_session.add(prefs)
+    db_session.commit()
+    return prefs
+
+
+@pytest.fixture
+def project_update(db_session, user_vetinari, project_expo2010):
+    """Create an update to add a notification for."""
+    db_session.commit()
+    update = Update(
+        project=project_expo2010,
+        user=user_vetinari,
+        title="New update",
+        body="New update body",
+    )
+    db_session.add(update)
+    db_session.commit()
+    update.publish(user_vetinari)
+    db_session.commit()
+    return update
+
+
+@pytest.fixture
+def update_user_notification(db_session, user_vetinari, project_update):
+    """Get a user notification for the update fixture."""
+    notification = NewUpdateNotification(project_update)
+    db_session.add(notification)
+    db_session.commit()
+
+    # Extract all the user notifications
+    all_user_notifications = list(notification.dispatch())
+    db_session.commit()
+    # There should be only one, assigned to Vetinari, but we'll let the test confirm
+    return all_user_notifications[0]
+
+
+def test_user_notification_is_for_user_vetinari(
+    update_user_notification, user_vetinari
+):
+    """Confirm the test notification is for the test user fixture."""
+    assert update_user_notification.user == user_vetinari
+
+
+@pytest.fixture
+def notification_view(update_user_notification):
+    """Get the notification view renderer."""
+    return Notification.renderers[update_user_notification.notification.type](
+        update_user_notification
+    )
+
+
+@pytest.fixture
+def unsubscribe_sms_short_url(
+    notification_view, phone_vetinari, notification_prefs_vetinari
+):
+    """Get an unsubscribe URL for the SMS notification."""
+    return notification_view.unsubscribe_short_url('sms')
+
+
+def test_unsubscribe_view_is_well_formatted(unsubscribe_sms_short_url, user_vetinari):
+    """Confirm the SMS unsubscribe URL is well formatted."""
+    prefix = 'https://bye.test/'
+    assert unsubscribe_sms_short_url.startswith(prefix)
+    assert len(unsubscribe_sms_short_url) == len(prefix) + 4  # 4 char random value
+
+
+def test_unsubscribe_sms_view(client, unsubscribe_sms_short_url, user_vetinari):
+    """Confirm the unsubscribe URL renders a form."""
+    unsub_url = url_for(
+        'notification_unsubscribe_short',
+        token=urlsplit(unsubscribe_sms_short_url).path[1:],
+        _external=True,
+    )
+
+    # Get the unsubscribe URL. This should cause a cookie to be set, with a
+    # redirect to the same URL and `?cookietest=1` appended
+    rv = client.get(unsub_url)
+    assert rv.status_code == 302
+    assert rv.location.startswith(unsub_url)
+    assert rv.location.endswith('cookietest=1')
+
+    # Follow the redirect. This will cause yet another redirect
+    rv = client.get(rv.location)
+    assert rv.status_code == 302
+    assert rv.location == url_for('notification_unsubscribe_do', _external=True)
+
+    # This time we'll get the unsubscribe form.
+    rv = client.get(rv.location)
+    assert rv.status_code == 200
+
+    # Assert the user has SMS notifications enabled, and the form agrees
+    assert user_vetinari.main_notification_preferences.by_sms is True
+    form = rv.form('form-unsubscribe-preferences')
+    assert form.fields['main'] == 'y'
+    form.fields['main'] = False
+    rv = form.submit(client)
+    # We'll now get an acknowledgement
+    assert rv.status_code == 200
+    # And the user's preferences will be turned off
+    assert user_vetinari.main_notification_preferences.by_sms is False