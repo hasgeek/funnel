41c260aac0967b3509fb40c305774cf64d4a4280
Vidya Ramakrishnan <vidya@hasgeek.in>
2021-06-30 16:26:14 +0530

    Latest app.css

--- runfrontendtests.sh
+++ runfrontendtests.sh
@@ -6,3 +6,11 @@ if [ -f secrets.test ]; then
         source ./secrets.test
 fi
 python -m tests.e2e.frontend_tests_initdb
+nohup flask run -p 3002 2>&1 1>/dev/null & echo $! > /tmp/server.pid
+nohup ./rq.sh 2>&1 1>/dev/null & echo $! > /tmp/rq.pid
+cd funnel/assets
+npx cypress run --browser chrome
+kill -9 `cat /tmp/rq.pid`
+kill -9 `cat /tmp/server.pid`
+cd ../..
+python -m tests.e2e.frontend_tests_dropdb
