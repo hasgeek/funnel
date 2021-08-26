d444e87e052d179b61a0f4adc3cd2808002d2bbd
Kiran Jonnalagadda <kiran@hasgeek.com>
2021-08-26 23:13:50 +0530

    Reorganize API views into separate folder (#1269)

--- funnel/views/index.py
+++ funnel/views/index.py
@@ -174,16 +174,6 @@ def policy(path):
     )
 
 
-@app.route('/api/1/template/offline')
-def offline():
-    return render_template('offline.html.jinja2')
-
-
-@app.route('/service-worker.js')
-def sw():
-    return app.send_static_file('build/js/service-worker.js')
-
-
 @app.route('/manifest.json')
 @app.route('/manifest.webmanifest')
 def manifest():
