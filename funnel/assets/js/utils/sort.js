0b9e23e3b9da9c2c40177af8b0d1e97607409c0a
Vidya Ramakrishnan <vidya@hasgeek.in>
2021-10-01 14:04:38 +0530

    Sort collaborators

---
+++ funnel/assets/js/utils/sort.js
@@ -0,0 +1,46 @@
+import Form from './formhelper';
+
+function sortItem(wrapperJqueryElem, placeholderClass, sortUrl) {
+  let oldList;
+  let target;
+  let other;
+  let before;
+  let newList;
+  let newPosition;
+  wrapperJqueryElem.sortable({
+    handle: '.drag-handle',
+    placeholder: placeholderClass,
+    forcePlaceholderSize: true,
+    revert: true,
+    scroll: true,
+    start(event, ui) {
+      $(`.${placeholderClass}`).height($(ui.item).height());
+      oldList = $(this).sortable('toArray');
+    },
+    update(event, ui) {
+      // True if moved up
+      before = !(ui.position.top - ui.originalPosition.top > 0);
+      newList = $(this).sortable('toArray');
+      target = $(ui.item).attr('id');
+      newPosition = newList.indexOf(target);
+      other = oldList[newPosition];
+      $.ajax({
+        url: sortUrl,
+        type: 'POST',
+        data: {
+          csrf_token: $('meta[name="csrf-token"]').attr('content'),
+          target,
+          other,
+          before,
+        },
+        dataType: 'json',
+        error(errorResponse) {
+          Form.handleAjaxError(errorResponse);
+          wrapperJqueryElem.sortable('cancel');
+        },
+      });
+    },
+  });
+}
+
+export default sortItem;
