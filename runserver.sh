ab76dc1bb473d65c1547c1175c726b887abcd944
vivekdurai <vivek@lexaude.com>
2021-10-30 14:21:03 +0530

    .

--- runserver.sh
+++ runserver.sh
@@ -3,7 +3,7 @@
 # For macOS: https://stackoverflow.com/a/52230415/78903
 export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
 
-export FLASK_ENV=development
+export FLASK_ENV=production
 export FLASK_RUN_HOST="${FLASK_RUN_HOST:=0.0.0.0}"
-export FLASK_RUN_PORT="${FLASK_RUN_PORT:=3000}"
+export FLASK_RUN_PORT="${FLASK_RUN_PORT:=5000}"
 flask run
