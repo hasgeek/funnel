4e36bd638bf1acbf0862320da85384e555001ff1
vivekdurai <vivek@lexaude.com>
2021-10-31 14:36:40 +0530

    .

--- runserver.sh
+++ runserver.sh
@@ -5,4 +5,5 @@ export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
 export FLASK_ENV=production
 export FLASK_RUN_HOST="${FLASK_RUN_HOST:=0.0.0.0}"
 export FLASK_RUN_PORT="${FLASK_RUN_PORT:=5000}"
+export REDIS_URL="redis://redis:6379/0"
 flask run
