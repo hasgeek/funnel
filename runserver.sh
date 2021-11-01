4bc29e5193e3f682a9da9d13b0c64568ce07dd6a
vivekdurai <vivek@lexaude.com>
2021-11-01 18:48:42 +0530

    cache_redis_url

--- runserver.sh
+++ runserver.sh
@@ -2,8 +2,9 @@
 
 # For macOS: https://stackoverflow.com/a/52230415/78903
 export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
-export FLASK_ENV=production
+export FLASK_ENV=development
 export FLASK_RUN_HOST="${FLASK_RUN_HOST:=0.0.0.0}"
 export FLASK_RUN_PORT="${FLASK_RUN_PORT:=5000}"
 export REDIS_URL="redis://redis:6379/0"
+export CACHE_REDIS_URL="redis://redis:6379/0"
 flask run
