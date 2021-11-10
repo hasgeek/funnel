49c8fc60196209801bab13c6c67f281ca2795817
Kiran Jonnalagadda <kiran@hasgeek.com>
2021-11-10 13:03:33 +0530

    Redis config should be in app config, not system env

--- runserver.sh
+++ runserver.sh
@@ -5,6 +5,4 @@ export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
 export FLASK_ENV=development
 export FLASK_RUN_HOST="${FLASK_RUN_HOST:=0.0.0.0}"
 export FLASK_RUN_PORT="${FLASK_RUN_PORT:=3000}"
-export REDIS_URL="redis://redis:6379/0"
-export CACHE_REDIS_URL="redis://redis:6379/0"
 flask run
