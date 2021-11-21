74ffb72c1eb6397b6954bf33914225f8711bb2b5
vivekdurai <vivek@lexaude.com>
2021-11-21 20:23:57 +0530

    added a new runtests version for docker called docker-tests.sh

--- docker-tests.sh
+++ docker-tests.sh
@@ -2,12 +2,12 @@
 set -e
 export FLASK_ENV="TESTING"
 
-sudo -- sh -c "echo '127.0.0.1  funnel.test' >> /etc/hosts"
-sudo -- sh -c "echo '127.0.0.1  f.test' >> /etc/hosts"
+echo '127.0.0.1  funnel.test' >> /etc/hosts
+echo '127.0.0.1  f.test' >> /etc/hosts
 psql postgresql://postgres:5432/postgres -c 'create database funnel_testing;'
-'flask dbconfig | sudo -u postgres psql funnel_testing'
+flask dbconfig | sudo -u postgres psql funnel_testing
 psql  postgresql://postgres:5432/postgres -c 'create database geoname_testing;'
-'flask dbconfig | sudo -u postgres psql geoname_testing'
+flask dbconfig | sudo -u postgres psql geoname_testing
 
 if [ -f secrets.test ]; then
 	source ./secrets.test
