7da31175e45d2641166c371424650239140e7363
vivekdurai <vivek@lexaude.com>
2021-11-23 19:41:00 +0530

    ssh deploy

--- aws_dev_deploy.sh
+++ aws_dev_deploy.sh
@@ -7,6 +7,7 @@ ssh ec2-user@3.144.184.165 "
         docker-compose down
         git fetch origin
         git reset --hard origin/aws  &&  echo 'git fetched and hard reset'
+        aws s3 cp s3://v2-alpha-test-environment-files/container_env instance/settings.py
         docker-compose build 
         docker run vivekdurai/funnel flask db upgrade
         docker-compose up -d
