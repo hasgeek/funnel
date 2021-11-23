b07ba6e4dc54f8c7ecf8aa5b6491d756cdf99659
vivekdurai <vivek@lexaude.com>
2021-11-23 18:28:25 +0530

    ssh deploy

--- aws_dev_deploy.sh
+++ aws_dev_deploy.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 echo 'Starting to Deploy...'
-ssh -o StrictHostKeyChecking=no ec2-user@ec2-3-144-184-165.us-east-2.compute.amazonaws.com "ls"
+ssh -o StrictHostKeyChecking=no ec2-user@ec2-3-144-184-165.us-east-2.compute.amazonaws.com "
+		docker image prune -f 
+        cd funnel
+        docker-compose down
+        git fetch origin
+        git reset --hard origin/aws  &&  echo 'git fetched and hard reset'
+        docker-compose build 
+        docker run web flask db upgrade
+        docker-compose up -d
+        "
 echo 'Deployed to staging without db update successfully'
