c7af6c017cb1eb4b2c50f460ae0111f1ad96849b
vivekdurai <vivek@lexaude.com>
2021-11-23 18:07:45 +0530

    ssh deploy

--- aws_dev_deploy.sh
+++ aws_dev_deploy.sh
@@ -5,6 +5,8 @@ ssh -o StrictHostKeyChecking=no ec2-user@ec2-3-144-184-165.us-east-2.compute.ama
         sudo docker-compose down
         git fetch origin
         git reset --hard origin/develop  &&  echo 'git fetched and hard reset'
-        sudo docker-compose build && sudo docker-compose up -d
+        sudo docker-compose build 
+        sudo docker exec -it web flask db upgrade
+        sudo docker-compose up -d
         "
 echo 'Deployed to staging without db update successfully'
