df311e04e38487d8dea8d1afddbfded1901195f0
vivekdurai <vivek@lexaude.com>
2021-11-23 19:25:50 +0530

    ssh deploy

--- aws_dev_deploy.sh
+++ aws_dev_deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 echo 'Starting to Deploy...'
-ssh ec2-user@ec2-3-144-184-165.us-east-2.compute.amazonaws.com "
+ssh -i ../private.key ec2-user@ec2-3-144-184-165.us-east-2.compute.amazonaws.com "
 		docker image prune -f 
         cd funnel
         docker-compose down
