9a277afbc5215e45e959495dcfae96232850242a
vivekdurai <vivek@lexaude.com>
2021-11-24 13:20:33 +0530

    ssh action added to yml

--- aws_dev_deploy.sh
+++ aws_dev_deploy.sh
@@ -5,7 +5,7 @@ ssh ec2-user@3.144.184.165 "
 		docker image prune -f 
         cd funnel
         docker-compose down
-        git fetch origin
+        git fetch origin aws
         git reset --hard origin/aws  &&  echo 'git fetched and hard reset'
         aws s3 cp s3://v2-alpha-test-environment-files/container_env instance/settings.py
         docker-compose build 
