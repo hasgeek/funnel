27d791d806ad20dd4bf57a731bcbe47234106ca4
Kiran Jonnalagadda <kiran@hasgeek.com>
2021-11-29 14:14:32 +0530

    pre-commit fixes

--- aws_dev_deploy.sh
+++ aws_dev_deploy.sh
@@ -2,13 +2,13 @@
 set -e
 echo 'Starting to Deploy...'
 ssh ec2-user@3.144.184.165 "
-		docker image prune -f 
+		docker image prune -f
         cd funnel
         docker-compose down
         git fetch origin aws
         git reset --hard origin/aws  &&  echo 'git fetched and hard reset'
         aws s3 cp s3://v2-alpha-test-environment-files/container_env instance/settings.py
-        docker-compose build 
+        docker-compose build
         docker run vivekdurai/funnel flask db upgrade
         docker-compose up -d
         "
