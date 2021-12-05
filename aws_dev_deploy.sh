6a34a3f656e8c0f1607dda5f2b3bd7711f8839d3
Vivek Durai <vivek@lexaude.com>
2021-12-05 18:43:36 +0530

    replaced s3 container env with github secrets

--- aws_dev_deploy.sh
+++ aws_dev_deploy.sh
@@ -2,14 +2,24 @@
 set -e
 echo 'Starting to Deploy...'
 ssh ec2-user@3.144.184.165 "
-		docker image prune -f 
-        cd funnel
-        docker-compose down
-        git fetch origin aws
-        git reset --hard origin/aws  &&  echo 'git fetched and hard reset'
-        aws s3 cp s3://v2-alpha-test-environment-files/container_env instance/settings.py
-        docker-compose build 
-        docker run vivekdurai/funnel flask db upgrade
-        docker-compose up -d
+          echo 'pruning unused docker images'
+          docker image prune -f 
+          #echo 'changing folder to funnel'
+          cd funnel
+          echo 'docker-compose down'
+          docker-compose down
+          echo 'fetching git repo'
+          git fetch origin aws
+          echo 'reset git repo'
+          git reset --hard origin/aws  &&  echo 'git fetched and hard reset'
+          echo 'fetching settings.py from s3'
+          aws s3 cp s3://v2-alpha-test-environment-files/container_env /app/instance/settings.py
+          #echo '${{ secrets.SERVER_ENV_DEV }}' > instance/settings.py
+          ls instance
+          cat /app/instance/settings.py
+          echo 'running migration'
+          docker run --mount src="$(pwd)",target=/app:Z,type=bind vivekdurai/funnel flask db upgrade
+          echo 'starting docker-compose'
+          docker-compose up -f docker-compose.staging.yml -d
         "
 echo 'Deployed to staging without db update successfully'
