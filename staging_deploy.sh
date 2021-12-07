ceadad1cec3fa30bdcdaaa812dcf4f0773fe63d9
Vivek Durai <vivek@lexaude.com>
2021-12-07 06:49:37 +0530

    staging deployment via fabric

--- staging_deploy.sh
+++ staging_deploy.sh
@@ -1,17 +1,17 @@
-echo 'pruning unused docker images'
+echo "pruning unused docker images"
 docker image prune -f
-echo 'changing folder to funnel'
+echo "changing folder to funnel"
 cd funnel
-echo 'docker-compose down
+echo "docker-compose down"
 docker-compose down
-echo 'fetching git repo'
+echo "fetching git repo"
 git fetch origin aws
-echo 'reset git repo'
-git reset --hard origin/aws  &&  echo 'git fetched and hard reset'
-echo 'fetching settings.py from s3://v2-alpha-test-environment-files'
+echo "reset git repo"
+git reset --hard origin/aws && echo "git fetched and hard reset"
+echo "fetching settings.py"
 aws s3 cp s3://v2-alpha-test-environment-files/container_env instance/settings.py
 ls instance
 cat instance/settings.py
-echo 'starting docker-compose'
+echo "starting docker-compose"
 docker-compose up -f docker-compose.staging.yml -d
-echo 'Deployed to staging without db update successfully'
+echo "Deployed to staging without db update successfully"
