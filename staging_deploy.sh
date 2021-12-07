03168a96eed9741b3265543d8bc54208411e25bd
Vivek Durai <vivek@lexaude.com>
2021-12-07 07:18:35 +0530

    staging deployment via fabric

--- staging_deploy.sh
+++ staging_deploy.sh
@@ -5,9 +5,9 @@ cd funnel
 echo "docker-compose down"
 docker-compose down
 echo "fetching git repo"
-git fetch origin aws
-echo "reset git repo"
-git reset --hard origin/aws && echo "git fetched and hard reset"
+git pull
+#echo "reset git repo"
+#git reset --hard origin/aws && echo "git fetched and hard reset"
 echo "fetching settings.py"
 aws s3 cp s3://v2-alpha-test-environment-files/container_env instance/settings.py
 ls instance
