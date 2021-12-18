06d4cce60317d2f851fab2aae606619e200b2af9
Vivek Durai <vivek@lexaude.com>
2021-12-18 18:18:25 +0530

    fab commands and staging database snapshot

--- staging-restore-db-from-s3.sh
+++ staging-restore-db-from-s3.sh
@@ -16,8 +16,8 @@ ls instance
 #echo "starting docker-compose"
 aws s3 cp s3://v2-alpha-test-environment-files/container_env instance/settings.py
 echo "starting the db"
-docker-compose -f docker-compose.staging.yml up -d db
-DOCKER_DB_NAME="$(docker-compose ps -q db)"
+docker-compose -f docker-compose.staging.yml up -d postgres
+DOCKER_DB_NAME="$(docker-compose ps -q postgres)"
 echo $DOCKER_DB_NAME 
 # get the latest backup from s3 and save it to instantbackups
 s3bucket = "s3://hasgeek-instant-db-backup/$(date +'%Y-%m')/instant-$(date +'%Y-%m-%d')"
@@ -26,5 +26,5 @@ echo $latestfile
 LOCAL_DUMP_PATH = "/tmp/instantbackups"
 aws s3 cp ${s3bucket}${latestfile} $LOCAL_DUMP_PATH
 docker exec -i "${DOCKER_DB_NAME}" pg_restore -C --clean --no-acl --no-owner  < "${LOCAL_DUMP_PATH}"
-docker-compose stop db
+docker-compose stop postgres --remove-orphans
 docker-compose -f docker-compose.staging.yml up -d
