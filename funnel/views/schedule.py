b6ee1b67f3fd739298fbd1bf4b0854684ea6c4a9
Kiran Jonnalagadda <kiran@hasgeek.com>
2021-12-20 15:14:42 +0530

    Add without_parent dataset; add render_with

--- funnel/views/schedule.py
+++ funnel/views/schedule.py
@@ -309,8 +309,9 @@ class ProjectScheduleView(ProjectViewMixin, UrlChangeCheck, UrlForView, ModelVie
     @route('update', methods=['POST'])
     @requires_login
     @requires_roles({'editor'})
+    @render_with(json=True)
     @requestargs(('sessions', json.loads))
-    def update_schedule(self, sessions) -> ReturnView:
+    def update_schedule(self, sessions) -> ReturnRenderWith:
         for session in sessions:
             try:
                 s = Session.query.filter_by(
