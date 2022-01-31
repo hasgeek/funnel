181fbf3640aa1904667f1db0da0cf5675999d2ab
Kiran Jonnalagadda <kiran@hasgeek.com>
2022-01-31 14:27:00 +0530

    Add labels for CfP state groups

--- funnel/models/project.py
+++ funnel/models/project.py
@@ -402,12 +402,25 @@ class Project(UuidMixin, BaseScopedNameMixin, db.Model):
     )
 
     cfp_state.add_state_group(
-        'OPENABLE', cfp_state.CLOSED, cfp_state.DRAFT, cfp_state.EXPIRED
+        'OPENABLE',
+        cfp_state.CLOSED,
+        cfp_state.DRAFT,
+        cfp_state.EXPIRED,
+        label=('openable', __("Openable")),
     )
     cfp_state.add_state_group(
-        'TOGGLEABLE', cfp_state.PUBLIC, cfp_state.CLOSED, cfp_state.DRAFT
+        'TOGGLEABLE',
+        cfp_state.PUBLIC,
+        cfp_state.CLOSED,
+        cfp_state.DRAFT,
+        label=('toggleable', __("Toggleable")),
     )
-    cfp_state.add_state_group('UNAVAILABLE', cfp_state.CLOSED, cfp_state.EXPIRED)
+    cfp_state.add_state_group(
+        'UNAVAILABLE',
+        cfp_state.CLOSED,
+        cfp_state.EXPIRED,
+        label=('unavailable', __("Unavailable")),
+    )
 
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
