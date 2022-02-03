64f9d7525c1295eb36a53653aa8dd5516d7aa8ae
Vidya Ramakrishnan <vidya@hasgeek.in>
2022-02-03 23:33:24 +0530

    Simplify open submission workflow (#1322)
    * Add submission open/off slider
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Simplify CfP states; remove non-tests
    
    * Change CfP transition to a toggle
    
    * Add functionality to toggle cfp submission
    
    * Disable cfp on/off when there is no guidelines
    
    * Redefine CfP state OPENABLE
    
    * Let OPENABLE also include EXPIRED
    
    * Add a TOGGLEABLE CfP state
    
    * Add labels for CfP state groups
    
    * Add submission btn component
    
    * Use cfp state TOGGLEABLE to enable/disable toggle
    
    * Enable toggle even if instructions is null
    
    * Enable open submission toggle
    
    * Dynamic added toggle for spa
    
    * change to cfp_end_at
    
    * Remove submission status text
    
    * Use cfp state
    
    * Fix test
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Kiran Jonnalagadda <kiran@hasgeek.com>

--- funnel/assets/js/cfp_form.js
+++ funnel/assets/js/cfp_form.js
@@ -1,17 +1,5 @@
-$(() => {
-  // Set current date & time in cfp_start_at input field and show cfp_end_at input field.
-  $('button[name="open-now"]').click((event) => {
-    event.preventDefault();
-    const now = new Date(
-      new Date().getTime() + new Date().getTimezoneOffset() * -60 * 1000
-    );
-    $('#cfp_start_at').val(now.toISOString().slice(0, 16));
-    $('.js-cfp-start-at, .js-cfp-end-at').removeClass('mui--hide');
-  });
+import Form from './utils/formhelper';
 
-  // Show cfp_start_at and cfp_end_at input field.
-  $('button[name="later"]').click((event) => {
-    event.preventDefault();
-    $('.js-cfp-start-at, .js-cfp-end-at').removeClass('mui--hide');
-  });
+$(() => {
+  Form.openSubmissionToggle('#open-sub', '.js-cfp-status');
 });
