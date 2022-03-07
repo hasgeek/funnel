126dbf7554eb7b648dc1a002f8cb51c5a292aca7
Vidya Ramakrishnan <vidya@hasgeek.in>
2022-03-07 17:00:05 +0530

    Add comment

--- funnel/views/helpers.py
+++ funnel/views/helpers.py
@@ -42,6 +42,7 @@ valid_timezones = set(common_timezones)
 
 nocache_expires = utc.localize(datetime(1990, 1, 1))
 
+# Six avatar colours defined in _variable.scss
 no_avatar_colours = 6
 
 # --- Utilities ------------------------------------------------------------------------
