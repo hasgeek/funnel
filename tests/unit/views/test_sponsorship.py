ce8ed1334735cbfc00a9a0be58a05f67368566b0
Amogh M Aradhya <amoghmanjunath99@gmail.com>
2022-04-04 14:15:42 +0530

    Updated test_sponsorship.py

--- tests/unit/views/test_sponsorship.py
+++ tests/unit/views/test_sponsorship.py
@@ -4,59 +4,63 @@ from funnel.models import SiteMembership, SponsorMembership, db
 
 
 @pytest.fixture
-def sponsor(user_vetinari, org_ankhmorpork, project_expo2010):
-    sponsor = SponsorMembership(
+def org_uu(user_vetinari, org_ankhmorpork, project_expo2010):
+    org_uu = SponsorMembership(
         granted_by=user_vetinari,
         profile=org_ankhmorpork.profile,
         project=project_expo2010,
         is_promoted=True,
         label="Diamond",
     )
-    db.session.add(sponsor)
+    db.session.add(org_uu)
     db.session.commit()
-    return sponsor
+    return org_uu
 
 
 @pytest.fixture
-def site_editor(user_vetinari):
-    site_editor = SiteMembership(
+def user_vetinari_site_editor(user_vetinari):
+    user_vetinari_site_editor = SiteMembership(
         user=user_vetinari, granted_by=user_vetinari, is_site_editor=True
     )
-    return site_editor
+    return user_vetinari_site_editor
 
 
-@pytest.mark.parametrize(['is_site_editor', 'code'], [(True, 200), (False, 403)])
-def test_check_site_editor_edit_sponsor(
-    site_editor, client, is_site_editor, sponsor, user_vetinari, code
-):
-    user_vetinari.is_site_editor = is_site_editor
-    db.session.add(site_editor)
+@pytest.fixture
+def user_twoflower_not_site_editor(user_twoflower):
+    user_twoflower_not_site_editor = SiteMembership(
+        user=user_twoflower, granted_by=user_twoflower, is_comment_moderator=True
+    )
+    return user_twoflower_not_site_editor
+
+
+@pytest.mark.parametrize(
+    ['user', 'code'],
+    [('user_vetinari_site_editor', 200), ('user_twoflower_not_site_editor', 403)],
+)
+def test_check_site_editor_edit_sponsor(client, org_uu, code, user, request):
+    db.session.add(request.getfixturevalue(user))
     db.session.commit()
-
-    endpoint = sponsor.url_for('edit_sponsor')
+    endpoint = org_uu.url_for('edit_sponsor')
     with client.session_transaction() as session:
-        session['userid'] = user_vetinari.userid
+        session['userid'] = request.getfixturevalue(user).user.userid
     rv = client.get(endpoint)
     assert rv.status_code == code
 
 
 def test_sponsor_edit(
     client,
-    sponsor,
+    org_uu,
     user_vetinari,
-    project_expo2010,
-    org_ankhmorpork,
-    new_user,
-    site_editor,
+    user_vetinari_site_editor,
 ):
-    db.session.add(site_editor)
+    db.session.add(user_vetinari_site_editor)
     db.session.commit()
-    endpoint = sponsor.url_for('edit_sponsor')
+    endpoint = org_uu.url_for('edit_sponsor')
     csrf_token = client.get('/api/baseframe/1/csrf/refresh').get_data(as_text=True)
     with client.session_transaction() as session:
-        session['userid'] = user_vetinari.userid
+        session['userid'] = user_vetinari_site_editor.user.userid
     data = {
-        'profile': org_ankhmorpork.name,
+        'profile': org_uu.profile.name,
         'label': 'diamond-edited',
         'is_promoted': False,
         'csrf_token': csrf_token,
@@ -66,7 +70,7 @@ def test_sponsor_edit(
 
     edited_sponsor = (
         SponsorMembership.query.filter(SponsorMembership.is_active)
-        .filter_by(profile_id=sponsor.profile.id)
+        .filter_by(profile_id=org_uu.profile.id)
         .one_or_none()
     )
     assert edited_sponsor.label == 'diamond-edited'
@@ -75,16 +79,13 @@ def test_sponsor_edit(
 
 def test_sponsor_remove(
     client,
-    sponsor,
+    org_uu,
     user_vetinari,
-    project_expo2010,
-    org_ankhmorpork,
-    new_user,
-    site_editor,
+    user_vetinari_site_editor,
 ):
-    db.session.add(site_editor)
+    db.session.add(user_vetinari_site_editor)
     db.session.commit()
-    endpoint = sponsor.url_for('remove_sponsor')
+    endpoint = org_uu.url_for('remove_sponsor')
     csrf_token = client.get('/api/baseframe/1/csrf/refresh').get_data(as_text=True)
     with client.session_transaction() as session:
         session['userid'] = user_vetinari.userid
@@ -95,6 +96,6 @@ def test_sponsor_remove(
     assert resp_post.status_code == 302
 
     removed_sponsor = SponsorMembership.query.filter_by(
-        profile_id=sponsor.profile.id
+        profile_id=org_uu.profile.id
     ).one_or_none()
     assert removed_sponsor.is_active is False
