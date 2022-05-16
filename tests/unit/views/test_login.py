1568795636ac283e566fe8ea85cdf577eb3c701a
Kiran Jonnalagadda <kiran@hasgeek.com>
2022-05-16 11:15:03 +0530

    Change user fixture to user_rincewind

--- tests/unit/views/test_login.py
+++ tests/unit/views/test_login.py
@@ -3,20 +3,26 @@ from werkzeug.datastructures import MultiDict
 import pytest
 
 from coaster.auth import current_auth
-from funnel.models import User
 
-fullname = 'wheatwater'
-password = 'f7kN{$a58p^AmL@$'  # noqa: S105
-email = 'wheatwater@gmail.com'
+test_passwords = {'rincewind': 'rincewind-password'}
+
+complex_test_password = 'f7kN{$a58p^AmL@$'  # noqa: S105
 
 
 @pytest.fixture
-def user_wheatwater(db_session):
-    user = User(fullname=fullname, password=password)
-    db_session.add(user)
-    user.add_email(email)
-    db_session.commit()
-    yield user
+def user_rincewind_with_password(user_rincewind):
+    user_rincewind.password = test_passwords['rincewind']
+    return user_rincewind
+
+
+@pytest.fixture
+def user_rincewind_phone(user_rincewind):
+    return user_rincewind.add_phone('+12345678901')
+
+
+@pytest.fixture
+def user_rincewind_email(user_rincewind):
+    return user_rincewind.add_email('rincewind@example.com')
 
 
 def test_user_register(client):
@@ -26,20 +32,21 @@ def test_user_register(client):
         data=MultiDict(
             {
                 'csrf_token': csrf_token,
-                'fullname': fullname,
-                'email': email,
-                'password': password,
-                'confirm_password': password,
+                'fullname': "Test User",
+                'email': 'email@example.com',
+                'password': complex_test_password,
+                'confirm_password': complex_test_password,
             }
         ),
     )
 
     assert rv.status_code == 303
-    assert current_auth.user.fullname == fullname
+    assert current_auth.user.fullname == "Test User"
 
 
-def test_user_logout(client, user_wheatwater, login):
-    login.as_(user_wheatwater)
+def test_user_logout(client, login, user_rincewind):
+    login.as_(user_rincewind)
+    assert current_auth.user == user_rincewind
     csrf_token = client.get('/api/baseframe/1/csrf/refresh').get_data(as_text=True)
     rv = client.post('/account/logout', data={'csrf_token': csrf_token})
 
@@ -47,14 +54,14 @@ def test_user_logout(client, user_wheatwater, login):
     assert current_auth.user is None
 
 
-def test_user_login_correct_password(client, user_wheatwater):
+def test_user_login_correct_password(client, user_rincewind, user_rincewind_email):
     csrf_token = client.get('/api/baseframe/1/csrf/refresh').get_data(as_text=True)
     rv = client.post(
         '/login',
         data=MultiDict(
             {
-                'username': email,
-                'password': password,
+                'username': str(user_rincewind_email),
+                'password': test_passwords['rincewind'],
                 'csrf_token': csrf_token,
                 'form.id': 'passwordlogin',
             }
@@ -62,4 +69,4 @@ def test_user_login_correct_password(client, user_wheatwater):
     )
 
     assert rv.status_code == 303
-    assert current_auth.user.fullname == fullname
+    assert current_auth.user == user_rincewind
