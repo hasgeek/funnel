47210fdfa13991577b7da777890bf0d68216ba9e
Amogh M Aradhya <amoghmanjunath99@gmail.com>
2022-05-18 16:03:06 +0530

    Refactored login test

--- tests/unit/views/test_login.py
+++ tests/unit/views/test_login.py
@@ -1,3 +1,5 @@
+from itertools import product
+
 from werkzeug.datastructures import MultiDict
 
 import pytest
@@ -5,13 +7,44 @@ import pytest
 from coaster.auth import current_auth
 
 test_passwords = {'rincewind': 'rincewind-password'}
-
 complex_test_password = 'f7kN{$a58p^AmL@$'  # noqa: S105
+wrong_password = 'wrong_password'  # noqa: S105
+
+logins = ['rincewind', 'rincewind@example.com', '+12345678901']
+passwords_with_status = [
+    {
+        'password': complex_test_password,
+        'status_code': 303,
+        'auth': True,
+    },
+    {'password': wrong_password, 'status_code': 200, 'auth': False},
+]
+
+# sms_response = {
+#     "SMSMessage": {
+#         "Sid": "0f477d60517e6e6a0f6d9a7e9af8630e",
+#         "AccountSid": "Exotel",
+#         "From": "0XXXXXX4890/WEBDEV",
+#         "To": "0XXXXX30240",
+#         "DateCreated": "2017-03-03 14:14:20",
+#         "DateUpdated": "2017-03-03 14:14:20",
+#         "DateSent": 'null',
+#         "Body": random_otp(),
+#         "Direction": "outbound-api",
+#         "Uri": "/v1/Accounts/Exotel/SMS/Messages/0f477d60517e6e6a0f6d9a7e9af8630e.json",
+#         "ApiVersion": 'null',
+#         "Price": 'null',
+#         "Status": "queued",
+#         "DetailedStatusCode": "21010",
+#         "DetailedStatus": "PENDING_TO_OPERATOR",
+#         "SmsUnits": 'null',
+#     }
+# }
 
 
 @pytest.fixture
 def user_rincewind_with_password(user_rincewind):
-    user_rincewind.password = test_passwords['rincewind']
+    user_rincewind.password = complex_test_password
     return user_rincewind
 
 
@@ -45,8 +78,7 @@ def test_user_register(client, csrf_token):
 
 def test_user_logout(client, login, user_rincewind, csrf_token):
     login.as_(user_rincewind)
-    # FIXME: login.as_ does not actually perform login. Funnel will do it when the
-    # next request is made, so we need another call before logout.
+    client.get('/')
     assert current_auth.user == user_rincewind
     rv = client.post('/account/logout', data={'csrf_token': csrf_token})
 
@@ -54,20 +86,37 @@ def test_user_logout(client, login, user_rincewind, csrf_token):
     assert current_auth.user is None
 
 
-def test_user_login_correct_password(
-    client, user_rincewind, user_rincewind_email, csrf_token
+@pytest.mark.parametrize(
+    ['login_type', 'passwords_with_status'], product(logins, passwords_with_status)
+)
+def test_login_types(
+    client,
+    csrf_token,
+    passwords_with_status,
+    login_type,
+    user_rincewind,
+    user_rincewind_with_password,
+    user_rincewind_phone,
+    user_rincewind_email,
 ):
     rv = client.post(
         '/login',
         data=MultiDict(
             {
-                'username': str(user_rincewind_email),
-                'password': test_passwords['rincewind'],
+                'username': str(login_type),
+                'password': passwords_with_status['password'],
                 'csrf_token': csrf_token,
                 'form.id': 'passwordlogin',
             }
         ),
     )
 
-    assert rv.status_code == 303
-    assert current_auth.user == user_rincewind_with_password
+    assert current_auth.is_authenticated is passwords_with_status['auth']
+    assert rv.status_code == passwords_with_status['status_code']
+
+
+# def mock_send(phone, message, callback):
+#     for prefix, sender in senders_by_prefix:
+#         return sender(phone, message, callback)
+
+# def test_sms(client,)
