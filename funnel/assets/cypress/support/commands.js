7f655a85bc3bfb12665ad1b36bece7747241c74d
Vidya Ramakrishnan <vidya@hasgeek.in>
2022-05-26 12:16:32 +0530

    Fix E2E test for OTP login form changes

--- funnel/assets/cypress/support/commands.js
+++ funnel/assets/cypress/support/commands.js
@@ -17,6 +17,7 @@ Cypress.Commands.add('fill_login_details', (username, password) => {
   cy.route('POST', '**/login').as('login');
 
   cy.get('.field-username').type(username, { log: false });
+  cy.get('a[data-cy="password-login"]').click();
   cy.get('.field-password').type(password, { log: false });
   cy.get('.form-actions').find('button').click();
   cy.wait('@login', { timeout: 20000 });
