7e19f23e6ba229900c218a777f093610b6cf22ab
Kiran Jonnalagadda <kiran@hasgeek.com>
2022-06-16 11:44:31 +0530

    Fix string quotes

--- tests/unit/transports/aws_ses/test_ses_json.py
+++ tests/unit/transports/aws_ses/test_ses_json.py
@@ -18,10 +18,10 @@ data_dir = os.path.join(os.path.dirname(__file__), 'data')
 
 def test_delivery() -> None:
     """Check if the delivery JSON is parsed correctly."""
-    with open(os.path.join(data_dir, "delivery.json"), encoding='utf-8') as file:
+    with open(os.path.join(data_dir, 'delivery.json'), encoding='utf-8') as file:
         data = file.read()
     obj: SesEvent = SesEvent.from_json(data)
-    assert obj.event_type == "Delivery"
+    assert obj.event_type == 'Delivery'
     assert obj.mail.tags
     assert obj.mail.timestamp
     assert obj.mail.source
@@ -41,12 +41,12 @@ def test_delivery() -> None:
 def test_delivery_without_subject() -> None:
     """Check if the delivery JSON is parsed correctly when missing a subject."""
     with open(
-        os.path.join(data_dir, "delivery-without-subject.json"),
+        os.path.join(data_dir, 'delivery-without-subject.json'),
         encoding='utf-8',
     ) as file:
         data = file.read()
     obj: SesEvent = SesEvent.from_json(data)
-    assert obj.event_type == "Delivery"
+    assert obj.event_type == 'Delivery'
     assert obj.mail.tags
     assert obj.mail.timestamp
     assert obj.mail.source
@@ -65,13 +65,13 @@ def test_delivery_without_subject() -> None:
 
 def test_bounce() -> None:
     """Check if Data classes for bounce is parsed correctly."""
-    with open(os.path.join(data_dir, "bounce.json"), encoding='utf-8') as file:
+    with open(os.path.join(data_dir, 'bounce.json'), encoding='utf-8') as file:
         data = file.read()
     obj: SesEvent = SesEvent.from_json(data)
     assert obj.bounce
     assert obj.bounce.is_hard_bounce is True
     assert len(obj.bounce.bounced_recipients) == 1
-    assert obj.bounce.bounce_sub_type == "General"
+    assert obj.bounce.bounce_sub_type == 'General'
     assert obj.bounce.feedbackid
     assert obj.bounce.timestamp
     assert obj.bounce.reporting_mta
@@ -79,7 +79,7 @@ def test_bounce() -> None:
 
 def test_complaint() -> None:
     """Check if Data classes for complaint is parsed correctly."""
-    with open(os.path.join(data_dir, "complaint.json"), encoding='utf-8') as file:
+    with open(os.path.join(data_dir, 'complaint.json'), encoding='utf-8') as file:
         data = file.read()
     obj: SesEvent = SesEvent.from_json(data)
     assert obj.complaint
@@ -87,20 +87,20 @@ def test_complaint() -> None:
     assert obj.complaint.complaint_sub_type is None
     assert obj.complaint.arrival_date
     assert len(obj.complaint.complained_recipients) == 1
-    assert obj.complaint.complaint_feedback_type == "abuse"
-    assert obj.complaint.user_agent == "Amazon SES Mailbox Simulator"
+    assert obj.complaint.complaint_feedback_type == 'abuse'
+    assert obj.complaint.user_agent == 'Amazon SES Mailbox Simulator'
 
 
 @pytest.mark.skip(reason="Certificate has expired")
 def test_signature_good_message() -> None:
     """Check if Signature Verification works."""
-    with open(os.path.join(data_dir, "full-message.json"), encoding='utf-8') as file:
+    with open(os.path.join(data_dir, 'full-message.json'), encoding='utf-8') as file:
         data = file.read()
 
     # Decode the JSON
     message = json.loads(data)
     validator = SnsValidator(
-        ["arn:aws:sns:ap-south-1:817922165072:ses-events-for-hasgeek_dot_com"]
+        ['arn:aws:sns:ap-south-1:817922165072:ses-events-for-hasgeek_dot_com']
     )
 
     # Checks
@@ -113,13 +113,13 @@ def test_signature_good_message() -> None:
 @pytest.mark.skip(reason="Certificate has expired")
 def test_signature_bad_message() -> None:
     """Check if Signature Verification works."""
-    with open(os.path.join(data_dir, "bad-message.json"), encoding='utf-8') as file:
+    with open(os.path.join(data_dir, 'bad-message.json'), encoding='utf-8') as file:
         data = file.read()
 
     # Decode the JSON
     message = json.loads(data)
     validator = SnsValidator(
-        ["arn:aws:sns:ap-south-1:817922165072:ses-events-for-hasgeek_dot_com"]
+        ['arn:aws:sns:ap-south-1:817922165072:ses-events-for-hasgeek_dot_com']
     )
 
     # Checks
