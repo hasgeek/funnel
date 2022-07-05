5941eef1be1a90cff2e13a3c80b304fe4ae083e7
Kiran Jonnalagadda <kiran@hasgeek.com>
2022-07-05 16:43:58 +0530

    SQLAlchemy events do no need named parameters

--- funnel/models/user_signals.py
+++ funnel/models/user_signals.py
@@ -39,105 +39,105 @@ from .user import (
 
 
 @event.listens_for(User, 'after_insert')
-def _user_new(mapper, connection, target):
+def _user_new(_mapper, _connection, target):
     model_user_new.send(target)
 
 
 @event.listens_for(User, 'after_update')
-def _user_edited(mapper, connection, target):
+def _user_edited(_mapper, _connection, target):
     model_user_edited.send(target)
 
 
 @event.listens_for(User, 'after_delete')
-def _user_deleted(mapper, connection, target):
+def _user_deleted(_mapper, _connection, target):
     model_user_deleted.send(target)
 
 
 @event.listens_for(Organization, 'after_insert')
-def _org_new(mapper, connection, target):
+def _org_new(_mapper, _connection, target):
     model_org_new.send(target)
 
 
 @event.listens_for(Organization, 'after_update')
-def _org_edited(mapper, connection, target):
+def _org_edited(_mapper, _connection, target):
     model_org_edited.send(target)
 
 
 @event.listens_for(Organization, 'after_delete')
-def _org_deleted(mapper, connection, target):
+def _org_deleted(_mapper, _connection, target):
     model_org_deleted.send(target)
 
 
 @event.listens_for(Team, 'after_insert')
-def _team_new(mapper, connection, target):
+def _team_new(_mapper, _connection, target):
     model_team_new.send(target)
 
 
 @event.listens_for(Team, 'after_update')
-def _team_edited(mapper, connection, target):
+def _team_edited(_mapper, _connection, target):
     model_team_edited.send(target)
 
 
 @event.listens_for(Team, 'after_delete')
-def _team_deleted(mapper, connection, target):
+def _team_deleted(_mapper, _connection, target):
     model_team_deleted.send(target)
 
 
 @event.listens_for(UserEmail, 'after_insert')
-def _useremail_new(mapper, connection, target):
+def _useremail_new(_mapper, _connection, target):
     model_useremail_new.send(target)
 
 
 @event.listens_for(UserEmail, 'after_update')
-def _useremail_edited(mapper, connection, target):
+def _useremail_edited(_mapper, _connection, target):
     model_useremail_edited.send(target)
 
 
 @event.listens_for(UserEmail, 'after_delete')
-def _useremail_deleted(mapper, connection, target):
+def _useremail_deleted(_mapper, _connection, target):
     model_useremail_deleted.send(target)
 
 
 @event.listens_for(UserEmailClaim, 'after_insert')
-def _useremailclaim_new(mapper, connection, target):
+def _useremailclaim_new(_mapper, _connection, target):
     model_useremailclaim_new.send(target)
 
 
 @event.listens_for(UserEmailClaim, 'after_update')
-def _useremailclaim_edited(mapper, connection, target):
+def _useremailclaim_edited(_mapper, _connection, target):
     model_useremailclaim_edited.send(target)
 
 
 @event.listens_for(UserEmailClaim, 'after_delete')
-def _useremailclaim_deleted(mapper, connection, target):
+def _useremailclaim_deleted(_mapper, _connection, target):
     model_useremailclaim_deleted.send(target)
 
 
 @event.listens_for(UserPhone, 'after_insert')
-def _userphone_new(mapper, connection, target):
+def _userphone_new(_mapper, _connection, target):
     model_userphone_new.send(target)
 
 
 @event.listens_for(UserPhone, 'after_update')
-def _userphone_edited(mapper, connection, target):
+def _userphone_edited(_mapper, _connection, target):
     model_userphone_edited.send(target)
 
 
 @event.listens_for(UserPhone, 'after_delete')
-def _userphone_deleted(mapper, connection, target):
+def _userphone_deleted(_mapper, _connection, target):
     model_userphone_deleted.send(target)
 
 
 @event.listens_for(UserPhoneClaim, 'after_insert')
-def _userphoneclaim_new(mapper, connection, target):
+def _userphoneclaim_new(_mapper, _connection, target):
     model_userphoneclaim_new.send(target)
 
 
 @event.listens_for(UserPhoneClaim, 'after_update')
-def _userphoneclaim_edited(mapper, connection, target):
+def _userphoneclaim_edited(_mapper, _connection, target):
     model_userphoneclaim_edited.send(target)
 
 
 @event.listens_for(UserPhoneClaim, 'after_delete')
-def _userphoneclaim_deleted(mapper, connection, target):
+def _userphoneclaim_deleted(_mapper, _connection, target):
     model_userphoneclaim_deleted.send(target)
