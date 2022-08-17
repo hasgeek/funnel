660862b7893c0bc0eb54968a3a1fe07192e6dc1b
Kiran Jonnalagadda <kiran@hasgeek.com>
2022-08-17 23:15:55 +0530

    Don't use nohup as the background process must terminate
    secrets.test is also deprecated in favour of .env.testing

--- runfrontendtests.sh
+++ runfrontendtests.sh
@@ -5,13 +5,11 @@ export FLASK_ENV=testing
 # For macos: https://stackoverflow.com/a/52230415/78903
 export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
 
-if [ -f secrets.test ]; then
-        source ./secrets.test
-fi
 python -m tests.cypress.frontend_tests_initdb
-nohup flask run -p 3002 2>&1 1>/tmp/funnel-server.log & echo $! > /tmp/funnel-server.pid
+flask run -p 3002 2>&1 1>/tmp/funnel-server.log & echo $! > /tmp/funnel-server.pid
 cd funnel/assets
 npx cypress run --browser chrome
 kill `cat /tmp/funnel-server.pid`
 cd ../..
 python -m tests.cypress.frontend_tests_dropdb
+fg
