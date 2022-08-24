47d30212915cc6c3f402841767efb8a841c32ca3
Mitesh Ashar <email@miteshashar.com>
2022-08-24 22:37:17 +0530

    Typing for funnel.utils.markdown base function

--- funnel/utils/markdown/base.py
+++ funnel/utils/markdown/base.py
@@ -1,15 +1,26 @@
 """Base files for markdown parser."""
 
 from copy import deepcopy
+from typing import Any, List, Mapping, Optional, Type, Union, overload
 
 from markdown_it import MarkdownIt
 from markupsafe import Markup
 from mdit_py_plugins import anchors, footnote, tasklists
+from typing_extensions import Protocol
 
 from coaster.utils import make_name
 from coaster.utils.text import VALID_TAGS, normalize_spaces_multiline, sanitize_html
 
 MARKDOWN_HTML_TAGS = deepcopy(VALID_TAGS)
+
+
+# --- Typing Protocol for markdown-it-py plugins ---------------------------------------
+class MarkdownItPluginProtocol(Protocol):  # pylint: disable=too-few-public-methods
+    """Protocol for callable that gets a callback to initilize markdown-it-py plugin."""
+
+    def __call__(self, md: MarkdownIt, **options) -> None:
+        ...
+
 
 # --- Standard extensions --------------------------------------------------------------
 # FOR CUT 2
@@ -25,17 +36,17 @@ MARKDOWN_HTML_TAGS = deepcopy(VALID_TAGS)
 #       https://www.npmjs.com/search?q=highlight%20keywords%3Amarkdown-it-plugin
 
 
-default_markdown_extensions_html = {
+default_markdown_extensions_html: Mapping[str, Type[MarkdownItPluginProtocol]] = {
     'footnote': footnote.footnote_plugin,
 }
 
-default_markdown_extensions = {
+default_markdown_extensions: Mapping[str, Type[MarkdownItPluginProtocol]] = {
     'footnote': footnote.footnote_plugin,
     'heading_anchors': anchors.anchors_plugin,
     'tasklists': tasklists.tasklists_plugin,
 }
 
-default_markdown_extension_configs = {
+default_markdown_extension_configs: Mapping[str, Mapping[str, Any]] = {
     'footnote': {},
     'heading_anchors': {
         'min_level': 1,
@@ -50,14 +61,38 @@ default_markdown_extension_configs = {
 # --- Markdown processor ---------------------------------------------------------------
 
 # pylint: disable=too-many-arguments
+@overload
 def markdown(
-    text,
-    html=False,
-    linkify=True,
-    valid_tags=None,
-    extensions=None,
-    extension_configs=None,
-):
+    text: None,
+    html: bool = False,
+    linkify: bool = True,
+    valid_tags: Optional[Union[List[str], Mapping[str, List]]] = None,
+    extensions: Optional[Mapping[str, Type[MarkdownItPluginProtocol]]] = None,
+    extension_configs: Optional[Mapping[str, Mapping[str, Any]]] = None,
+) -> None:
+    ...
+
+
+@overload
+def markdown(
+    text: str,
+    html: bool = False,
+    linkify: bool = True,
+    valid_tags: Optional[Union[List[str], Mapping[str, List]]] = None,
+    extensions: Optional[Mapping[str, Type[MarkdownItPluginProtocol]]] = None,
+    extension_configs: Optional[Mapping[str, Mapping[str, Any]]] = None,
+) -> Markup:
+    ...
+
+
+def markdown(
+    text: Optional[str],
+    html: bool = False,
+    linkify: bool = True,
+    valid_tags: Optional[Union[List[str], Mapping[str, List]]] = None,
+    extensions: Optional[Mapping[str, Type[MarkdownItPluginProtocol]]] = None,
+    extension_configs: Optional[Mapping[str, Mapping[str, Any]]] = None,
+) -> Optional[Markup]:
     """
     Markdown parser compliant with Commonmark+GFM.
 
