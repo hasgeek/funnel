25054b6cc4687e709a3768281601b36d866326bb
Kiran Jonnalagadda <kiran@hasgeek.com>
2022-09-16 09:22:48 +0530

    Allow additional diagramming in Markdown (#1469)
    * Allow additional diagramming in Markdown
    
    * Markmap support
    
    * Use markmap autoloader since markmap-lib is heavy library
    
    * Init markmap in profile page
    
    Co-authored-by: Vidya Ramakrishnan <vidya@hasgeek.in>

---
+++ funnel/assets/js/utils/markmap.js
@@ -0,0 +1,50 @@
+const MarkmapEmbed = {
+  addMarkmap() {
+    $('.language-markmap').each(function embedMarkmap() {
+      $(this).addClass('embed-added');
+      $(this).find('code').addClass('markmap');
+    });
+    window.markmap.autoLoader.renderAll();
+  },
+  loadMarkmap() {
+    const self = this;
+    const CDN = [
+      'https://cdn.jsdelivr.net/npm/d3@6',
+      'https://cdn.jsdelivr.net/npm/markmap-view',
+      'https://cdn.jsdelivr.net/npm/markmap-autoloader',
+    ];
+    let asset = 0;
+    const loadMarkmapScript = () => {
+      $.ajax({
+        url: CDN[asset],
+        dataType: 'script',
+        cache: true,
+      }).done(() => {
+        if (asset < CDN.length - 1) {
+          asset += 1;
+          if (window.markmap) {
+            window.markmap = {
+              autoLoader: { manual: true },
+            };
+          }
+          loadMarkmapScript();
+        } else {
+          self.addMarkmap();
+        }
+      });
+    };
+    if (!window.markmap) {
+      loadMarkmapScript();
+    } else {
+      self.addMarkmap();
+    }
+  },
+  init(containerDiv) {
+    this.containerDiv = containerDiv;
+    if ($('.language-markmap').length > 0) {
+      this.loadMarkmap();
+    }
+  },
+};
+
+export default MarkmapEmbed;
