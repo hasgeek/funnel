e70b2fbe9c4dc17799a451abc48d97e40aad011f
Vidya Ramakrishnan <vidya@hasgeek.in>
2022-09-26 17:13:04 +0530

    Use htmx for search page

--- funnel/assets/js/search.js
+++ funnel/assets/js/search.js
@@ -1,5 +1,6 @@
 import Utils from './utils/helper';
 import { RactiveApp } from './utils/ractive_util';
+import LazyloadImg from './utils/lazyloadimage';
 
 const Search = {
   init(config) {
@@ -160,7 +161,6 @@ const Search = {
           this.activateTab(searchType, config.results);
         } else {
           searchType = this.get('tabs')[0].type;
-          this.fetchResult(searchType);
         }
       },
       onrender() {
@@ -176,10 +176,43 @@ const Search = {
           event.preventDefault();
           this.set('queryString', document.querySelector('.js-search-field').value);
           // Clear results for the new query
-          this.set('results', '');
-          this.fetchResult(this.getQueryString('type'));
-        });
+    $('title').html(title);
+    $('meta[name=DC\\.title]').attr('content', title);
+    $('meta[property=og\\:title]').attr('content', title);
+    $('meta[name=description]').attr('content', description);
+    $('meta[property=og\\:description]').attr('content', description);
+    if (url) {
+      $('link[rel=canonical]').attr('href', url);
+      $('meta[property=og\\:url]').attr('content', url);
+    }
+  },
+  updateBrowserHistory(url) {
+    window.history.replaceState('', '', url);
+  },
+  afterFetch(activeTab, url) {
+    $(this.config.tabElem).removeClass(this.config.activetabClassName);
+    $(activeTab).addClass(this.config.activetabClassName);
+    $(this.config.tabWrapperElem).animate(
+      {
+        scrollLeft: document.querySelector(`.${this.config.activetabClassName}`)
+          .offsetLeft,
       },
+      'slow'
+    );
+    Utils.truncate();
+    LazyloadImg.init('js-lazyload-img');
+    this.updateBrowserHistory(url);
+    this.updateMetaTags();
+  },
+  init(config) {
+    this.config = config;
+    const countHash = {};
+    config.counts.forEach((count) => {
+      countHash[count.type] = count.count;
+    });
+    this.count = countHash;
+    document.body.addEventListener('htmx:afterOnLoad', (event) => {
+      this.afterFetch(event.detail.elt, event.detail.pathInfo.requestPath);
     });
   },
 };
