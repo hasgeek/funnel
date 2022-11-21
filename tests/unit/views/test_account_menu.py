329fc2430cf9201354dd1bb15cf6b87f82f291a8
Kiran Jonnalagadda <kiran@hasgeek.com>
2022-11-21 15:31:02 +0530

    Document recent org list and upgrade test

--- tests/unit/views/test_account_menu.py
+++ tests/unit/views/test_account_menu.py
@@ -1,9 +1,14 @@
+"""Tests for account menu drop-down views."""
+# pylint: disable=too-many-arguments
+
+import time
+
 import pytest
 
 from funnel import models
-from funnel.views.account import recent_organization_memberships
 
 
+@pytest.mark.dbcommit()  # Required for granted_at time to be unique per commit
 @pytest.mark.parametrize(
     (
         'number_of_orgs',
@@ -15,6 +20,8 @@ from funnel.views.account import recent_organization_memberships
     ),
     [
         (1, 1, 0, 1, 0, 0),
+        (2, 1, 0, 1, 0, 1),
+        (2, 1, 1, 1, 1, 1),
         (2, 2, 0, 2, 0, 0),
         (3, 3, 0, 3, 0, 0),
         (4, 3, 1, 3, 1, 1),
@@ -32,13 +39,20 @@ def test_recent_organization_memberships_count(
     returned_overflow,
     returned_extra_count,
 ) -> None:
+    """Test if organization list in account menu handles counts correctly."""
     for i in range(number_of_orgs):
-        org = models.Organization(name=str(i), title=str(i), owner=user_twoflower)
+        org = models.Organization(
+            name=f'org{i}', title=f"Org {i}", owner=user_twoflower
+        )
         db_session.add(org)
-    db_session.commit()
-    result = recent_organization_memberships(
-        user_twoflower, require_listed, require_overflow
+        db_session.commit()
+        time.sleep(0.001)
+    result = user_twoflower.views.recent_organization_memberships(
+        require_listed, require_overflow
     )
+    # Most recently added org should be first in results. The `dbcommit` mark is
+    # required to ensure this, as granted_at timestamp is set by the SQL transaction
+    assert result.recent[0].organization.name == org.name
     assert len(result.recent) == returned_listed
     assert len(result.overflow) == returned_overflow
     assert result.extra_count == returned_extra_count
