c8c98b17cd370aa580598d6dfc7f4a18e63d43e2
Kiran Jonnalagadda <kiran@hasgeek.com>
2023-01-08 15:26:18 +0530

    Move refresh group into a subfolder

---
+++ funnel/cli/refresh/markdown.py
@@ -0,0 +1,126 @@
+"""Cache refresh actions."""
+
+from __future__ import annotations
+
+from typing import ClassVar, Dict, List, Optional, Set
+
+import click
+
+import rich.progress
+
+from ... import models
+from ...models import db, sa
+from . import refresh
+
+
+class MarkdownModel:
+    """Holding class for a model that has markdown fields with custom configuration."""
+
+    registry: ClassVar[Dict[str, MarkdownModel]] = {}
+    config_registry: ClassVar[Dict[str, Set[MarkdownModel]]] = {}
+
+    def __init__(self, model, fields: Set[str]):
+        self.name = model.__tablename__
+        self.model = model
+        self.fields = fields
+        self.config_fields: Dict[str, Set[str]] = {}
+        for field in fields:
+            config = getattr(model, field).original_property.composite_class.config.name
+            self.config_fields.setdefault(config, set()).add(field)
+
+    @classmethod
+    def register(cls, model, fields: Set[str]):
+        """Create an instance and add it to the registry."""
+        obj = cls(model, fields)
+        for config in obj.config_fields:
+            cls.config_registry.setdefault(config, set()).add(obj)
+        cls.registry[obj.name] = obj
+
+    def reparse(self, config: Optional[str] = None, obj=None):
+        """Reparse Markdown fields, optionally for a single config profile."""
+        if config and config not in self.config_fields:
+            return
+        if config:
+            fields = self.config_fields[config]
+        else:
+            fields = self.fields
+
+        if obj is not None:
+            iter_list = [obj]
+            iter_total = 1
+        else:
+            load_columns = (
+                ['id']
+                + [f'{field}_text'.lstrip('_') for field in fields]
+                + [f'{field}_html'.lstrip('_') for field in fields]
+            )
+            iter_list = (
+                self.model.query.order_by('id')
+                .options(sa.orm.load_only(*load_columns))
+                .yield_per(10)
+            )
+            iter_total = self.model.query.count()
+
+        for item in rich.progress.track(
+            iter_list, description=self.name, total=iter_total
+        ):
+            for field in fields:
+                setattr(item, field, getattr(item, field).text)
+
+
+MarkdownModel.register(models.Comment, {'_message'})
+MarkdownModel.register(models.Profile, {'description'})
+MarkdownModel.register(models.Project, {'description', 'instructions'})
+MarkdownModel.register(models.Proposal, {'body'})
+MarkdownModel.register(models.Session, {'description'})
+MarkdownModel.register(models.Update, {'body'})
+MarkdownModel.register(models.Venue, {'description'})
+MarkdownModel.register(models.VenueRoom, {'description'})
+
+
+@refresh.command('markdown')
+@click.argument('content', type=click.Choice(MarkdownModel.registry.keys()), nargs=-1)
+@click.option(
+    '-a',
+    '--all',
+    'allcontent',
+    is_flag=True,
+    help="Reparse all Markdown content (use with caution).",
+)
+@click.option(
+    '-c',
+    '--config',
+    type=click.Choice(MarkdownModel.config_registry.keys()),
+    help="Reparse Markdown content using a specific configuration.",
+)
+@click.option(
+    '-u',
+    '--url',
+    help="Reparse content at this URL",
+)
+def markdown(
+    content: List[str], config: Optional[str], allcontent: bool, url: Optional[str]
+) -> None:
+    """Reparse Markdown content."""
+    if allcontent:
+        if config or content or url:
+            raise click.BadOptionUsage(
+                'allcontent',
+                "The --all option overrides other options and must be used standalone",
+            )
+        for mm in MarkdownModel.registry.values():
+            mm.reparse()
+            db.session.commit()
+    else:
+        if url:
+            raise click.BadOptionUsage('url', "URL refresh is not supported yet.")
+        if content:
+            for model in content:
+                MarkdownModel.registry[model].reparse()
+                db.session.commit()
+        if config:
+            for mm in MarkdownModel.config_registry[config]:
+                mm.reparse(config)
+                db.session.commit()
+    if not (allcontent or config or content or url):
+        click.echo("Specify content, --config <name>, --url <url>, or --all")
