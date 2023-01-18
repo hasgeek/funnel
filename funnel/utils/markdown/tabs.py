03249f9a97da08d6f87badcdda819513ac99c505
Mitesh Ashar <email@miteshashar.com>
2023-01-18 13:54:23 +0530

    Accessibility features(screen reader and keyboard nav) for markdown tabs.

--- funnel/utils/markdown/tabs.py
+++ funnel/utils/markdown/tabs.py
@@ -52,8 +52,11 @@ class TabsetNode:
     start: int
     parent: Optional[TabNode] = None
     children: List[TabNode] = field(default_factory=list)
-    _html_tabs: ClassVar[str] = '<ul class="mui-tabs__bar">{items_html}</ul>'
+    _html_tabs: ClassVar[
+        str
+    ] = '<ul role="tablist" class="mui-tabs__bar">{items_html}</ul>'
     html_close: ClassVar[str] = '</div>'
+    _tabset_id: str = ''
 
     def flatten(self) -> List[TabNode]:
         tabs = self.children
@@ -65,7 +68,18 @@ class TabsetNode:
     @property
     def html_open(self):
         items_html = ''.join([item.html_tab_item for item in self.children])
-        return '<div>' + self._html_tabs.format(items_html=items_html)
+        return (
+            f'<div id="{self.tabset_id}" class="md-tabset">'
+            + self._html_tabs.format(items_html=items_html)
+        )
+
+    @property
+    def tabset_id(self) -> str:
+        return 'md-tabset-' + self._tabset_id
+
+    @tabset_id.setter
+    def tabset_id(self, value) -> None:
+        self._tabset_id = value
 
 
 @dataclass
@@ -78,13 +92,17 @@ class TabNode:
     _tab_id: str = ''
     children: List[TabsetNode] = field(default_factory=list)
     _opening: ClassVar[str] = (
-        '<div{class_attr} id="{tab_id}">' + '<div class="grid__col-sm-12">'
+        '<div role="tabpanel"{class_attr} id="{tab_id}-panel"'
+        + ' aria-labelledby="{tab_id}" tabindex="0">'
+        + '<div class="grid__col-sm-12">'
     )
     _closing: ClassVar[str] = '</div></div>'
     _item_html: ClassVar[str] = (
-        '<li{class_attr}>'
-        + '<a class="mui--text-body2" href="javascript:void(0)"'
-        + ' data-mui-toggle="tab" data-mui-controls="{tab_id}">{title}</a></li>'
+        '<li role="presentation"{class_attr}>'
+        + '<a role="tab" class="mui--text-body2" href="javascript:void(0)"'
+        + ' id="{tab_id}" data-tabset="{tabset_id}" data-mui-toggle="tab"'
+        + ' data-mui-controls="{tab_id}-panel" aria-controls="{tab_id}-panel"'
+        + '{accessibility}>{title}</a></li>'
     )
 
     def _class_attr(self, classes=None):
@@ -92,6 +110,19 @@ class TabNode:
             classes = []
         classes = classes + self._active_class
         return f' class="{" ".join(classes)}"' if len(classes) > 0 else ''
+
+    @property
+    def _item_aria(self):
+        if self.is_first:
+            return ' tabindex="0" aria-selected="true"'
+        return ' tabindex="-1" aria-selected="false"'
+
+    def flatten(self) -> List[TabsetNode]:
+        tabsets = self.children
+        for tabset in self.children:
+            for tab in tabset.children:
+                tabsets = tabsets + tab.flatten()
+        return tabsets
 
     @property
     def _active_class(self):
@@ -103,7 +134,7 @@ class TabNode:
 
     @property
     def tab_id(self) -> str:
-        return 'md-tab-id-' + self._tab_id
+        return 'md-tab-' + self._tab_id
 
     @tab_id.setter
     def tab_id(self, value) -> None:
@@ -120,7 +151,7 @@ class TabNode:
     @property
     def html_open(self) -> str:
         opening = self._opening.format(
-            tab_id=self.tab_id, class_attr=self._class_attr(['md-tabs-panel', 'grid'])
+            tab_id=self.tab_id, class_attr=self._class_attr(['mui-tabs__pane', 'grid'])
         )
         if self.is_first:
             opening = self.parent.html_open + opening
@@ -133,7 +164,11 @@ class TabNode:
     @property
     def html_tab_item(self):
         return self._item_html.format(
-            tab_id=self.tab_id, title=self.title, class_attr=self._class_attr()
+            tab_id=self.tab_id,
+            tabset_id=self.parent.tabset_id,
+            title=self.title,
+            class_attr=self._class_attr(),
+            accessibility=self._item_aria,
         )
 
 
@@ -189,6 +224,14 @@ class TabsManager:
                 return self._index[start]
             except KeyError:
                 return None
+
+        tabsets: List[TabsetNode] = []
+        for tabset in self.tabsets:
+            tabsets.append(tabset)
+            for tab in tabset.children:
+                tabsets = tabsets + tab.flatten()
+        for i, tabset in enumerate(tabsets):
+            tabset.tabset_id = str(i + 1)
         tabs: List[TabNode] = reduce(
             lambda tablist, tabset: tablist + tabset.flatten(), self.tabsets, []
         )
