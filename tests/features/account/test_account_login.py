8f33cdb3dc06a4b5b66e038e1faf504079ac4e87
Mitesh Ashar <email@miteshashar.com>
2023-02-11 14:22:50 +0530

    Python BDD step functions should place the db_session argument after the live_server argument to ensure app_context is popped in the correct order.

--- tests/features/account/test_account_login.py
+++ tests/features/account/test_account_login.py
@@ -6,8 +6,10 @@ PATCH_SMS_SEND = 'funnel.transports.sms.send'
 scenarios('account_login.feature')
 
 
+# pytest-bdd loads fixtures by looping through the function arguments of the step.
+# Hence, db_session should be placed after live_server in all BDD steps.
 @given("Twoflower visits the login page")
-def given_twoflower_visits_login_page(browser, db_session, live_server, user_twoflower):
+def given_twoflower_visits_login_page(browser, live_server, db_session, user_twoflower):
     browser.visit(live_server.url + 'login')
 
 
@@ -20,21 +22,21 @@ def given_twoflower_enters_phone_number(
 
 @when("they click on get otp")
 def when_click_get_otp(
-    browser, db_session, live_server, user_twoflower, client, csrf_token
+    browser, live_server, db_session, user_twoflower, client, csrf_token
 ):
     browser.find_by_xpath('//*[@id="form-passwordlogin"]/div[4]/button').click()
 
 
 @when("enter their name with correct otp")
-def when_enter_name_and_otp(browser, db_session, live_server, user_twoflower):
+def when_enter_name_and_otp(browser, live_server, db_session, user_twoflower):
     pass
 
 
-@then("they get 'You are now logged in' flash message")
+@then("they get \"You are now logged in\" flash message")
 def then_get_flash_message():
     pass
 
 
 @given("they enter the email")
-def given_twoflower_enters_email(browser, db_session, live_server, user_twoflower):
+def given_twoflower_enters_email(browser, live_server, db_session, user_twoflower):
     browser.find_by_name('username').fill('test@example.com')
