266025fd4005c1e845b89eb69d3428c0f86c935d
Amogh M Aradhya <amoghmanjunath99@gmail.com>
2023-02-24 14:32:10 +0530

    added pytest-selenium

--- tests/e2e/basic/test_live_server.py
+++ tests/e2e/basic/test_live_server.py
@@ -1,20 +1,33 @@
 """Test live server."""
+import time
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
-def test_open_homepage(live_server, browser, db_session, org_uu) -> None:
+def test_open_homepage(live_server, selenium, db_session, org_uu) -> None:
     """Launch a live server and visit homepage."""
     org_uu.profile.is_verified = True
     db_session.commit()
-    browser.visit(live_server.url)
-    assert browser.is_text_present("Explore communities")
+    selenium.get(live_server.url)
+    time.sleep(1)
+    assert (
+        selenium.find_element(By.CLASS_NAME, "project-headline").text
+        == "Explore communities"
+    )
 
 
-def test_transport_mock_sms(live_server, app, browser) -> None:
+def test_transport_mock_sms(live_server, selenium, app) -> None:
     """Live server fixture mocks transport functions to a logger."""
-    browser.visit(app.url_for('login'))
-    browser.find_by_name('username').fill('8123456789')
-    browser.find_by_css('#form-passwordlogin button').click()
-    browser.find_by_name('otp').click()  # This causes browser to wait until load
+    selenium.get(app.url_for('login'))
+    selenium.find_element(By.NAME, "username").send_keys("8123456789")
+    selenium.find_element(By.CSS_SELECTOR, "#form-passwordlogin button").send_keys(
+        Keys.RETURN
+    )
+    # browser.find_by_name('username').fill('8123456789')
+    # browser.find_by_css('#form-passwordlogin button').click()
+    # browser.find_by_name('otp').click()  # This causes browser to wait until load
+    time.sleep(1)
     captured_sms = live_server.transport_calls.sms[-1]
     assert captured_sms.phone == '+918123456789'
     assert captured_sms.message.startswith('OTP is')
