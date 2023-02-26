bf0544fc75ad7a70b2a0c86b26c0479f2c09c83d
Mitesh Ashar <email@miteshashar.com>
2023-02-26 09:54:42 +0530

    Cleanup code for markdown tabs and tabs accessibility.

--- funnel/assets/js/utils/tabs.js
+++ funnel/assets/js/utils/tabs.js
@@ -1,35 +1,28 @@
 import Utils from './helper';
 
-const hasOverflow = (el) => el.scrollLeft + el.scrollWidth > el.clientWidth;
-const hasLeftOverflow = (el) => Boolean(el.scrollLeft);
-const hasRightOverflow = (el) =>
-  el.scrollLeft + el.clientWidth + el.offsetLeft <
-  el.children[el.children.length - 1].offsetLeft +
-    el.children[el.children.length - 1].clientWidth;
-
-const MUITabs = {
-  // ResizeObserver to mark tab bars having overflow, with a class.
-  resizeObserver: new ResizeObserver((entries) => {
+const Tabs = {
+  overflowObserver: new ResizeObserver(function checkOverflow(entries) {
     entries.forEach((entry) => {
-      if (hasOverflow(entry.target)) $(entry.target).parent().addClass('has-overflow');
+      if (Tabs.helpers.hasOverflow(entry.target))
+        $(entry.target).parent().addClass('has-overflow');
       else $(entry.target).parent().removeClass('has-overflow');
     });
   }),
-  getIntersectionObserver(tabsBar) {
+  getIntersectionObserver(tablist) {
     return new IntersectionObserver((entries) => {
       entries.forEach(
         (entry) => {
           $(entry.target).data('isIntersecting', entry.isIntersecting);
-          $(entry.target).data('intersection', entry.intersectionRatio);
         },
         {
-          root: tabsBar,
+          root: tablist,
           threshold: 1,
         }
       );
     });
   },
-  wrapAndAddIcons(tabsBar, icons) {
+  wrapAndAddIcons(tablist) {
+    const icons = Tabs.helpers.createIconset();
     // Wrap the tabs bar with a container, to allow introduction of
     // tabs navigation arrow icons.
     const $leftIcons = $('<div class="tabs-nav-icons-left"></div>').html(
@@ -38,198 +31,200 @@ const MUITabs = {
     const $rightIcons = $('<div class="tabs-nav-icons-right"></div>').html(
       Object.values(icons.right)
     );
-    $(tabsBar)
-      .wrap('<div class="mui-tabs__bar--wrapper"></div>')
+    $(tablist)
+      .wrap('<div class="md-tablist-wrapper"></div>')
       .before($leftIcons)
       .after($rightIcons);
-  },
-  getLeftScrollIndex(tabsBar, tabs) {
-    const tabsBarWidth = tabsBar.clientWidth;
-    // Find the first visible tab.
-    let firstVisible = 0;
-    while (
-      firstVisible < tabs.length - 1 &&
-      !$(tabs.get(firstVisible)).data('isIntersecting')
-    )
-      firstVisible += 1;
-    // Calculate the tab to switch to.
-    let switchTo = firstVisible;
-    const end = tabsBar.scrollLeft;
-    while (
-      switchTo >= 0 &&
-      end - tabs.get(switchTo).parentElement.offsetLeft < tabsBarWidth
-    )
-      switchTo -= 1;
-    return switchTo;
-  },
-  getRightScrollIndex(tabs) {
-    // Calculate tab to switch to.
-    let switchTo = tabs.length - 1;
-    while (switchTo > 0 && !$(tabs[switchTo]).data('isIntersecting')) switchTo -= 1;
-    return switchTo;
+    $(icons.left.touch).click(function previousTab() {
+      tablist.dispatchEvent(new Event('previous-tab'));
+    });
+    $(icons.right.touch).click(function nextTab() {
+      tablist.dispatchEvent(new Event('next-tab'));
+    });
+    $(icons.left.scroll).click(function scrollLeft() {
+      tablist.dispatchEvent(new Event('scroll-left'));
+    });
+    $(icons.right.scroll).click(function scrollRight() {
+      tablist.dispatchEvent(new Event('scroll-right'));
+    });
   },
   checkScrollability() {
     // Function to update no-scroll-left and no-scroll-right
     // classes for tabs bar wrapper.
-    if (!hasLeftOverflow(this)) $(this).parent().addClass('no-scroll-left');
+    if (!Tabs.helpers.hasLeftOverflow(this))
+      $(this).parent().addClass('no-scroll-left');
     else $(this).parent().removeClass('no-scroll-left');
-    if (!hasRightOverflow(this)) $(this).parent().addClass('no-scroll-right');
+    if (!Tabs.helpers.hasRightOverflow(this))
+      $(this).parent().addClass('no-scroll-right');
     else $(this).parent().removeClass('no-scroll-right');
+  },
+  getLeftScrollIndex(tablist, $tabs) {
+    const tablistWidth = tablist.clientWidth;
+    // Find the first visible tab.
+    let firstVisible = 0;
+    while (
+      firstVisible < $tabs.length - 1 &&
+      !$($tabs.get(firstVisible)).data('isIntersecting')
+    )
+      firstVisible += 1;
+    // Calculate the tab to switch to.
+    let switchTo = firstVisible;
+    const end = tablist.scrollLeft;
+    while (
+      switchTo >= 0 &&
+      end - $tabs.get(switchTo).parentElement.offsetLeft < tablistWidth
+    )
+      switchTo -= 1;
+    return switchTo + 1;
+  },
+  getRightScrollIndex($tabs) {
+    // Calculate tab to switch to.
+    let switchTo = $tabs.length - 1;
+    while (switchTo > 0 && !$($tabs[switchTo]).data('isIntersecting')) switchTo -= 1;
+    return switchTo;
+  },
+  addScrollListeners(tablist, $tabs) {
+    function scrollTo(i) {
+      tablist.scrollLeft = $tabs.get(i).offsetLeft - tablist.offsetLeft;
+    }
+    tablist.addEventListener('scroll-left', function scrollLeft() {
+      scrollTo(Tabs.getLeftScrollIndex(tablist, $tabs));
+    });
+    tablist.addEventListener('scroll-right', function scrollRight() {
+      scrollTo(Tabs.getRightScrollIndex($tabs));
+    });
+    $(tablist).scroll(Utils.debounce(Tabs.checkScrollability, 500));
+  },
+  addNavListeners(tablist, $tabs) {
+    let index = 0;
+    function activateCurrent() {
+      window.mui.tabs.activate($($tabs.get(index)).data('mui-controls'));
+    }
+    tablist.addEventListener('previous-tab', function previousTab() {
+      if (index > 0) index -= 1;
+      else index = $tabs.length - 1;
+      activateCurrent();
+    });
+    tablist.addEventListener('next-tab', function nextTab() {
+      if (index < $tabs.length - 1) index += 1;
+      else index = 0;
+      activateCurrent();
+    });
+    $tabs.each(function addTabListeners(tabIndex, tab) {
+      tab.addEventListener('mui.tabs.showend', function tabActivated(ev) {
+        index = tabIndex;
+        ev.srcElement.scrollIntoView();
+      });
+    });
+  },
+  enhanceARIA(tablist, $tabs) {
+    $tabs.on('keydown', function addArrowNav(event) {
+      const [LEFT, UP, RIGHT, DOWN] = [37, 38, 39, 40];
+      const k = event.which || event.keyCode;
+      if (k >= LEFT && k <= DOWN) {
+        switch (k) {
+          case LEFT:
+          case UP:
+            tablist.dispatchEvent(new Event('previous-tab'));
+            break;
+          case RIGHT:
+          case DOWN:
+            tablist.dispatchEvent(new Event('next-tab'));
+            break;
+          default:
+        }
+        event.preventDefault();
+      }
+    });
+    $tabs.each(function addTabListeners(tabIndex, tab) {
+      tab.addEventListener('mui.tabs.showend', function tabActivated(ev) {
+        $(ev.srcElement).attr({ tabindex: 0, 'aria-selected': 'true' }).focus();
+      });
+      tab.addEventListener('mui.tabs.hideend', function tabDeactivated(ev) {
+        $(ev.srcElement).attr({ tabindex: -1, 'aria-selected': 'false' }).focus();
+      });
+    });
+  },
+  async processTablist(index, tablist) {
+    const $tablist = $(tablist);
+    const $tabs = $tablist.find('[role=tab]');
+    const isMarkdown = $tablist.parent().hasClass('md-tabset');
+    let visibilityObserver;
+    let $tablistContainer;
+    Tabs.addNavListeners(tablist, $tabs);
+    if (isMarkdown) {
+      $tablist.addClass('mui-tabs__bar');
+      Tabs.addScrollListeners(tablist, $tabs);
+      Tabs.wrapAndAddIcons(tablist);
+      $tablistContainer = $tablist.parent();
+      Tabs.overflowObserver.observe(tablist);
+      visibilityObserver = Tabs.getIntersectionObserver(tablist);
+      Tabs.checkScrollability.bind(tablist)();
+    }
+    $tabs.each(function processTab(tabIndex, tab) {
+      if (isMarkdown) {
+        $(tab)
+          .attr('data-mui-toggle', 'tab')
+          .attr('data-mui-controls', $(tab).attr('aria-controls'));
+        visibilityObserver.observe(tab);
+        const $panel = $(`#${$(tab).attr('aria-controls')}`);
+        $panel.mouseenter(
+          $tablistContainer.addClass.bind($tablistContainer, 'has-panel-hover')
+        );
+        $panel.mouseleave(
+          $tablistContainer.removeClass.bind($tablistContainer, 'has-panel-hover')
+        );
+      }
+    });
+    Tabs.enhanceARIA(tablist, $tabs);
+    $tablist.addClass('activated').removeClass('activating');
+  },
+  process($parentElement, $tablists) {
+    $parentElement.find('.md-tabset [role=tabpanel]').addClass('mui-tabs__pane');
+    $parentElement.find('.md-tabset .md-tab-active').addClass('mui--is-active');
+    $tablists.each(this.processTablist);
   },
   async init(container) {
     const $parentElement = $(container || 'body');
-    $parentElement.find('[role=tabpanel]').addClass('mui-tabs__pane');
-    $parentElement.find('.md-tab-active').addClass('mui--is-active');
-    $parentElement
-      .find('.md-tabset ul[role=tablist]:not(.activating):not(.activated)')
-      .each(function handleTabset() {
-        // Function being called once for each tabs bar
-        const tabsBar = this;
-        $(tabsBar).addClass('activating').addClass('mui-tabs__bar');
-        const $tabs = $(tabsBar).find('[role=tab]');
-        const icons = MUITabs.createIconset();
-
-        MUITabs.wrapAndAddIcons(tabsBar, icons);
-        // $tabsBarContainer should be initialised after calling wrapAndAddIcons.
-        const $tabsBarContainer = $(tabsBar).parent();
-
-        // Observe this tabs bar with ResizeObserver.
-        MUITabs.resizeObserver.observe(tabsBar);
-
-        // Use IntersectionObserver to update tab element with it's
-        // visibility status.
-        const observer = MUITabs.getIntersectionObserver(tabsBar);
-
-        // Attach this to the scroll event.
-        $(tabsBar).scroll(Utils.debounce(MUITabs.checkScrollability, 500, this));
-
-        // Functions to scroll the tabs bar left and right.
-        function scrollTo(i) {
-          tabsBar.scrollLeft = $tabs.get(i).offsetLeft - tabsBar.offsetLeft;
-        }
-        function leftScroll() {
-          scrollTo(MUITabs.getLeftScrollIndex(tabsBar, $tabs) + 1);
-        }
-        function rightScroll() {
-          scrollTo(MUITabs.getRightScrollIndex($tabs));
-        }
-
-        $tabs.each(function handleTab() {
-          const tab = this;
-          $(tab)
-            .attr('data-mui-toggle', 'tab')
-            .attr('data-mui-controls', $(tab).attr('aria-controls'));
-
-          // Observe each tab for visibility within it's tabs
-          // bar using IntersectionObserver.
-          observer.observe(tab);
-
-          // Toggle has-panel-hover on the tabs bar wrapper
-          // when a related panel is in hover state.
-          const $panel = $(`#${$(tab).data('mui-controls')}`);
-          $panel.mouseenter(
-            $tabsBarContainer.addClass.bind($tabsBarContainer, 'has-panel-hover')
-          );
-          $panel.mouseleave(
-            $tabsBarContainer.removeClass.bind($tabsBarContainer, 'has-panel-hover')
-          );
-        });
-
-        // Bind scroll/touch actions to the arrow icons.
-        $(icons.left.touch).click(function previousTab() {
-          tabsBar.dispatchEvent(new Event('previousTab'));
-        });
-        $(icons.right.touch).click(function nextTab() {
-          tabsBar.dispatchEvent(new Event('nextTab'));
-        });
-        $(icons.left.scroll).click(leftScroll);
-        $(icons.right.scroll).click(rightScroll);
-
-        // Update scrollability classes for tabs bar wrapper.
-        MUITabs.checkScrollability.bind(tabsBar)();
-
-        $(tabsBar).removeClass('activating').addClass('activated');
-      });
-    $parentElement
-      .find('.mui-tabs__bar:not(.activating-aria):not(.activated-aria)')
-      .each(function handleTabsetARIA() {
-        const tabsBar = this;
-        $(tabsBar).addClass('activating-aria');
-        // http://web-accessibility.carnegiemuseums.org/code/tabs/
-        let index = 0;
-        const $tabs = $(tabsBar).find('[role=tab]');
-        tabsBar.addEventListener('previousTab', function previousTab(ev) {
-          if (index > 0) index -= 1;
-          else index = $tabs.length - 1;
-          ev.target.dispatchEvent(new Event('activateCurrent'));
-        });
-        tabsBar.addEventListener('nextTab', function nextTab(ev) {
-          if (index < $tabs.length - 1) index += 1;
-          else index = 0;
-          ev.target.dispatchEvent(new Event('activateCurrent'));
-        });
-        tabsBar.addEventListener('activateCurrent', function activateCurrent() {
-          window.mui.tabs.activate($($tabs.get(index)).data('mui-controls'));
-        });
-        // Bind arrow keys to previous/next for accessibility.
-        $tabs.on('keydown', function onpress(event) {
-          const LEFT_ARROW = 37;
-          const UP_ARROW = 38;
-          const RIGHT_ARROW = 39;
-          const DOWN_ARROW = 40;
-          const k = event.which || event.keyCode;
-
-          if (k >= LEFT_ARROW && k <= DOWN_ARROW) {
-            if (k === LEFT_ARROW || k === UP_ARROW)
-              tabsBar.dispatchEvent(new Event('previousTab'));
-            else if (k === RIGHT_ARROW || k === DOWN_ARROW)
-              tabsBar.dispatchEvent(new Event('nextTab'));
-            event.preventDefault();
-          }
-        });
-        $tabs.each(function handleTab(tabIndex) {
-          const tab = this;
-          // Attach event listeners to update accessibility attributes of tabs shown/hidden.
-          tab.addEventListener('mui.tabs.showend', function ariaActive(ev) {
-            $(ev.srcElement).attr({ tabindex: 0, 'aria-selected': 'true' }).focus();
-            index = tabIndex;
-            ev.srcElement.scrollIntoViewIfNeeded();
-
-            // Uncomment to enable hiding of nav on
-            // non-touch devices, when first / last tab
-            // is activated. This will need more work at init, if enabled.
-
-            // if (index === 0) $tabsBarContainer.addClass('tabs-active-first');
-            // else $tabsBarContainer.removeClass('tabs-active-first');
-            // if (index === $tabs.length - 1)
-            //   $tabsBarContainer.addClass('tabs-active-last');
-            // else $tabsBarContainer.removeClass('tabs-active-last');
-          });
-          tab.addEventListener('mui.tabs.hideend', function ariaInactive(ev) {
-            $(ev.srcElement).attr({ tabindex: '-1', 'aria-selected': 'false' });
-          });
-        });
-        $(tabsBar).removeClass('activating-aria').addClass('activated-aria');
-      });
+    const $tablists = $parentElement.find(
+      '[role=tablist]:not(.activating):not(.activated)'
+    );
+    $tablists.addClass('activating');
+    this.process($parentElement, $tablists);
   },
-  createIconset() {
-    return {
-      left: {
-        touch: this.createIcon('touch', 'left'),
-        scroll: this.createIcon('scroll', 'left'),
-      },
-      right: {
-        touch: this.createIcon('touch', 'right'),
-        scroll: this.createIcon('scroll', 'right'),
-      },
-    };
-  },
-  createIcon(mode, direction) {
-    return Utils.getFaiconHTML(`angle-${direction}`, 'body', true, [
-      `tabs-nav-icon-${direction}`,
-      `js-tabs-${mode}`,
-    ]);
+  helpers: {
+    createIconset() {
+      return {
+        left: {
+          touch: this.createIcon('touch', 'left'),
+          scroll: this.createIcon('scroll', 'left'),
+        },
+        right: {
+          touch: this.createIcon('touch', 'right'),
+          scroll: this.createIcon('scroll', 'right'),
+        },
+      };
+    },
+    createIcon(mode, direction) {
+      return Utils.getFaiconHTML(`angle-${direction}`, 'body', true, [
+        `tabs-nav-icon-${direction}`,
+        `js-tabs-${mode}`,
+      ]);
+    },
+    hasOverflow(el) {
+      return el.scrollLeft + el.scrollWidth > el.clientWidth;
+    },
+    hasLeftOverflow(el) {
+      return Boolean(el.scrollLeft);
+    },
+    hasRightOverflow(el) {
+      return (
+        el.scrollLeft + el.clientWidth + el.offsetLeft <
+        el.children[el.children.length - 1].offsetLeft +
+          el.children[el.children.length - 1].clientWidth
+      );
+    },
   },
 };
 
-export default MUITabs;
+export default Tabs;
--- funnel/utils/markdown/tabs.py
+++ funnel/utils/markdown/tabs.py
@@ -3,41 +3,16 @@ from __future__ import annotations
 
 from dataclasses import dataclass, field
 from functools import reduce
-from typing import ClassVar, Dict, List, Optional, Tuple
+from typing import Any, ClassVar, Dict, List, Optional, Tuple
+
+from markdown_it.token import Token
 
 __all__ = ['render_tab']
 
 
-def get_tab_tokens(tokens):
-    return [
-        {
-            'index': i,
-            'nesting': token.nesting,
-            'info': token.info,
-            'key': '-'.join([token.markup, str(token.level)]),
-        }
-        for i, token in enumerate(tokens)
-        if token.type in ('container_tab_open', 'container_tab_close')
-    ]
-
-
-def get_pair(tab_tokens, start=0) -> Optional[Tuple[int, int]]:
-    i = 1
-    while i < len(tab_tokens):
-        if (
-            tab_tokens[i]['nesting'] == -1
-            and tab_tokens[0]['key'] == tab_tokens[i]['key']
-        ):
-            break
-        i += 1
-    if i >= len(tab_tokens):
-        return None
-    return (start, start + i)
-
-
-def render_tab(self, tokens, idx, _options, env):
+def render_tab(self, tokens: List[Token], idx, _options, env):
     if 'manager' not in env:
-        env['manager'] = TabsManager(get_tab_tokens(tokens))
+        env['manager'] = TabsManager(tokens)
 
     node = env['manager'].index(idx)
     if node is None:
@@ -97,7 +72,7 @@ class TabNode:
     _item_html: ClassVar[str] = (
         '<li role="presentation"{class_attr}>'
         + '<a role="tab" href="javascript:void(0)" id="{tab_id}"'
-        + ' aria-controls="{tab_id}-panel" {accessibility}>{title}</a></li>'
+        + ' aria-controls="{tab_id}-panel"{accessibility}>{title}</a></li>'
     )
 
     def _class_attr(self, classes=None):
@@ -175,18 +150,23 @@ class TabsManager:
     tabsets: List[TabsetNode]
     _index: Dict[int, TabNode]
 
-    def __init__(self, tab_tokens) -> None:
+    def __init__(self, tokens: List[Token]) -> None:
+        tab_tokens = self._get_tab_tokens(tokens)
         self.tabsets: List[TabsetNode] = self.make(tab_tokens)
         self._index = {}
         self.index()
 
-    def make(self, tab_tokens, parent: Optional[TabNode] = None) -> List[TabsetNode]:
+    def make(
+        self, tab_tokens: List[Dict[str, Any]], parent: Optional[TabNode] = None
+    ) -> List[TabsetNode]:
         open_index, close_index = 0, len(tab_tokens) - 1
         nodes: List[TabNode] = []
         tabsets: List[TabsetNode] = []
         previous: Optional[TabNode] = None
         while True:
-            pairs = get_pair(tab_tokens[open_index : close_index + 1], start=open_index)
+            pairs = self._tab_token_pair(
+                tab_tokens[open_index : close_index + 1], start=open_index
+            )
             if pairs is None:
                 break
             open_index, close_index = pairs
@@ -216,6 +196,33 @@ class TabsManager:
             previous = node
 
         return tabsets
+
+    def _get_tab_tokens(self, tokens: List[Token]) -> List[Dict[str, Any]]:
+        return [
+            {
+                'index': i,
+                'nesting': token.nesting,
+                'info': token.info,
+                'key': '-'.join([token.markup, str(token.level)]),
+            }
+            for i, token in enumerate(tokens)
+            if token.type in ('container_tab_open', 'container_tab_close')
+        ]
+
+    def _tab_token_pair(
+        self, tab_tokens: List[Dict[str, Any]], start=0
+    ) -> Optional[Tuple[int, int]]:
+        i = 1
+        while i < len(tab_tokens):
+            if (
+                tab_tokens[i]['nesting'] == -1
+                and tab_tokens[0]['key'] == tab_tokens[i]['key']
+            ):
+                break
+            i += 1
+        if i >= len(tab_tokens):
+            return None
+        return (start, start + i)
 
     def index(self, start: Optional[int] = None) -> Optional[TabNode]:
         if start is not None:
