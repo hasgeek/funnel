6004b56c065a008a25f30774340ff9ae6969ab4b
Amogh M Aradhya <amoghmanjunath99@gmail.com>
2023-02-27 12:42:44 +0530

    Migrated to Selenium

--- tests/e2e/account_user/test_account_create.py
+++ tests/e2e/account_user/test_account_create.py
@@ -1,6 +1,10 @@
 import time
 
 from pytest_bdd import given, parsers, scenarios, then, when
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.wait import WebDriverWait
 import pytest
 
 scenarios('account_user/account_create.feature')
@@ -13,8 +17,8 @@ pytestmark = pytest.mark.usefixtures('live_server')
 
 
 @given("Anonymous visitor is on the home page")
-def given_anonuser_home_page(live_server, browser, db_session):
-    browser.visit(live_server.url)
+def given_anonuser_home_page(live_server, selenium, db_session):
+    selenium.get(live_server.url)
 
 
 @when(
@@ -25,49 +29,56 @@ def given_anonuser_home_page(live_server, browser, db_session):
     target_fixture='anon_username',
 )
 def when_anonuser_navigates_login_and_submits(
-    app, live_server, browser, phone_or_email
+    app, live_server, selenium, phone_or_email
 ):
-    assert browser.url == live_server.url
-    #: This doesn't work, no clue why
-    # browser.links.find_by_partial_href('/login').click()
-    #: This does work, but it doesn't test if there's a Login link on screen
-    # browser.visit(app.url_for('login', _external=True))
-    #: This one is the working hack, asking browser to visit the found link
-    browser.visit(browser.links.find_by_partial_href('/login')['href'])
+    assert selenium.current_url == live_server.url
+    wait = WebDriverWait(selenium, 10)
+    time.sleep(1)
+    # Ideally wait.until() should wait until the element is clickable,
+    # but the test is failing when used without time.sleep
+    wait.until(ec.element_to_be_clickable((By.CLASS_NAME, 'header__button'))).send_keys(
+        Keys.RETURN
+    )
     if phone_or_email == "a phone number":
         username = '8123456789'
     elif phone_or_email == "an email address":
         username = 'anon@example.com'
     else:
         pytest.fail("Unknown username type")
-    browser.find_by_name('username').fill(username)
-    browser.find_by_css('#form-passwordlogin button').click()
+    wait.until(ec.element_to_be_clickable((By.NAME, "username"))).send_keys(username)
+    time.sleep(1)
+    wait.until(
+        ec.element_to_be_clickable((By.CSS_SELECTOR, '#form-passwordlogin button'))
+    ).send_keys(Keys.ENTER)
     return {'phone_or_email': phone_or_email, 'username': username}
 
 
 @then("they are prompted for their name and the OTP, which they provide")
-def then_anonuser_prompted_name_and_otp(live_server, browser, anon_username):
+def then_anonuser_prompted_name_and_otp(live_server, selenium, anon_username):
     time.sleep(1)
-    browser.find_by_name('fullname').fill("Twoflower")
+    selenium.find_element(By.NAME, 'fullname').send_keys("Twoflower")
     if anon_username['phone_or_email'] == "a phone number":
         otp = live_server.transport_calls.sms[-1].vars['otp']
     elif anon_username['phone_or_email'] == "an email address":
         otp = live_server.transport_calls.email[-1].subject.split(' ')[-1]
     else:
         pytest.fail("Unknown username type")
-    browser.find_by_name('otp').fill(otp)
-    browser.find_by_css('#form-otp button').click()
+    selenium.find_element(By.NAME, 'otp').send_keys(otp)
+    selenium.find_element(By.CSS_SELECTOR, '#form-otp button').send_keys(Keys.ENTER)
 
 
 @then("they get an account and are logged in")
-def then_they_are_logged_in(browser):
-    assert browser.is_text_present("You are now one of us")
-    browser.quit()
+def then_they_are_logged_in(selenium):
+    time.sleep(1)
+    assert (
+        selenium.find_element(By.CLASS_NAME, "alert__text").text
+        == "You are now one of us. Welcome aboard!"
+    )
 
 
 @given("Twoflower visitor is on the home page")
-def when_twoflower_visits_homepage(live_server, browser, db_session, user_twoflower):
-    browser.visit(live_server.url)
+def when_twoflower_visits_homepage(live_server, selenium, db_session, user_twoflower):
+    selenium.get(live_server.url)
     user_twoflower.password = 'te@pwd3289'  # nosec
     user_twoflower.add_phone('+12345678900')
     user_twoflower.add_email('twoflower@example.org')
@@ -75,70 +86,74 @@ def when_twoflower_visits_homepage(live_server, browser, db_session, user_twoflo
 
 
 @when("they navigate to the login page")
-def when_navigate_to_login_page(app, live_server, browser):
-    browser.visit(app.url_for('login', _external=True))
+def when_navigate_to_login_page(app, live_server, selenium):
+    wait = WebDriverWait(selenium, 10)
+    time.sleep(1)
+    wait.until(ec.element_to_be_clickable((By.CLASS_NAME, 'header__button'))).send_keys(
+        Keys.RETURN
+    )
 
 
 @when("they submit the email address with password")
 @when("submit an email address with password")
-def when_submit_email_password(browser):
+def when_submit_email_password(selenium):
+    WebDriverWait(selenium, 10)
     time.sleep(1)
-    browser.find_by_name('username').fill('twoflower@example.org')
-    browser.find_by_id("use-password-login").click()
-    browser.find_by_name('password').fill('te@pwd3289')
-    browser.find_by_id("login-btn").click()
+    selenium.find_element(By.NAME, 'username').send_keys('twoflower@example.org')
+    selenium.find_element(By.ID, 'use-password-login').click()
+    selenium.find_element(By.NAME, 'password').send_keys('te@pwd3289')
+    selenium.find_element(By.ID, 'login-btn').send_keys(Keys.ENTER)
 
 
 @then("they are logged in")
-def then_logged_in(browser):
-    assert browser.is_text_present("You are now logged in")
-    browser.quit()
+def then_logged_in(selenium):
+    time.sleep(1)
+    assert (
+        selenium.find_element(By.CLASS_NAME, "alert__text").text
+        == "You are now logged in"
+    )
 
 
 @when("they submit the phone number with password")
 @when("submit a phone number with password")
-def when_submit_phone_password(app, live_server, browser):
+def when_submit_phone_password(app, live_server, selenium):
+    WebDriverWait(selenium, 10)
     time.sleep(1)
-    browser.find_by_name('username').fill('+12345678900')
-    browser.find_by_id("use-password-login").click()
-    browser.find_by_name('password').fill('te@pwd3289')
-    browser.find_by_id("login-btn").click()
+    selenium.find_element(By.NAME, 'username').send_keys('+12345678900')
+    selenium.find_element(By.ID, 'use-password-login').click()
+    selenium.find_element(By.NAME, 'password').send_keys('te@pwd3289')
+    selenium.find_element(By.ID, 'login-btn').send_keys(Keys.ENTER)
 
 
 @given("Anonymous visitor is on a project page")
-def given_anonymous_project_page(live_server, browser, db_session, new_project):
+def given_anonymous_project_page(live_server, selenium, db_session, new_project):
     new_project.publish()
     db_session.add(new_project)
     db_session.commit()
-    browser.visit(live_server.url + new_project.profile.name + '/' + new_project.name)
+    selenium.get(live_server.url + new_project.profile.name + '/' + new_project.name)
 
 
 @when("they click on follow")
-def when_they_click_follow(browser):
+def when_they_click_follow(selenium):
     time.sleep(1)
-    browser.find_by_id('register-nav').click()
-    # element = WebDriverWait(browser.driver, 10).until(
-    #     EC.presence_of_element_located(
-    #         (
-    #             By.ID,
-    #             'register-nav',
-    #         )
-    #     )
-    # )
-    # element.click()
+    selenium.find_element(By.ID, 'register-nav').send_keys(Keys.ENTER)
 
 
 @then("a register modal appears")
-def then_register_modal_appear(browser):
+def then_register_modal_appear(selenium):
     time.sleep(1)
-    assert browser.is_text_present("Tell us where you’d like to get updates.")
+    assert (
+        selenium.find_element(By.XPATH, '//*[@id="passwordform"]/p[2]').text
+        == "Tell us where you’d like to get updates. We’ll send an OTP to confirm."
+    )
 
 
 @when(
     parsers.re("they enter (?P<phone_or_email>a phone number|an email address)"),
     target_fixture='anon_username',
 )
-def when_they_enter_email(browser, phone_or_email):
+def when_they_enter_email(selenium, phone_or_email):
+    wait = WebDriverWait(selenium, 10)
     time.sleep(1)
     if phone_or_email == "a phone number":
         username = '8123456789'
@@ -146,14 +161,17 @@ def when_they_enter_email(browser, phone_or_email):
         username = 'anon@example.com'
     else:
         pytest.fail("Unknown username type")
-    browser.find_by_name('username').fill(username)
-    browser.find_by_css('#form-passwordlogin button').click()
+    wait.until(ec.element_to_be_clickable((By.NAME, "username"))).send_keys(username)
+    time.sleep(1)
+    wait.until(
+        ec.element_to_be_clickable((By.CSS_SELECTOR, '#form-passwordlogin button'))
+    ).send_keys(Keys.ENTER)
     return {'phone_or_email': phone_or_email, 'username': username}
 
 
 @given("Twoflower is on the project page")
 def given_twoflower_visits_project(
-    live_server, browser, db_session, user_twoflower, new_project
+    live_server, selenium, db_session, user_twoflower, new_project
 ):
     user_twoflower.password = 'te@pwd3289'  # nosec
     user_twoflower.add_phone('+12345678900')
@@ -161,4 +179,4 @@ def given_twoflower_visits_project(
     new_project.publish()
     db_session.add(new_project)
     db_session.commit()
-    browser.visit(live_server.url + new_project.profile.name + '/' + new_project.name)
+    selenium.get(live_server.url + new_project.profile.name + '/' + new_project.name)
