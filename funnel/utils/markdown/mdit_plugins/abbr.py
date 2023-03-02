56eda66355d8c9f75b4bc073f63c22e0fcb9ef52
Mitesh Ashar <email@miteshashar.com>
2023-03-02 06:18:24 +0530

    Makes abbreviation storage and tagging faster. Resolves other minor issues raised in review.

--- funnel/utils/markdown/mdit_plugins/abbr.py
+++ funnel/utils/markdown/mdit_plugins/abbr.py
@@ -5,7 +5,6 @@ Ported from javascript plugin markdown-it-abbr.
 """
 
 import re
-import unicodedata
 
 from markdown_it import MarkdownIt
 from markdown_it.rules_block import StateBlock
@@ -14,62 +13,35 @@ from markdown_it.token import Token
 
 __all__ = ['abbr_plugin']
 
-ASTERISK_CHAR = 0x2A  # ASCII value for `*`
-OPEN_SQBR_CHAR = 0x5B  # ASCII value for `[`
-CLOSE_SQBR_CHAR = 0x5D  # ASCII value for `]`
-BACKSLASH_CHAR = 0x5C  # ASCII value for `\`
-COLON_CHAR = 0x3A  # ASCII value for `:`
-
-PADDING_CHARACTERS_RE = (
-    '['
-    + ''.join(
-        chr(x) for x in range(65536) if unicodedata.category(chr(x))[0] in ('P', 'Z')
-    )
-    .replace('\\', '\\\\')
-    .replace('[', '\\[')
-    .replace(']', '\\]')
-    + '\r\n$+<=>^`|~'
-    + ']'
-)
+abbr_def_re = re.compile(r'^\*\[([\\\w]+)\]:(.+)$')
 
 
-def abbr_def(state: StateBlock, start_line: int, end_line: int, silent: bool):
+def abbr_def(state: StateBlock, start_line: int, end_line: int, silent: bool) -> bool:
+    """Store abbreviation definitions in env and remove them from content."""
     pos = state.bMarks[start_line] + state.tShift[start_line]
     maximum = state.eMarks[start_line]
 
     if pos + 2 >= maximum:
         return False
 
-    # Check if line is of the format `*[Abbreviation]:Full Form`
-    if state.srcCharCode[pos] != ASTERISK_CHAR:
-        return False
-    pos += 1
-    if state.srcCharCode[pos] != OPEN_SQBR_CHAR:
-        return False
-    pos += 1
+    line = state.src[pos:maximum]
 
-    label_end = -1
-    label_start = pos
-    while pos < maximum:
-        ch = state.srcCharCode[pos]
-        if ch == OPEN_SQBR_CHAR:  # pylint: disable=no-else-return
-            return False
-        elif ch == CLOSE_SQBR_CHAR:
-            label_end = pos
-            break
-        elif ch == BACKSLASH_CHAR:
-            pos += 1
-        pos += 1
+    if not line.startswith('*['):
+        return False
 
-    if label_end < 0 or state.srcCharCode[label_end + 1] != COLON_CHAR:
+    result = abbr_def_re.match(line)
+
+    if result is None:
         return False
 
     if silent:
         return True
 
     # Extract label and title and store it in state.env
-    label = state.src[label_start:label_end].replace('\\', '')
-    title = state.src[label_end + 2 : maximum].strip()
+
+    label = result.group(1).replace('\\', '')
+    title = result.group(2).strip()
+
     if len(label) == 0 or len(title) == 0:
         return False
 
@@ -84,6 +56,7 @@ def abbr_def(state: StateBlock, start_line: int, end_line: int, silent: bool):
 
 
 def abbr_replace(state: StateInline):
+    """Tokenizes and tags defined abbreviations in content."""
     block_tokens = state.tokens
 
     if 'abbr' not in state.env:
@@ -95,11 +68,7 @@ def abbr_replace(state: StateInline):
 
     simple_re = re.compile('(?:' + labels_re_str + ')')
 
-    match_re_str = (
-        '(^|' + PADDING_CHARACTERS_RE + ')'
-        '(' + labels_re_str + ')'
-        '($|' + PADDING_CHARACTERS_RE + ')'
-    )
+    match_re_str = r'(^|\W)(' + labels_re_str + r')($|\W)'
 
     match_re = re.compile(match_re_str)
 
