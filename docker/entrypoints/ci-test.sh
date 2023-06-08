be04cd70878a66ca9ce9d644353229c9e556b23a
Mitesh Ashar <email@miteshashar.com>
2023-06-08 08:02:54 +0530

    Corrections in CI docker tests setup

--- docker/entrypoints/ci-test.sh
+++ docker/entrypoints/ci-test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-cp .ci-cache/files/.eslintcache .eslintcache
+if [[ -f ".ci-cache/files/.eslintcache" ]]
+then
+    cp .ci-cache/files/.eslintcache .eslintcache
+fi
 make install-test
 pytest --gherkin-terminal-reporter -vv --showlocals --cov=funnel
 coverage lcov -o coverage/funnel.lcov
