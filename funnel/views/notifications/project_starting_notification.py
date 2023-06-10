5b15dc40c8dc605e33cee77fff5ce001017f971e
Amogh M Aradhya <amoghmanjunath99@gmail.com>
2023-06-10 12:11:58 +0530

    Updated ProjectStartingTemplate Notification

--- funnel/views/notifications/project_starting_notification.py
+++ funnel/views/notifications/project_starting_notification.py
@@ -10,9 +10,33 @@ from baseframe import _, __
 from baseframe.filters import time_filter
 
 from ...models import Project, ProjectStartingNotification, Session
-from ...transports.sms import OneLineTemplate
+from ...transports.sms import SmsTemplate
 from ..helpers import shortlink
 from ..notification import RenderNotification
+
+
+class ProjectStartingTemplate(SmsTemplate):
+    registered_template = 'Reminder: {#var#} is starting soon. Join at {#var#}\n\n{#var#} to stop - Hasgeek'
+    template = 'Reminder: {project} is starting soon. Join at {url}\n\n{unsubscribe_url} to stop - Hasgeek'
+    plaintext_template = 'Reminder: {project} is starting soon. Join at {url}'
+
+    project: str
+    url: str
+    unsubscribe_url: str
+
+    @property
+    def unsubscribe_url(self) -> str:
+        return "https://bye.li"
+
+    def available_var_len(self):
+        """Discount the two URLs from available length."""
+        return self.template_var_len - len(self.url) - len(self.unsubscribe_url)
+
+    def truncate(self) -> None:
+        """Truncate text1 to fit."""
+        max_text_length = self.available_var_len()
+        if len(self.project) > max_text_length:
+            self.project = self.project[: max_text_length - 1] + '…'
 
 
 @ProjectStartingNotification.renderer
@@ -41,12 +65,9 @@ class RenderProjectStartingNotification(RenderNotification):
             'notifications/project_starting_email.html.jinja2', view=self
         )
 
-    def sms(self) -> OneLineTemplate:
-        return OneLineTemplate(
-            text1=_("{project} starts at {time}.").format(
-                project=self.project.joined_title,
-                time=time_filter((self.session or self.project).start_at_localized),
-            ),
+    def sms(self):
+        return ProjectStartingTemplate(
+            project=self.project.joined_title,
             url=shortlink(
                 self.project.url_for(_external=True, **self.tracking_tags('sms')),
                 shorter=True,
