06c1ac8b26c28661fd303769bae2b842d8f5a798
Mitesh Ashar <email@miteshashar.com>
2023-06-14 06:38:54 +0530

    Attempt to use resolve cache bind mounts' ownership issues for docker CI setup

--- ci.Dockerfile
+++ ci.Dockerfile
@@ -5,9 +5,11 @@
 
 FROM hasgeek/funnel-base-devtest
 LABEL name="FunnelCI" version="0.1"
-USER pn
+USER 1000:1000
 RUN \
     mkdir -pv /home/pn/.npm /home/pn/app/node_modules /home/pn/.cache/pip \
+    /home/pn/app/coverage /home/pn/.local && \
+    chown -R 1000:1000 /home/pn/.npm /home/pn/app /home/pn/.cache \
     /home/pn/app/coverage /home/pn/.local
 
 WORKDIR /home/pn/app
--- docker/entrypoints/ci-test.sh
+++ docker/entrypoints/ci-test.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 
-chown pn:pn /home/pn/.npm /home/pn/.cache /home/pn/.cache/pip /home/pn/app \
-    /home/pn/app/coverage /home/pn/.local
 make install-test
 pytest --allow-hosts=127.0.0.1,::1,$(hostname -i),$(getent ahosts db-test | awk '/STREAM/ { print $1}'),$(getent ahosts redis-test | awk '/STREAM/ { print $1}') --gherkin-terminal-reporter -vv --showlocals --cov=funnel
 coverage lcov -o coverage/funnel.lcov
