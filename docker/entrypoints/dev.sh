94e803a9c0193e49de9c4dbb529d7039e392af39
Mitesh Ashar <email@miteshashar.com>
2023-06-15 04:11:19 +0530

    Remove OBJC_DISABLE_INITIALIZE_FORK_SAFETY from docker setup; do not run db migration in dev entrypoint

--- docker/entrypoints/dev.sh
+++ docker/entrypoints/dev.sh
@@ -3,8 +3,6 @@
 if [ "$(psql -XtA -U postgres -h $DB_HOST funnel -c "select count(*) from information_schema.tables where table_schema = 'public';")" = "0" ]; then
     flask dbcreate
     flask db stamp
-else
-    flask db upgrade head
 fi
 
 ./devserver.py
