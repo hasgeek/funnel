cf35b0a97c62e8dcd4a98e2c3259c5b3da4361b6
Mitesh Ashar <email@miteshashar.com>
2023-06-14 15:13:52 +0530

    Remove obsolete files

--- runserver.production.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
-export FLASK_ENV="production"
-export FLASK_RUN_HOST="${FLASK_RUN_HOST:=0.0.0.0}"
-export FLASK_RUN_PORT="${FLASK_RUN_PORT:=3000}"
-flask db upgrade
-uwsgi --ini ./uwsgi.ini
