f48d90f5760744b74c0fa71f447fa5a459f46d66
Mitesh Ashar <email@miteshashar.com>
2023-06-25 15:17:59 +0530

    Add postfix setup to docker compose

--- docker/entrypoints/migrate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ "$(psql -XtA -U postgres -h $DB_HOST $DB_FUNNEL -c "select count(*) from information_schema.tables where table_schema = 'public';")" = "0" ]; then
-    flask dbcreate
-    flask db stamp
-else
-    flask db upgrade head
-fi
