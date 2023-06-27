a5575d40157f2819792333e2bca11f11fc499802
Mitesh Ashar <email@miteshashar.com>
2023-06-27 21:39:09 +0530

    Move coverage report preparation for docker-based CI back into entrypoint shell script

--- docker/entrypoints/ci.sh
+++ docker/entrypoints/ci.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-GITHUB_ACTIONS=true pytest \
+pytest \
     --allow-hosts=127.0.0.1,::1,$(hostname -i),$(getent ahosts $DB_HOST | awk '/STREAM/ { print $1}'),$(getent ahosts $REDIS_HOST | awk '/STREAM/ { print $1}') \
     --gherkin-terminal-reporter -vv --showlocals --cov=funnel $@
+coverage lcov -o coverage/funnel.lcov
