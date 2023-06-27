f814079315ce44ea62a018d6e64b6c8fbed20f7e
Mitesh Ashar <email@miteshashar.com>
2023-06-27 20:38:51 +0530

    Try running tests with docker run in the GHA pytest docker workflow

--- docker/entrypoints/ci.sh
+++ docker/entrypoints/ci.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-pytest \
+GITHUB_ACTIONS=true pytest \
     --allow-hosts=127.0.0.1,::1,$(hostname -i),$(getent ahosts $DB_HOST | awk '/STREAM/ { print $1}'),$(getent ahosts $REDIS_HOST | awk '/STREAM/ { print $1}') \
     --gherkin-terminal-reporter -vv --showlocals --cov=funnel $@
