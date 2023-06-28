554434ea92bf14d1364ac1316acd07dfbd9d8a92
Mitesh Ashar <email@miteshashar.com>
2023-06-28 22:39:51 +0530

    Change entrypoint shell script for docker CI to not exit with code 0 for pytest failures

--- docker/entrypoints/ci.sh
+++ docker/entrypoints/ci.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+
+set -e
 
 pytest \
     --allow-hosts=127.0.0.1,::1,$(hostname -i),$(getent ahosts $DB_HOST | awk '/STREAM/ { print $1}'),$(getent ahosts $REDIS_HOST | awk '/STREAM/ { print $1}') \
