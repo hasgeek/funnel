ee9b62f8ebcb2e1ebc38f90df251cf0f5e8586d4
Vidya Ramakrishnan <vidya@hasgeek.in>
2023-07-11 23:09:36 +0530

    Use  Intl.DateTimeFormat() to set timezone cookie

---
+++ funnel/assets/js/utils/timezone.js
@@ -0,0 +1,15 @@
+import 'jquery.cookie';
+
+// Detect timezone for login
+async function setTimezoneCookie() {
+  if (!$.cookie('timezone') || $.cookie('timezone') === 'null') {
+    let timezone = Intl.DateTimeFormat()?.resolvedOptions()?.timeZone;
+    if (!timezone) {
+      const { default: jstz } = await import('jstz');
+      timezone = jstz.determine().name();
+    }
+    $.cookie('timezone', timezone, { path: '/' });
+  }
+}
+
+export default setTimezoneCookie;
