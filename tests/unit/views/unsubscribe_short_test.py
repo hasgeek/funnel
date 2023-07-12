83f3ea144d5a2136bf981bb4766df7d98cb811bb
Kiran Jonnalagadda <kiran@hasgeek.com>
2023-07-12 15:30:28 +0530

    Change unsubscribe app's index redirect destination and test it (#1796)

--- tests/unit/views/unsubscribe_short_test.py
+++ tests/unit/views/unsubscribe_short_test.py
@@ -5,6 +5,19 @@ from secrets import token_urlsafe
 from flask import Flask
 
 
+def test_unsubscribe_app_index(app: Flask, unsubscribeapp: Flask) -> None:
+    """Unsubscribe app redirects from index to main app's notification preferences."""
+    with unsubscribeapp.test_client() as client:
+        rv = client.get('/')
+        assert rv.status_code == 301
+    redirect_url: str = rv.location
+    assert redirect_url.startswith(('http://', 'https://'))
+    assert (
+        app.url_for('notification_preferences', utm_medium='sms', _external=True)
+        == redirect_url
+    )
+
+
 def test_unsubscribe_app_url_redirect(app: Flask, unsubscribeapp: Flask) -> None:
     """Unsubscribe app does a simple redirect to main app's unsubscribe URL."""
     random_token = token_urlsafe(3)
