af94b1e5090ddf554c4c3a8d85f10a3073b93480
Vidya Ramakrishnan <vidya@hasgeek.in>
2023-07-12 10:47:16 +0530

    Fix timezone cookie check

--- funnel/assets/js/utils/timezone.js
+++ funnel/assets/js/utils/timezone.js
@@ -2,7 +2,7 @@ import 'jquery.cookie';
 
 // Detect timezone for login
 async function setTimezoneCookie() {
-  if (!$.cookie('timezone') || $.cookie('timezone') === 'null') {
+  if (!$.cookie('timezone')) {
     let timezone = Intl.DateTimeFormat()?.resolvedOptions()?.timeZone;
     if (!timezone) {
       const { default: jstz } = await import('jstz');
