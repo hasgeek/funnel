778558fa85e06ffeaf39028b68cab4e445f614b5
Kiran Jonnalagadda <kiran@hasgeek.com>
2023-08-05 20:12:47 +0530

    Fix test mock

--- tests/unit/transports/email_test.py
+++ tests/unit/transports/email_test.py
@@ -1,9 +1,8 @@
 """Test email transport functions."""
 
-from unittest.mock import patch
 
 import pytest
-from flask_mailman.message import EmailMessage, sanitize_address
+from flask_mailman.message import sanitize_address
 
 from funnel.models import EmailAddress
 from funnel.transports import TransportRecipientError
@@ -57,33 +56,28 @@ def test_process_recipient() -> None:
 @pytest.mark.usefixtures('db_session')
 def test_send_email_blocked() -> None:
     """Confirm that send_email will raise an exception on a blocked email address."""
-    with patch.object(EmailMessage, 'send', return_value=None):
-        ea = EmailAddress.add('blocked@example.com')
-        EmailAddress.mark_blocked(ea.email)
-        with pytest.raises(
-            TransportRecipientError, match='This email address has been blocked'
-        ):
-            send_email("Email subject", ['blocked@example.com'], "Email content")
+    ea = EmailAddress.add('blocked@example.com')
+    EmailAddress.mark_blocked(ea.email)
+    with pytest.raises(
+        TransportRecipientError, match='This email address has been blocked'
+    ):
+        send_email("Email subject", ['blocked@example.com'], "Email content")
 
 
 @pytest.mark.usefixtures('db_session')
 def test_send_email_hard_bouncing() -> None:
     """Confirm that send_email will raise an exception on a hard bouncing email."""
-    with patch.object(EmailMessage, 'send', return_value=None):
-        ea = EmailAddress.add('hard-fail@example.com')
-        ea.mark_hard_fail()
-        with pytest.raises(
-            TransportRecipientError, match='This email address is bouncing'
-        ):
-            send_email("Email subject", ['hard-fail@example.com'], "Email content")
+    ea = EmailAddress.add('hard-fail@example.com')
+    ea.mark_hard_fail()
+    with pytest.raises(TransportRecipientError, match='This email address is bouncing'):
+        send_email("Email subject", ['hard-fail@example.com'], "Email content")
 
 
 @pytest.mark.usefixtures('db_session')
 def test_send_email_soft_bouncing() -> None:
     """Confirm that send_email will not fail on a soft bouncing email address."""
-    with patch.object(EmailMessage, 'send', return_value=None):
-        ea = EmailAddress.add('soft-fail@example.com')
-        ea.mark_soft_fail()
-        assert isinstance(
-            send_email("Email subject", ['soft-fail@example.com'], "Email content"), str
-        )
+    ea = EmailAddress.add('soft-fail@example.com')
+    ea.mark_soft_fail()
+    assert isinstance(
+        send_email("Email subject", ['soft-fail@example.com'], "Email content"), str
+    )
