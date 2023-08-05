a9ea59ec83d3b59d89eb118446688fafcafa9b22
Kiran Jonnalagadda <kiran@hasgeek.com>
2023-08-05 18:23:02 +0530

    Reject email to known bouncing addresses and test it

--- tests/unit/transports/email_test.py
+++ tests/unit/transports/email_test.py
@@ -1,8 +1,13 @@
 """Test email transport functions."""
 
-from flask_mailman.message import sanitize_address
+from unittest.mock import patch
 
-from funnel.transports.email import process_recipient
+import pytest
+from flask_mailman.message import EmailMessage, sanitize_address
+
+from funnel.models import EmailAddress
+from funnel.transports import TransportRecipientError
+from funnel.transports.email import process_recipient, send_email
 
 
 def test_process_recipient() -> None:
@@ -47,3 +52,38 @@ def test_process_recipient() -> None:
         )
     )
     assert process_recipient(("", "example@example.com")) == 'example@example.com'
+
+
+@pytest.mark.usefixtures('db_session')
+def test_send_email_blocked() -> None:
+    """Confirm that send_email will raise an exception on a blocked email address."""
+    with patch.object(EmailMessage, 'send', return_value=None):
+        ea = EmailAddress.add('blocked@example.com')
+        EmailAddress.mark_blocked(ea.email)
+        with pytest.raises(
+            TransportRecipientError, match='This email address has been blocked'
+        ):
+            send_email("Email subject", ['blocked@example.com'], "Email content")
+
+
+@pytest.mark.usefixtures('db_session')
+def test_send_email_hard_bouncing() -> None:
+    """Confirm that send_email will raise an exception on a hard bouncing email."""
+    with patch.object(EmailMessage, 'send', return_value=None):
+        ea = EmailAddress.add('hard-fail@example.com')
+        ea.mark_hard_fail()
+        with pytest.raises(
+            TransportRecipientError, match='This email address is bouncing'
+        ):
+            send_email("Email subject", ['hard-fail@example.com'], "Email content")
+
+
+@pytest.mark.usefixtures('db_session')
+def test_send_email_soft_bouncing() -> None:
+    """Confirm that send_email will not fail on a soft bouncing email address."""
+    with patch.object(EmailMessage, 'send', return_value=None):
+        ea = EmailAddress.add('soft-fail@example.com')
+        ea.mark_soft_fail()
+        assert isinstance(
+            send_email("Email subject", ['soft-fail@example.com'], "Email content"), str
+        )
