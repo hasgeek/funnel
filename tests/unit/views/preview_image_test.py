949a275d88ba2cd2df17c1deccd05e488e51d66f
anishTP <anish@hasgeek.com>
2023-11-07 19:35:25 +0530

    updated arguments to existing pytest fixture new_project

--- tests/unit/views/preview_image_test.py
+++ tests/unit/views/preview_image_test.py
@@ -2,10 +2,10 @@ import pytest
 
 from funnel.views.thumbnails import render_project_preview_image
 
-@pytest.mark.usefixtures('app_context')
+@pytest.mark.usefixtures('app_context', 'new_project')
 @pytest.fixture()
-def add_tasks_to_queue(project):
-    render_project_preview_image.queue(project_id=project.id, job_id=project.id)
+def add_tasks_to_queue(new_project):
+    render_project_preview_image.queue(project_id=new_project.id, job_id=new_project.id)
 
 @pytest.mark.usefixtures('add_tasks_to_queue', 'project_expo2011', 'project_ai1',
                          'project_ai2')
