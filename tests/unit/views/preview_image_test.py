f98aa1bffbcc13a704e11326206cfbc5871ea339
anishTP <anish@hasgeek.com>
2023-11-10 16:32:24 +0530

    Add mimetype sniffer and update test dependencies

--- tests/unit/views/preview_image_test.py
+++ tests/unit/views/preview_image_test.py
@@ -1,5 +1,6 @@
 import pytest
 import io
+from magic import from_buffer
 from PIL import Image
 
 from funnel.views.thumbnails import render_project_preview_image
@@ -14,5 +15,6 @@ def test_preview_image_jobs(project_expo2011) -> None:
 def test_preview_image_size(project_expo2011) -> None:
     render_project_preview_image(project_id=project_expo2011.id)
     preview_image = Image.open(io.BytesIO(project_expo2011.preview_image))
+    result = from_buffer(io.BytesIO(project_expo2011.preview_image).read(2048), mime=True)
     assert preview_image.size == (1280, 720)
-    assert preview_image.format is 'PNG'
+    assert result == 'image/png'
