b5c7b6270233328a8539d965ec127e4dc219db6f
anishTP <anish@hasgeek.com>
2023-11-10 17:20:04 +0530

    Add context manager for file handling

--- tests/unit/views/preview_image_test.py
+++ tests/unit/views/preview_image_test.py
@@ -14,7 +14,7 @@ def test_preview_image_jobs(project_expo2011) -> None:
 @pytest.mark.usefixtures('project_expo2011', 'all_fixtures')
 def test_preview_image_size(project_expo2011) -> None:
     render_project_preview_image(project_id=project_expo2011.id)
-    preview_image = Image.open(io.BytesIO(project_expo2011.preview_image))
-    result = from_buffer(io.BytesIO(project_expo2011.preview_image).read(2048), mime=True)
-    assert preview_image.size == (1280, 720)
-    assert result == 'image/png'
+    with Image.open(io.BytesIO(project_expo2011.preview_image)) as preview_image:
+        assert preview_image.size == (1280, 720)
+    image_mimetype = from_buffer(io.BytesIO(project_expo2011.preview_image).read(2048), mime=True)
+    assert image_mimetype == 'image/png'
