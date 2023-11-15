d3c909ff5c546aee244a3f37f459e095ff90b8d3
Kiran Jonnalagadda <kiran@hasgeek.com>
2023-11-15 12:39:04 +0530

    Typing fixes

--- funnel/transports/sms/__init__.py
+++ funnel/transports/sms/__init__.py
@@ -1,7 +1,5 @@
 """SMS transport support."""
 # flake8: noqa
 
-from __future__ import annotations
-
 from .send import *
 from .template import *
--- funnel/transports/whatsapp/__init__.py
+++ funnel/transports/whatsapp/__init__.py
@@ -1,7 +1,5 @@
 """WhatsApp transport support."""
 # flake8: noqa
 
-from __future__ import annotations
-
 from .send import *
 from .template import *
