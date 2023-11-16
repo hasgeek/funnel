6589575c89ca5643736e6b7284f24fcc84efa10d
Amogh M Aradhya <amoghmanjunath99@gmail.com>
2023-11-16 18:19:08 +0530

    Fixed tests

--- tests/e2e/account/pw_register_test.py
+++ tests/e2e/account/pw_register_test.py
@@ -1,4 +1,7 @@
 """Test account registration."""
+# pylint: disable=redefined-outer-name
+
+import re
 
 import pytest
 from playwright.sync_api import Page, expect
@@ -11,8 +14,21 @@ TWOFLOWER_EMAIL = 'twoflower@example.org'
 TWOFLOWER_PHONE = '+12015550123'
 TWOFLOWER_PASSWORD = 'te@pwd3289'  # nosec
 
+pytestmark = pytest.mark.filterwarnings(
+    "ignore:Object of type <AccountPhone> not in session",
+    "ignore:Object of type <AccountEmail> not in session",
+)
 
-def check_recaptcha_loaded(page: Page) -> None:
+
+@pytest.fixture()
+def user_twoflower_with_password_and_contact(user_twoflower):
+    user_twoflower.password = TWOFLOWER_PASSWORD
+    user_twoflower.add_phone(TWOFLOWER_PHONE)
+    user_twoflower.add_email(TWOFLOWER_EMAIL)
+    return user_twoflower
+
+
+def wait_until_recaptcha_loaded(page: Page) -> None:
     page.wait_for_selector(
         '#form-passwordlogin > div.g-recaptcha > div > div.grecaptcha-logo > iframe',
         timeout=10000,
@@ -42,7 +58,7 @@ def when_anonuser_navigates_login_and_submits(
     else:
         pytest.fail("Unknown username type")
     page.click('.header__button')
-    check_recaptcha_loaded(page)
+    wait_until_recaptcha_loaded(page)
     page.wait_for_selector('input[name=username]').fill(username)
     page.click('#form-passwordlogin button')
     return {'phone_or_email': phone_or_email, 'username': username}
@@ -54,7 +70,9 @@ def then_anonuser_prompted_name_and_otp(live_server, anon_username, page: Page) 
     if anon_username['phone_or_email'] == "a phone number":
         otp = live_server.transport_calls.sms[-1].vars['otp']
     elif anon_username['phone_or_email'] == "an email address":
-        otp = live_server.transport_calls.email[-1].subject.split(' ')[-1]
+        subject = live_server.transport_calls.email[-1].subject
+        otp_pattern = r"\b\d{4}\b"
+        otp = re.search(otp_pattern, subject).group(0)
     else:
         pytest.fail("Unknown username type")
     page.wait_for_selector('input[name=otp]').fill(otp)
@@ -62,21 +80,20 @@ def then_anonuser_prompted_name_and_otp(live_server, anon_username, page: Page) 
 
 
 @then("they get an account and are logged in")
-def then_they_are_logged_in(live_server, page: Page) -> None:
+def then_they_are_logged_in(
+    live_server, user_twoflower_with_password_and_contact, page: Page
+) -> None:
     assert (
-        page.get_by_text("You are now one of us. Welcome aboard!").inner_text()
+        page.wait_for_selector('.alert__text').inner_text()
         == "You are now one of us. Welcome aboard!"
     )
 
 
 @given("Twoflower visitor is on the home page")
 def when_twoflower_visits_homepage(
-    live_server, page: Page, db_session, user_twoflower
+    live_server, page: Page, db_session, user_twoflower_with_password_and_contact
 ) -> None:
     page.goto(live_server.url)
-    user_twoflower.password = TWOFLOWER_PASSWORD
-    user_twoflower.add_phone(TWOFLOWER_PHONE)
-    user_twoflower.add_email(TWOFLOWER_EMAIL)
     db_session.commit()
 
 
@@ -88,7 +105,7 @@ def when_navigate_to_login_page(app, live_server, page: Page):
 @when("they submit the email address with password")
 @when("submit an email address with password")
 def when_submit_email_password(page: Page) -> None:
-    check_recaptcha_loaded(page)
+    wait_until_recaptcha_loaded(page)
     page.wait_for_selector('input[name=username]').fill(TWOFLOWER_EMAIL)
     page.click('#use-password-login')
     page.wait_for_selector('input[name=password]').fill(TWOFLOWER_PASSWORD)
@@ -105,7 +122,7 @@ def then_logged_in(live_server, page: Page) -> None:
 @when("they submit the phone number with password")
 @when("submit a phone number with password")
 def when_submit_phone_password(app, live_server, page: Page) -> None:
-    check_recaptcha_loaded(page)
+    wait_until_recaptcha_loaded(page)
     page.wait_for_selector('input[name=username]').fill(TWOFLOWER_PHONE)
     page.click('#use-password-login')
     page.wait_for_selector('input[name=password]').fill(TWOFLOWER_PASSWORD)
@@ -140,7 +157,7 @@ def then_register_modal_appear(page: Page) -> None:
     target_fixture='anon_username',
 )
 def when_they_enter_email(page: Page, phone_or_email: str) -> dict[str, str]:
-    check_recaptcha_loaded(page)
+    wait_until_recaptcha_loaded(page)
     if phone_or_email == "a phone number":
         username = '8123456789'
     elif phone_or_email == "an email address":
@@ -154,11 +171,13 @@ def when_they_enter_email(page: Page, phone_or_email: str) -> dict[str, str]:
 
 @given("Twoflower is on the project page")
 def given_twoflower_visits_project(
-    live_server, page: Page, db_session, user_twoflower, new_project
+    live_server,
+    page: Page,
+    db_session,
+    user_twoflower,
+    new_project,
+    user_twoflower_with_password_and_contact,
 ) -> None:
-    user_twoflower.password = TWOFLOWER_PASSWORD
-    user_twoflower.add_phone(TWOFLOWER_PHONE)
-    user_twoflower.add_email(TWOFLOWER_EMAIL)
     new_project.publish()
     db_session.add(new_project)
     db_session.commit()
@@ -167,11 +186,12 @@ def given_twoflower_visits_project(
 
 @given("the server uses Recaptcha")
 def given_server_uses_recaptcha(
-    live_server, db_session, user_twoflower, new_project, funnel
+    live_server,
+    db_session,
+    user_twoflower_with_password_and_contact,
+    new_project,
+    funnel,
 ) -> None:
-    user_twoflower.password = TWOFLOWER_PASSWORD
-    user_twoflower.add_phone(TWOFLOWER_PHONE)
-    user_twoflower.add_email(TWOFLOWER_EMAIL)
     new_project.publish()
     db_session.add(new_project)
     db_session.commit()
