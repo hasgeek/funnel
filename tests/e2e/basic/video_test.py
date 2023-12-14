722ed0c9bce2afd4547c3c657263faf42f327f2c
Amogh M Aradhya <amoghmanjunath99@gmail.com>
2023-12-14 14:02:50 +0530

    Added private vimeo link

--- tests/e2e/basic/video_test.py
+++ tests/e2e/basic/video_test.py
@@ -33,17 +33,25 @@ def test_login_add_livestream(
     page.get_by_label("Update livestream URLs").click()
     page.get_by_label("Livestream URLs. One per line").click()
     page.get_by_label("Livestream URLs. One per line").fill(
-        "https://youtube.com/live/JdHDG6Zudi4?feature=share\nhttps://vimeo.com/828748049?share=copy"
+        "https://www.youtube.com/watch?v=dQw4w9WgXcQ\nhttps://vimeo.com/336892869\nhttps://player.vimeo.com/video/860038461?h=87fb31038b"
     )
     page.get_by_role("button", name="Save changes").click()
     expect(
         page.frame_locator(
             "internal:role=tabpanel[name=\"Livestream\"i] >> iframe"
-        ).get_by_role("link", name="Faster, Better, Cheaper -")
-    ).to_be_visible()
+        ).get_by_label("YouTube Video Player")
+    ).to_contain_text("Rick Astley - Never Gonna Give You Up (Official Music Video)")
     page.get_by_role("tab", name="Livestream 2").click()
     expect(
         page.frame_locator(
             "internal:role=tabpanel[name=\"Livestream\"i] >> iframe"
         ).get_by_role("banner")
-    ).to_contain_text("How to do AI right - Workshop Promo")
+    ).to_contain_text("Rick Astley - Never Gonna Give You Up (Video)")
+    page.get_by_role("tab", name="Livestream 3").click()
+    expect(
+        page.frame_locator(
+            "internal:role=tabpanel[name=\"Livestream\"i] >> iframe"
+        ).get_by_role("banner")
+    ).to_contain_text(
+        "Practical SLSA for developers and application security professionals"
+    )
