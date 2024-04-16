a019fe42a935e9f2ec77052758089d7d5a7df012
Vidya Ramakrishnan <vidya@hasgeek.in>
2024-04-16 16:29:48 +0530

    Updating the project filter

--- funnel/views/profile.py
+++ funnel/views/profile.py
@@ -260,8 +260,22 @@ class ProfileView(UrlChangeCheck, AccountViewBase):
             if end > start:
                 filtered_projects = (
                     all_projects.filter(
-                        Project.start_at >= start,
-                        Project.end_at < end,
+                        sa.or_(
+                            sa.and_(
+                                Project.start_at >= start,
+                                Project.end_at < end,
+                            ),
+                            sa.and_(
+                                Project.start_at < start,
+                                Project.end_at > start,
+                                Project.end_at < end,
+                            ),
+                            sa.and_(
+                                Project.start_at < end,
+                                Project.start_at >= start,
+                                Project.end_at >= end),
+                        )
+
                     )
                     .order_by(Project.order_by_date())
                     .all()
