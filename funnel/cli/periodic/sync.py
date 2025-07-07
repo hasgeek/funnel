0d63de4dcf095f0b93ce8b828a997e0d08fa306c
Kiran Jonnalagadda <kiran@hasgeek.com>
2025-07-07 19:45:24 +0530

    Add a CLI command to sync tickets (#2189)

---
+++ funnel/cli/periodic/sync.py
@@ -0,0 +1,29 @@
+"""Periodic data sync."""
+
+from __future__ import annotations
+
+from collections.abc import Iterable
+
+import click
+
+from ...models import Project
+from ...views.jobs import import_tickets
+from . import periodic
+
+
+@periodic.command('sync')
+@click.argument('projects', type=str, nargs=-1)
+def sync(projects: Iterable[str]) -> None:
+    """Sync tickets for specified projects (2m)."""
+    if not projects:
+        raise click.UsageError("Specify projects to sync as account/project.")
+    for name in projects:
+        project = Project.get(name)
+        if project is None:
+            click.echo(f"Project {name} does not exist", err=True)
+            continue
+        if not project.ticket_clients:
+            click.echo(f"Project {name} has nothing to sync", err=True)
+            continue
+        for ticket_client in project.ticket_clients:
+            import_tickets.enqueue(ticket_client.id)
